538b9d60f8de348fd780cbd58faae30e14a8f22b
lsw <1461069578@qq.com>
2019-07-22 10:26:21 +0800

    'lsw修改协同任务功能'

--- public/assets/js/backend/infosynergytaskmanage/info_synergy_task.js
+++ public/assets/js/backend/infosynergytaskmanage/info_synergy_task.js
@@ -62,7 +62,22 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                     ]
                 ]
             });
-
+            $('.panel-heading a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+                var field = $(this).data("field");
+                var value = $(this).data("value");
+                var options = table.bootstrapTable('getOptions');
+                options.pageNumber = 1;
+                options.queryParams = function (params) {
+                    var filter = {};
+                    if (value !== '') {
+                        filter[field] = value;
+                    }
+                    params.filter = JSON.stringify(filter);
+                    return params;
+                };
+                table.bootstrapTable('refresh', {});
+                return false;
+            });
             // 为表格绑定事件
             Table.api.bindevent(table);
         },
