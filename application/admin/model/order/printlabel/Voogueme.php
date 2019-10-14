6b8911624b25477e10e72f9cc7cadfc3bd412bc3
3232539029 <37733499+3232539029@users.noreply.github.com>
2019-10-14 14:57:45 +0800

    配货模块

--- application/admin/model/order/printlabel/Voogueme.php
+++ application/admin/model/order/printlabel/Voogueme.php
@@ -12,9 +12,8 @@ class Voogueme extends Model
 
 
     //数据库
-    // protected $connection = 'database';
+    // protected $connection = 'database.db_voogueme_online';
     protected $connection = 'database.db_voogueme';
-
 
     // 表名
     protected $table = 'sales_flat_order';
