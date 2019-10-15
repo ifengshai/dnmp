bfc788e43cdea03c2bb24722c10b49eef01a5082
3232539029 <37733499+3232539029@users.noreply.github.com>
2019-10-15 11:21:18 +0800

    SKU 过滤字母

--- application/admin/controller/order/printlabel/Zeelool.php
+++ application/admin/controller/order/printlabel/Zeelool.php
@@ -9,7 +9,7 @@ use think\Loader;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 use Util\ZeeloolPrescriptionDetailHelper;
-
+use Util\SKUHelper;
 /**
  * Sales Flat Order
  *
@@ -852,7 +852,7 @@ EOF;
             " . $prismcheck_os_value . $os_add . $os_pd .
                     " </tr>
             <tr>
-            <td colspan='2'>" . $processing_value['sku'] . "</td>
+            <td colspan='2'>" . SKUHelper::sku_filter($processing_value['sku']) . "</td>
             <td colspan='8' style=' text-align:center'>Lens：" . $final_print['index_type'] . "</td>
             </tr>  
             </tbody></table></div>";
---
+++ extend/Util/SKUHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Util;
+use think\Db;
+
+class SKUHelper{
+
+    /* 
+    * SKU 过滤字符  GOP01954-01 -> 01954-01
+    */
+    public static function sku_filter($target_sku)
+    {
+        $target_sku = str_replace('Z', '', $target_sku);
+        $target_sku = str_replace('V', '', $target_sku);
+        $target_sku = str_replace('F', '', $target_sku);
+        $target_sku = str_replace('P', '', $target_sku);
+        $target_sku = str_replace('T', '', $target_sku);
+        $target_sku = str_replace('M', '', $target_sku);
+        $target_sku = str_replace('N', '', $target_sku);
+        $target_sku = str_replace('H', '', $target_sku);
+        $target_sku = str_replace('A', '', $target_sku);
+        $target_sku = str_replace('X', '', $target_sku);
+        $target_sku = str_replace('I', '', $target_sku);
+        $target_sku = str_replace('O', '', $target_sku);
+        $target_sku = str_replace('G', '', $target_sku);
+        return $target_sku;
+    }
+    
+}
