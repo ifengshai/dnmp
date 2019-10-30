0f1f0a7ea1e509728376f8b6fd9a7e7d7d42beac
wpl <15978896108@163.com>
2019-10-30 12:06:47 +0800

    优化细节调整

--- application/admin/model/purchase/PurchaseReturn.php
+++ application/admin/model/purchase/PurchaseReturn.php
@@ -8,13 +8,13 @@ use think\Model;
 class PurchaseReturn extends Model
 {
 
-    
+
 
     //数据库
     protected $connection = 'database';
     // 表名
     protected $name = 'purchase_return';
-    
+
     // 自动写入时间戳字段
     protected $autoWriteTimestamp = 'int';
 
@@ -24,31 +24,23 @@ class PurchaseReturn extends Model
     protected $deleteTime = false;
 
     // 追加属性
-    protected $append = [
+    protected $append = [];
 
-    ];
-    
     //关联模型
     public function purchaseReturnItem()
     {
-        return $this->hasMany('purchaseReturnItem','return_id');
+        return $this->hasMany('purchaseReturnItem', 'return_id');
     }
-    
+
     //关联模型
     public function purchaseOrder()
     {
-        return $this->belongsTo('app\admin\model\purchase\PurchaseOrder', 'purchase_id')->setEagerlyType(0);
+        return $this->belongsTo('app\admin\model\purchase\PurchaseOrder', 'purchase_id', 'id', '', 'left')->setEagerlyType(0);
     }
 
     //关联模型
     public function supplier()
     {
-        return $this->belongsTo('app\admin\model\purchase\Supplier', 'supplier_id')->setEagerlyType(0);
+        return $this->belongsTo('app\admin\model\purchase\Supplier', 'supplier_id', 'id', '', 'left')->setEagerlyType(0);
     }
-
-
-
-
-
-
 }
