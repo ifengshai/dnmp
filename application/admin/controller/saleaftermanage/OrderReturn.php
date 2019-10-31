f953b5bdc9054cdca73d9363f15099c47049a8ae
lsw <1461069578@qq.com>
2019-10-31 17:03:32 +0800

    修改退货列表bug

--- application/admin/controller/saleaftermanage/OrderReturn.php
+++ application/admin/controller/saleaftermanage/OrderReturn.php
@@ -734,7 +734,7 @@ class OrderReturn extends Backend
     {
         if ($this->request->isAjax()) {
             $map['id'] = ['in', $ids];
-            $data['order_status'] = 4;
+            $data['order_status'] = 5;
             $result = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
             if (false !== $result) {
                 return $this->success('确认成功');
@@ -745,6 +745,23 @@ class OrderReturn extends Backend
             return $this->error('404 Not Found');
         }
     }
+    //退货单提交审核
+    public function submitAudit($ids = null)
+    {
+        if ($this->request->isAjax()) {
+            $map['id'] = ['in', $ids];
+            $map['order_status'] = 1;
+            $data['order_status'] = 2;
+            $res = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
+            if ($res !== false) {
+                $this->success('启动成功');
+            } else {
+                $this->error('启动失败');
+            }
+        } else {
+            $this->error('404 Not found');
+        }
+    }
         /***
      * 多个一起审核通过
      */
@@ -752,8 +769,8 @@ class OrderReturn extends Backend
     {
         if ($this->request->isAjax()) {
             $map['id'] = ['in', $ids];
-            $map['order_status'] = 1;
-            $data['order_status'] = 2;
+            $map['order_status'] = 2;
+            $data['order_status'] = 3;
             $res = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
             if ($res !== false) {
                 $this->success('审核成功');
@@ -771,8 +788,8 @@ class OrderReturn extends Backend
     {
         if ($this->request->isAjax()) {
             $map['id'] = ['in', $ids];
-            $map['order_status'] =1;
-            $data['order_status'] = 3;
+            $map['order_status'] =2;
+            $data['order_status'] = 4;
             $res = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
             if ($res !== false) {
                 $this->success('审核拒绝成功');
