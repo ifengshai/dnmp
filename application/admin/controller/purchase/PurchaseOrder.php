2c852884f5257bc669123e7b39b8ca020d4c7838
wpl <15978896108@163.com>
2019-11-07 16:01:31 +0800

    调试采集

--- application/admin/controller/purchase/PurchaseOrder.php
+++ application/admin/controller/purchase/PurchaseOrder.php
@@ -786,6 +786,7 @@ class PurchaseOrder extends Backend
             //设置缓存
             cache('Crontab_getAlibabaPurchaseOrder_' . date('YmdH') . md5(serialize($params)), $data, 3600);
         }
+        dump($data);die;
 
         foreach ($data as $key => $val) {
             if (!$val) {
