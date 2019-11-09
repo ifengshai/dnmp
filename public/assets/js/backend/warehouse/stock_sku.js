2a88c059e95b7a20e0c29d14ca8fd1e8c93ea9fd
wpl <15978896108@163.com>
2019-11-09 16:55:10 +0800

    修改bug

--- public/assets/js/backend/warehouse/stock_sku.js
+++ public/assets/js/backend/warehouse/stock_sku.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui'], function ($, undefined, Backend, Table, Form) {
 
     var Controller = {
         index: function () {
@@ -28,10 +28,10 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         { checkbox: true },
                         { field: 'id', title: __('Id') },
                         { field: 'sku', title: __('Sku'), operate: 'like' },
-                        { field: 'name', title: __('商品名称'), operate: 'like' },
+                        { field: 'name', title: __('商品名称'), operate: false },
                         {
                             field: 'is_open', title: __('SKU启用状态'), custom: { 1: 'success', 2: 'danger' },
-                            searchList: { 1: '启用', 2: '禁用' },
+                            searchList: { 1: '启用', 2: '禁用' }, operate: false,
                             formatter: Table.api.formatter.status
                         },
 
@@ -54,6 +54,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         },
         add: function () {
             Controller.api.bindevent();
+
         },
         edit: function () {
             Controller.api.bindevent();
@@ -61,6 +62,42 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         api: {
             bindevent: function () {
                 Form.api.bindevent($("form[role=form]"));
+
+                //模糊匹配订单
+                $('.sku').autocomplete({
+                    source: function (request, response) {
+                        if (request.term.length > 2) {
+                            $.ajax({
+                                type: "POST",
+                                url: "ajax/ajaxGetLikeOriginSku",
+                                dataType: "json",
+                                cache: false,
+                                async: false,
+                                data: {
+                                    origin_sku: request.term
+                                },
+                                success: function (json) {
+                                    var data = json.data;
+                                    response($.map(data, function (item) {
+                                        return {
+                                            label: item,//下拉框显示值
+                                            value: item,//选中后，填充到input框的值
+                                            //id:item.bankCodeInfo//选中后，填充到id里面的值
+                                        };
+                                    }));
+                                }
+                            });
+                        }
+
+                    },
+                    delay: 10,//延迟100ms便于输入
+                    select: function (event, ui) {
+                        $("#bankUnionNo").val(ui.item.id);//取出在return里面放入到item中的属性
+                    },
+                    scroll: true,
+                    pagingMore: true,
+                    max: 5000
+                });
             }
         }
     };
