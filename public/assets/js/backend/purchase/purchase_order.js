609c0fa7ebdbc12ae6e8bf8814552d226bf5d1b5
wpl <15978896108@163.com>
2019-11-15 16:36:27 +0800

    修改提货先入先出

--- public/assets/js/backend/purchase/purchase_order.js
+++ public/assets/js/backend/purchase/purchase_order.js
@@ -85,7 +85,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui'], function ($,
                             searchList: { 0: '否', 1: '是' },
                             formatter: Table.api.formatter.status
                         },
-                        { field: 'create_person', title: __('Create_person') ,operate: 'like'},
+                        { field: 'create_person', title: __('Create_person'), operate: 'like' },
                         { field: 'createtime', title: __('Createtime'), operate: 'RANGE', addclass: 'datetimerange' },
                         {
                             field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, buttons: [
@@ -299,7 +299,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui'], function ($,
             })
 
 
-           
+
 
         },
         edit: function () {
@@ -562,7 +562,59 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui'], function ($,
                     });
 
                 })
+
+
+
+                //选中的开始时间和现在的时间比较
+                $(document).on('dp.change', '.delivery_stime', function () {
+                    var time_value = $(this).val();
+                    //console.log(time_value);
+                    function getNow(s) {
+                        return s < 10 ? '0' + s : s;
+                    }
+
+                    var myDate = new Date();
+
+                    var year = myDate.getFullYear();        //获取当前年
+                    var month = myDate.getMonth() + 1;   //获取当前月
+                    var date = myDate.getDate();            //获取当前日
+                    var h = myDate.getHours();              //获取当前小时数(0-23)
+                    var m = myDate.getMinutes() - 10;          //获取当前分钟数(0-59)
+                    var s = myDate.getSeconds();
+                    var now = year + '-' + getNow(month) + "-" + getNow(date) + " " + getNow(h) + ':' + getNow(m) + ":" + getNow(s);
+                    if (time_value > now) {
+                        //console.log(1111);
+                    } else {
+                        Layer.alert('预售开始时间小于当前时间,如果添加则默认开始预售');
+                        //console.log(2222);
+                    }
+
+                });
+                ////选中的结束时间和现在的时间比较
+                $(document).on('dp.change', '.delivery_etime', function () {
+                    var time_end_value = $(this).val();
+                    console.log(time_end_value);
+                    function getNow(s) {
+                        return s < 10 ? '0' + s : s;
+                    }
+                    var myDate = new Date();
+                    var year = myDate.getFullYear();        //获取当前年
+                    var month = myDate.getMonth() + 1;   //获取当前月
+                    var date = myDate.getDate();            //获取当前日
+                    var h = myDate.getHours();              //获取当前小时数(0-23)
+                    var m = myDate.getMinutes() - 10;          //获取当前分钟数(0-59)
+                    var s = myDate.getSeconds();
+                    var now = year + '-' + getNow(month) + "-" + getNow(date) + " " + getNow(h) + ':' + getNow(m) + ":" + getNow(s);
+                    if (time_end_value > now) {
+                        $('.btn-success').removeClass('btn-disabled disabled');
+                    } else {
+                        $('.btn-success').addClass('btn-disabled disabled');
+                        Layer.alert('当前时间大于预售结束时间无法添加,请重新选择');
+                        return false;
+                    }
+                });
             }
+
         }
     };
     return Controller;
