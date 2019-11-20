fdd2dffe97437aeb817f7ad51cd5f544cec31852
lsw <1461069578@qq.com>
2019-11-20 18:13:32 +0800

    提交

--- public/assets/js/backend/saleaftermanage/order_return.js
+++ public/assets/js/backend/saleaftermanage/order_return.js
@@ -689,7 +689,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui'], function ($,
                 var entity_id = $(this).parent().prev().prev().html();
                 var order_platform = $('#c-order_platform').val();
                 var track_number = $(this).html();
-                Backend.api.open('saleaftermanage/order_return/get_logistics_info/?track_number='+track_number+'&entity_id='+entity_id+'&order_platform='+order_platform,'查询物流信息',{area:["50%", "50%"]});
+                Backend.api.open('saleaftermanage/order_return/get_logistics_info/?track_number='+track_number+'&entity_id='+entity_id+'&order_platform='+order_platform,'查询物流信息',{area:["60%", "60%"]});
             });
             // $(document).on('change','#increment_id',function(){
             //     var incrementId = $('#increment_id').val();
