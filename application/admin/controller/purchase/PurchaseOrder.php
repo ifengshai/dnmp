bd7492e40e3e4c15b1d866b0f26e8c4cc9caa77c
wpl <15978896108@163.com>
2019-12-27 17:14:26 +0800

    '修改'

--- application/admin/controller/purchase/PurchaseOrder.php
+++ application/admin/controller/purchase/PurchaseOrder.php
@@ -737,7 +737,7 @@ class PurchaseOrder extends Backend
          * @todo 后面添加采集时间段
          */
         $params = [
-            'createStartTime' => date('YmdHis', strtotime("-60 day")) . '000+0800',
+            'createStartTime' => date('YmdHis', strtotime("-90 day")) . '000+0800',
             'createEndTime' => date('YmdHis') . '000+0800',
         ];
 
