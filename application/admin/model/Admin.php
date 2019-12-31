7ea51c9ea09a762d8454e81425a9e759cc859334
liushiwei <1461069578@qq.com>
2019-12-31 09:46:33 +0800

    tijiao

--- application/admin/model/Admin.php
+++ application/admin/model/Admin.php
@@ -39,7 +39,9 @@ class Admin extends Model
     public function getStaffList($id)
     {
         $id = 35;
-        $result = $this->alias('a')->join(' auth_group_access g','a.id = g.uid')->where('a.status','=','normal')->where('g.group_id','in',$id)->field('a.id,a.nickname
+        // $result = $this->alias('a')->join(' auth_group_access g','a.id = g.uid')->where('a.status','=','normal')->where('g.group_id','in',$id)->field('a.id,a.nickname
+        // ')->select();
+        $result = $this->alias('a')->join(' auth_group_access g','a.id = g.uid')->where('a.status','=','normal')->field('a.id,a.nickname
         ')->select();
         if(!$result){
             return false;
