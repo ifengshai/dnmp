792361421c321a8f796dd847d7d20d2be55676aa
wpl <15978896108@163.com>
2020-01-02 13:45:10 +0800

    '修改'

--- public/assets/js/backend/purchase/purchase_order.js
+++ public/assets/js/backend/purchase/purchase_order.js
@@ -206,7 +206,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui', 'bootstrap-ta
                                     },
                                     visible: function (row) {
                                         //返回true时按钮显示,返回false隐藏
-                                        if (row.purchase_status == 0) {
+                                        if (row.purchase_status == 0 || row.purchase_type == 2) {
                                             return true;
                                         } else {
                                             return false;
