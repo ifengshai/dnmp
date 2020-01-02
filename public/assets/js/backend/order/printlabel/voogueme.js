85944d6dea446e9018368a27dd267836e932f700
wpl <15978896108@163.com>
2020-01-02 09:38:29 +0800

    '修改'

--- public/assets/js/backend/order/printlabel/voogueme.js
+++ public/assets/js/backend/order/printlabel/voogueme.js
@@ -31,11 +31,11 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'bootstrap-table-jump
                         { field: 'base_shipping_amount', title: __('运费'), operate: false, formatter: Controller.api.formatter.float_format },
 
                         { field: 'total_qty_ordered', title: __('SKU数量'), operate: false, formatter: Controller.api.formatter.int_format },
-                        { field: 'custom_print_label', title: __('打印标签'), operate: false, custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
-                        { field: 'custom_is_match_frame', title: __('配镜架'), operate: false, custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
-                        { field: 'custom_is_match_lens', title: __('配镜片'), operate: false, custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
-                        { field: 'custom_is_send_factory', title: __('加工'), operate: false, custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
-                        { field: 'custom_is_delivery', title: __('质检'), operate: false, custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
+                        { field: 'custom_print_label', title: __('打印标签'),  custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
+                        { field: 'custom_is_match_frame', title: __('配镜架'),  custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
+                        { field: 'custom_is_match_lens', title: __('配镜片'),  custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
+                        { field: 'custom_is_send_factory', title: __('加工'),  custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
+                        { field: 'custom_is_delivery', title: __('质检'),  custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
                         {
                             field: 'task_info', title: __('协同任务'), operate: false, formatter: function (value, row) {
                                 if (value) {
