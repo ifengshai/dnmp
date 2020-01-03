043f5a7c49b9327db81e162aee4b3ef04185307b
wpl <15978896108@163.com>
2020-01-03 15:04:15 +0800

    '修改'

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -554,7 +554,7 @@ class Dashboard extends Backend
     /**
      * 处理采购单旧数据
      */
-    public function purchase_test()
+    protected function purchase_test()
     {
         set_time_limit(0);
         
