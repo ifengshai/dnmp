4143a8b5c888384add3e1ca5a9191e136c0b35dd
wpl <15978896108@163.com>
2020-01-07 18:04:19 +0800

    '修改'

--- public/assets/js/backend/warehouse/check.js
+++ public/assets/js/backend/warehouse/check.js
@@ -23,7 +23,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui', 'bootstrap-se
             table.bootstrapTable({
                 url: $.fn.bootstrapTable.defaults.extend.index_url,
                 pk: 'id',
-                sortName: 'id',
+                sortName: 'createtime',
                 columns: [
                     [
                         { checkbox: true },
