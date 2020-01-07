5ee773dc30173e76a236b1ecde45b6af0e23e8e0
wpl <15978896108@163.com>
2020-01-07 11:18:04 +0800

    '修改补货'

--- application/admin/controller/warehouse/Check.php
+++ application/admin/controller/warehouse/Check.php
@@ -675,6 +675,6 @@ class Check extends Backend
         $data = $supplier->getSupplierData();
         $this->assign('supplier', $data);
 
-        return $this->view->fetch();
+        return $this->fetch();
     }
 }
