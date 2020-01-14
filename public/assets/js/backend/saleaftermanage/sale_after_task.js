7172cbfdf09825b32504cd5cda078e8f5e013d70
liushiwei <1461069578@qq.com>
2020-01-14 17:34:07 +0800

    tijiao

--- public/assets/js/backend/saleaftermanage/sale_after_task.js
+++ public/assets/js/backend/saleaftermanage/sale_after_task.js
@@ -217,6 +217,20 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','jqui','custom-css','b
                     }
                 );
             });
+            //批量导出xls 
+            $('.btn-batch-export-xls').click(function () {
+                var ids = Table.api.selectedids(table);
+                if (ids.length > 0) {
+                    window.open(Config.moduleurl + '/saleaftermanage/sale_after_task/batch_export_xls?ids=' + ids, '_blank');
+                } else {
+                    var options = table.bootstrapTable('getOptions');
+                    var search = options.queryParams({});
+                    var filter = search.filter;
+                    var op = search.op;
+                    window.open(Config.moduleurl + '/saleaftermanage/sale_after_task/batch_export_xls?filter=' + filter + '&op=' + op, '_blank');
+                }
+                
+            });
 
         },
         add: function () {
