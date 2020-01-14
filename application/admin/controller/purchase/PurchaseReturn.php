530894d496871a6ee1781eb8e94ddce5a0e5d2c2
wpl <15978896108@163.com>
2020-01-14 14:30:04 +0800

    'xiugai'

--- application/admin/controller/purchase/PurchaseReturn.php
+++ application/admin/controller/purchase/PurchaseReturn.php
@@ -7,6 +7,7 @@ use think\Db;
 use think\Exception;
 use think\exception\PDOException;
 use think\exception\ValidateException;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 /**
  * 退销单管理
@@ -539,5 +540,150 @@ class PurchaseReturn extends Backend
         } else {
             $this->error('取消失败！！');
         }
+    }
+
+
+    //批量导出xls
+    public function batch_export_xls()
+    {
+        set_time_limit(0);
+        ini_set('memory_limit', '512M');
+        $ids = input('ids');
+
+        if ($ids) {
+            $map['purchase_return.id'] = ['in', $ids];
+        }
+
+        list($where) = $this->buildparams();
+        $list = $this->model
+            ->with(['purchaseorder' , 'supplier'])
+            ->join(['fa_purchase_return_item' => 'b'], 'b.return_id=purchase_return.id')
+            ->join(['fa_check_order_item' => 'c'], 'b.check_item_id=c.id')
+            ->field('purchase_return.*,b.*,c.purchase_num,c.supplier_sku,c.arrivals_num,c.quantity_num,c.remark as check_remark')
+            ->where($where)
+            ->where($map)
+            ->select();
+        
+        $list = collection($list)->toArray();
+        
+        //从数据库查询需要的数据
+        $spreadsheet = new Spreadsheet();
+
+        //常规方式：利用setCellValue()填充数据
+        $spreadsheet->setActiveSheetIndex(0)->setCellValue("A1", "退销单号")
+            ->setCellValue("B1", "采购单号")
+            ->setCellValue("C1", "供应商");   //利用setCellValues()填充数据
+        $spreadsheet->setActiveSheetIndex(0)->setCellValue("D1", "供应商SKU")
+            ->setCellValue("E1", "采购数量")
+            ->setCellValue("F1", "到货数量")
+            ->setCellValue("G1", "合格数量");
+        $spreadsheet->setActiveSheetIndex(0)->setCellValue("H1", "退销数量")
+            ->setCellValue("I1", "退销类型");
+        $spreadsheet->setActiveSheetIndex(0)
+            ->setCellValue("J1", "退销金额")
+            ->setCellValue("K1", "退销备注")
+            ->setCellValue("L1", "质检备注")
+            ->setCellValue("M1", "联系人");
+        $spreadsheet->setActiveSheetIndex(0)->setCellValue("N1", "联系电话")
+            ->setCellValue("O1", "收货地址")
+            ->setCellValue("P1", "创建时间")
+            ->setCellValue("Q1", "创建人");
+
+        $spreadsheet->setActiveSheetIndex(0)->setTitle('退销单数据');
+
+        foreach ($list as $key => $value) {
+
+            $spreadsheet->getActiveSheet()->setCellValue("A" . ($key * 1 + 2), $value['return_number']);
+            $spreadsheet->getActiveSheet()->setCellValue("B" . ($key * 1 + 2), $value['purchaseorder']['purchase_number']);
+            $spreadsheet->getActiveSheet()->setCellValue("C" . ($key * 1 + 2), $value['supplier']['supplier_name']);
+            $spreadsheet->getActiveSheet()->setCellValue("D" . ($key * 1 + 2), $value['supplier_sku']);
+            $spreadsheet->getActiveSheet()->setCellValue("E" . ($key * 1 + 2), $value['purchase_num']);
+            $spreadsheet->getActiveSheet()->setCellValue("F" . ($key * 1 + 2), $value['arrivals_num']);
+            $spreadsheet->getActiveSheet()->setCellValue("G" . ($key * 1 + 2), $value['quantity_num']);
+            $spreadsheet->getActiveSheet()->setCellValue("H" . ($key * 1 + 2), $value['return_num']);
+
+            if ($value['return_type'] == 1) {
+                $type = '仅退款';
+            } elseif($value['return_type'] == 2) {
+                $type = '退货退款';
+            } else {
+                $type = '调换货';
+            }
+            $spreadsheet->getActiveSheet()->setCellValue("I" . ($key * 1 + 2), $type);
+            $spreadsheet->getActiveSheet()->setCellValue("J" . ($key * 1 + 2), $value['return_money']);
+            $spreadsheet->getActiveSheet()->setCellValue("K" . ($key * 1 + 2), $value['remark']);
+            $spreadsheet->getActiveSheet()->setCellValue("L" . ($key * 1 + 2), $value['check_remark']);
+            $spreadsheet->getActiveSheet()->setCellValue("M" . ($key * 1 + 2), $value['supplier_linkname']);
+            $spreadsheet->getActiveSheet()->setCellValue("N" . ($key * 1 + 2), $value['supplier_linkphone']);
+            $spreadsheet->getActiveSheet()->setCellValue("O" . ($key * 1 + 2), $value['supplier_address']);
+            $spreadsheet->getActiveSheet()->setCellValue("P" . ($key * 1 + 2), $value['createtime']);
+            $spreadsheet->getActiveSheet()->setCellValue("Q" . ($key * 1 + 2), $value['create_person']);
+
+        }
+
+        //设置宽度
+        $spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(30);
+        $spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(30);
+        $spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(30);
+        $spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(12);
+        $spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(12); 
+        $spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(12);
+        $spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(12);
+
+        $spreadsheet->getActiveSheet()->getColumnDimension('H')->setWidth(12);
+        $spreadsheet->getActiveSheet()->getColumnDimension('I')->setWidth(20);
+
+        $spreadsheet->getActiveSheet()->getColumnDimension('J')->setWidth(20);
+        $spreadsheet->getActiveSheet()->getColumnDimension('K')->setWidth(30);
+        $spreadsheet->getActiveSheet()->getColumnDimension('L')->setWidth(30);
+        $spreadsheet->getActiveSheet()->getColumnDimension('M')->setWidth(20);
+        $spreadsheet->getActiveSheet()->getColumnDimension('N')->setWidth(30);
+        $spreadsheet->getActiveSheet()->getColumnDimension('O')->setWidth(30);
+        $spreadsheet->getActiveSheet()->getColumnDimension('P')->setWidth(25);
+        $spreadsheet->getActiveSheet()->getColumnDimension('Q')->setWidth(20);
+
+        
+
+        //设置边框
+        $border = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN, // 设置border样式
+                    'color'       => ['argb' => 'FF000000'], // 设置border颜色
+                ],
+            ],
+        ];
+
+        $spreadsheet->getDefaultStyle()->getFont()->setName('微软雅黑')->setSize(12);
+
+
+        $setBorder = 'A1:' . $spreadsheet->getActiveSheet()->getHighestColumn() . $spreadsheet->getActiveSheet()->getHighestRow();
+        $spreadsheet->getActiveSheet()->getStyle($setBorder)->applyFromArray($border);
+
+        $spreadsheet->getActiveSheet()->getStyle('A1:Q' . $spreadsheet->getActiveSheet()->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+       
+
+        $spreadsheet->setActiveSheetIndex(0);
+        // return exportExcel($spreadsheet, 'xls', '登陆日志');
+        $format = 'xlsx';
+        $savename = '退销单数据' . date("YmdHis", time());;
+        
+        if ($format == 'xls') {
+            //输出Excel03版本
+            header('Content-Type:application/vnd.ms-excel');
+            $class = "\PhpOffice\PhpSpreadsheet\Writer\Xls";
+        } elseif ($format == 'xlsx') {
+            //输出07Excel版本
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            $class = "\PhpOffice\PhpSpreadsheet\Writer\Xlsx";
+        }
+
+        //输出名称
+        header('Content-Disposition: attachment;filename="' . $savename . '.' . $format . '"');
+        //禁止缓存
+        header('Cache-Control: max-age=0');
+        $writer = new $class($spreadsheet);
+
+        $writer->save('php://output');
     }
 }
--- public/assets/js/backend/purchase/purchase_return.js
+++ public/assets/js/backend/purchase/purchase_return.js
@@ -147,6 +147,21 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui','bootstrap-tab
                     table.bootstrapTable('refresh');
                 });
             })
+
+            //批量导出xls 
+            $('.btn-batch-export-xls').click(function () {
+                var ids = Table.api.selectedids(table);
+                if (ids.length > 0) {
+                    window.open(Config.moduleurl + '/purchase/purchase_return/batch_export_xls?ids=' + ids, '_blank');
+                } else {
+                    var options = table.bootstrapTable('getOptions');
+                    var search = options.queryParams({});
+                    var filter = search.filter;
+                    var op = search.op;
+                    window.open(Config.moduleurl + '/purchase/purchase_return/batch_export_xls?filter=' + filter + '&op=' + op, '_blank');
+                }
+                
+            });
         },
         add: function () {
             Controller.api.bindevent();
