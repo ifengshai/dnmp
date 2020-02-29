285fc49e34c0c4b680d063b3ce7d805ef7724f56
lei121 <15978896108@163.com>
2020-02-29 16:08:46 +0800

    添加

--- public/assets/js/backend/new_product.js
+++ public/assets/js/backend/new_product.js
@@ -38,6 +38,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui', 'fast', 'boot
                         },
                         { field: 'id', title: __('Id'), operate: false, visible: false },
                         { field: 'sku', title: __('Sku'), operate: 'like' },
+                        { field: 'price', title: __('单价'), operate: false },
                         { field: 'name', title: __('Name'), operate: 'like' },
                         { field: 'supplier.supplier_name', title: __('供应商名称'), operate: 'like' },
                         { field: 'supplier_sku', title: __('供应商SKU'), operate: 'like' },
