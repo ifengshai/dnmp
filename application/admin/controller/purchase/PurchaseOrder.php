e4e1ad2dfdefb8b8c6c03e37d34ff21a270c406b
lei121 <15978896108@163.com>
2020-03-03 10:22:55 +0800

    修改

--- application/admin/controller/purchase/PurchaseOrder.php
+++ application/admin/controller/purchase/PurchaseOrder.php
@@ -788,8 +788,10 @@ class PurchaseOrder extends Backend
     public function matching()
     {
         //查询SKU为空的采购单
-        $data = $this->purchase_order_item->where('sku', 'exp', 'is null')->select();
+        
+        $data = $this->purchase_order_item->whereExp('','LENGTH(trim(sku))=0','xor')->whereOr('sku', 'exp', 'is null')->select(false);
         $data = collection($data)->toArray();
+       
         foreach ($data as $k => $v) {
             //匹配SKU
             if ($v['skuid']) {
