f39bc327c84b2ebcc0b8aa8f197c0755a3a66fc7
crasphb <646054215@qq.com>
2020-03-03 09:55:03 +0800

    修改数据表

--- database/migrations/20200228023708_zendesk_reply_detail.php
+++ database/migrations/20200228023708_zendesk_reply_detail.php
@@ -14,7 +14,7 @@ class ZendeskReplyDetail extends Migrator
         $table->addColumn('reply_id', 'integer',array('limit' => 10,'default'=>'0','signed' => false,'comment'=>'email的id'))
             ->addColumn('body','text',array('comment' => '发送内容'))
             ->addColumn('html_body','text',array('comment' => '发送内容的html格式'))
-            ->addColumn('tags','string',array('limit' => 20,'null' => true,'comment' => 'tags,多个用，号连接'))
+            ->addColumn('tags','string',array('limit' => 50,'null' => true,'comment' => 'tags,多个用，号连接'))
             ->addColumn('status','string',array('limit' => 10,'comment' => '状态'))
             ->addColumn('assignee_id','string',array('limit' => 20,'null' => true,'default'=>'0','comment' => '最后处理人id'))
             ->addColumn('create_time', 'datetime', [])
