5f2412556228c11a4169f87810e13de5f746ffc7
lei121 <15978896108@163.com>
2020-03-04 16:48:08 +0800

    修改

--- application/admin/controller/datacenter/Index.php
+++ application/admin/controller/datacenter/Index.php
@@ -655,7 +655,7 @@ class Index extends Backend
         $sql = "select SUM(IF((b.sph > - 3 AND b.sph < 0 ) AND b.cyl < 2, 1, 0 )) AS A,
         SUM(IF(( sph > - 3.00 AND sph < 0 AND cyl > 2.00 ) OR ( sph < - 3.00 AND sph > - 6.00 AND cyl < 2.00 ),1, 0 )) AS B,
         SUM(IF(( sph < - 3.00 AND sph > - 6.00 AND cyl > 2.00 ) OR ( sph > - 6.00 AND cyl > 0 ),1, 0)) AS C from
-        (select if((od_sph-os_sph) > 0,od_sph,os_sph) as sph,if((od_cyl-os_cyl) > 0,od_cyl,os_cyl) as cyl 
+        (select if (od_sph>os_sph,od_sph,os_sph) as sph,if(od_cyl>os_cyl,od_cyl,os_cyl) as cyl 
         from sales_flat_order_item_prescription where $where ) b where sph != '' and cyl != '' limit 1";
         $res = Db::connect('database.db_zeelool')->table('sales_flat_order_item_prescription')->query($sql);
 
