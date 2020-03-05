299bba0887f980929e38ad76636e3792dd3a96f7
lei121 <15978896108@163.com>
2020-03-05 14:32:41 +0800

    添加批发站站点

--- application/admin/controller/order/printlabel/Weseeoptical.php
+++ application/admin/controller/order/printlabel/Weseeoptical.php
@@ -35,14 +35,6 @@ class Weseeoptical extends Backend
         $this->model = new \app\admin\model\order\printlabel\Weseeoptical;
     }
 
-    // public function test(){
-    //     // echo '123456';
-    //     $entity_id = 1181;
-    //     dump(NihaoPrescriptionDetailHelper::get_one_by_entity_id($entity_id));
-    //     // $increment_id = '130023358';
-    //     // dump(VooguemePrescriptionDetailHelper::get_one_by_increment_id($increment_id));
-    // }
-
     /**
      * 默认生成的控制器所继承的父类中有index/add/edit/del/multi五个基础方法、destroy/restore/recyclebin三个回收站方法
      * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
