0a3d29e86c5cb979e3eca62223f288da9d62d7b6
liushiwei <1461069578@qq.com>
2020-03-11 10:35:39 +0800

    tijiao

--- application/admin/model/itemmanage/ItemPlatformSku.php
+++ application/admin/model/itemmanage/ItemPlatformSku.php
@@ -59,7 +59,12 @@ class ItemPlatformSku extends Model
                         $prefix = 'W';
                         break;
                     default:
+                        $prefix = false;
                         break;    
+                }
+                //判断前缀是否存在
+                if(false == $prefix){
+                    continue;
                 }
                 //监测平台sku是否存在
                 $platformSkuExists =$this->getTrueSku($prefix.$row['sku'],$k);
