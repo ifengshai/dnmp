eee487b1ca1e53812d0256c02a7c76b17c9b2019
liushiwei <1461069578@qq.com>
2020-03-14 16:44:20 +0800

    tijiao

--- application/admin/controller/datacenter/operationanalysis/operationkanban/Conversionrate.php
+++ application/admin/controller/datacenter/operationanalysis/operationkanban/Conversionrate.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\admin\controller\datacenter\operationanalysis\operationkanban;
 use app\common\controller\Backend;
+use app\admin\model\OrderStatistics;
 use app\admin\model\platformmanage\MagentoPlatform;
 class Conversionrate extends Backend{
     /**
@@ -13,9 +14,25 @@ class Conversionrate extends Backend{
      */
     public function index ()
     {
-        $platform = (new MagentoPlatform())->getOrderPlatformList();
+        $platform = (new MagentoPlatform())->getOrderPlatformList();	
+        //头部数据
+        if($this->request->isAjax()){
+            $orderStatistics = new OrderStatistics();
+            $list = $orderStatistics->getAllData();
+            foreach ($list as $v) {
+                $zeeloolShoppingCartUpdateTotal[$v['create_date']]              = $v['zeelool_shoppingcart_update_total'];
+                $zeeloolShoppingCartUpdateConversion[$v['create_date']] 	    = $v['zeelool_shoppingcart_update_conversion'];
+            }
+            $json['columnData'] = [
+                'type' => 'line',
+                'data' => $zeeloolShoppingCartUpdateTotal ?: [],
+                'name' => '购物车数量、购物车转化率线图'
+            ];
+        }
+
         $this->view->assign(
             [
+
                 'orderPlatformList'	=> $platform
             ]
         );
