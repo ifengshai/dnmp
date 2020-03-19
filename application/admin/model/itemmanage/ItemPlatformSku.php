c162191d4978402302654bb2a1257467596ccce4
liushiwei <1461069578@qq.com>
2020-03-19 18:31:33 +0800

    tijiao

--- application/admin/model/itemmanage/ItemPlatformSku.php
+++ application/admin/model/itemmanage/ItemPlatformSku.php
@@ -254,4 +254,28 @@ class ItemPlatformSku extends Model
         $map['sku'] = ['in', $skus];
         return $this->where($map)->group('sku')->count(1);
     }
+    /**
+     * 获取所有sku在各个平台上的上架数量
+     *
+     * @Description created by lsw
+     * @author lsw
+     * @since 2020/03/19 16:04:42 
+     * @param  $id sku类型 1 镜框 3 配饰 
+     * @param  $platform 平台 
+     * @return void
+     */
+    public function putawayDifferenceSku($id,$platform)
+    {
+        $item = new \app\admin\model\itemmanage\Item();
+        if(1 == $id){
+            $skus = $item->getFrameSku();
+        }elseif(3 == $id){
+            $skus = $item->getOrnamentsSku();
+        }
+        
+        $map['outer_sku_status'] = 1;
+        $map['platform_type']    = $platform;
+        $map['sku'] = ['in', $skus];
+        return $this->where($map)->group('sku')->count(1);
+    }    
 }
