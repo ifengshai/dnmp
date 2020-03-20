09a577cffd5916b504dac42d93858c00560bbf3f
wpl <wangpenglei@nextmar.com>
2020-03-20 17:11:45 +0800

    '修改'

--- application/admin/model/DataConfig.php
+++ application/admin/model/DataConfig.php
@@ -9,4 +9,17 @@ class DataConfig extends Model
     // 表名
     protected $name = 'data_config';
 
+    /**
+     * 查询对应字段数据
+     *
+     * @Description
+     * @author wpl
+     * @since 2020/03/20 16:27:53 
+     * @param [type] $key 对应key
+     * @return void
+     */
+    public function getValue($key)
+    {
+       return $this->where('key', $key)->value('value');
+    }
 }
