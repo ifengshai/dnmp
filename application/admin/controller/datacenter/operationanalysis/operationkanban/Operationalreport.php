dbf886f2c369224d5b3dc8a514a45337da87bc94
liushiwei <1461069578@qq.com>
2020-03-23 17:51:26 +0800

    tijiao

--- application/admin/controller/datacenter/operationanalysis/operationkanban/Operationalreport.php
+++ application/admin/controller/datacenter/operationanalysis/operationkanban/Operationalreport.php
@@ -2,6 +2,7 @@
 namespace app\admin\controller\datacenter\operationanalysis\operationkanban;
 use think\Db;
 use app\common\controller\Backend;
+use app\admin\model\OrderItemInfo;
 use app\admin\model\platformmanage\MagentoPlatform;
 class Operationalreport extends Backend{
     //订单类型数据统计
@@ -33,11 +34,61 @@ class Operationalreport extends Backend{
             if(4<=$order_platform){
                 return $this->error('该平台暂时没有数据');
             }
-            $result = $this->platformOrderInfo($order_platform,$map,$itemMap);
-            if(!$result){
-                return $this->error('暂无数据');
+            $orderItemInfo = new OrderItemInfo();
+            $list = $orderItemInfo->getAllData($order_platform);
+            $create_date = $frame_sales_num = $frame_in_print_num = $decoration_sales_num = $decoration_in_print_num =[];
+            foreach ($list as $v) {
+                $frame_sales_num[]          = $v['frame_sales_num'];
+                $frame_in_print_num[]       = $v['frame_in_print_num'];
+                $decoration_sales_num[]     = $v['decoration_sales_num'];
+                $decoration_in_print_num[]  = $v['decoration_in_print_num'];
+                $create_date[]              = $v['create_date'];  
             }
-            return json(['code' => 1, 'data'=>[1,2,3],'rows' => $result]);
+            $json['xcolumnData'] = $json2['xcolumnData'] = $create_date ? $create_date :[];
+            $json['column'] = ['眼镜销售副数', '眼镜动销数'];
+            $json2['column'] = ['配饰销售副数', '配饰动销数'];
+            $json['columnData'] = [
+                [
+                    'type' => 'bar',
+                    'barWidth' => '20%',
+                    'data' => $frame_sales_num ? $frame_sales_num:[],
+                    'name' => '眼镜销售副数'
+                ],
+                [
+                    'type' => 'line',
+                    'yAxisIndex' => 1,
+                    'data' => $frame_in_print_num ? $frame_in_print_num:[],
+                    'name' => '眼镜动销数'                    
+                ]
+
+            ];
+            $json2['columnData'] = [
+                [
+                    'type' => 'bar',
+                    'barWidth' => '20%',
+                    'data' => $decoration_sales_num ? $decoration_sales_num:[],
+                    'name' => '配饰销售副数'
+                ],
+                [
+                    'type' => 'line',
+                    'yAxisIndex' => 1,
+                    'data' => $decoration_in_print_num ? $decoration_in_print_num:[],
+                    'name' => '配饰动销数'                    
+                ]
+            ];
+            if($params['key'] == 'frame_sales_num'){
+                return json(['code' => 1, 'data'=>$json]);
+
+            }elseif($params['key'] == 'decoration_sales_num'){
+                return json(['code' => 1, 'data'=>$json2]);
+            }else{
+           // $result = $this->platformOrderInfo($order_platform,$map,$itemMap);
+            // if(!$result){
+            //     return $this->error('暂无数据');
+            // }
+                return json(['code' => 1, 'rows' => $result]);
+            }
+            
 
         }	
         $this->view->assign(
--- public/assets/js/backend/datacenter/operationanalysis/operationkanban/operationalreport.js
+++ public/assets/js/backend/datacenter/operationanalysis/operationkanban/operationalreport.js
@@ -4,20 +4,19 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table','form', 'echartsobj
         index: function () {            
             Controller.api.formatter.daterangepicker($("form[role=form1]"));
             Form.api.bindevent($("form[role=form]"));
-                //购物车图表
+            //眼镜销售副数和动销数柱状和折线混合图
                 var chartOptions = {
                     targetId: 'echart',
                     downLoadTitle: '图表',
-                    type: 'line',
-                    line: {
+                    type: 'bar',
+                    bar:{
                         xAxis: {
                             type: 'category',
-                            boundaryGap: [0, 0.01]
                         },
                         yAxis: [
                             {
                                 type: 'value',
-                                name: '购物车数量',
+                                name: '配饰销售副数',
 								position: 'left',
                                 axisLabel: {
                                     formatter: '{value}'
@@ -25,10 +24,42 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table','form', 'echartsobj
                             },
                             {
                                 type: 'value',
-                                name: '购物车转化率',
+                                name: '配饰动销数',
 								position: 'right',
                                 axisLabel: {
-                                    formatter: '{value} %'
+                                    formatter: '{value}'
+                                }
+                            }
+                        ],                        
+                    }
+                };            
+                //饰品销售件数和动销数柱状和折线混合图
+                var chartOptions2 = {
+                    targetId: 'echart2',
+                    downLoadTitle: '图表',
+                    type: 'bar',
+                    bar: {
+                        xAxis: {
+                            type: 'category',
+                            axisPointer: {
+                                type: 'shadow'
+                            }
+                        },
+                        yAxis: [
+                            {
+                                type: 'value',
+                                name: '配饰销售副数',
+								position: 'left',
+                                axisLabel: {
+                                    formatter: '{value}'
+                                }
+                            },
+                            {
+                                type: 'value',
+                                name: '配饰动销数',
+								position: 'right',
+                                axisLabel: {
+                                    formatter: '{value}'
                                 }
                             }
                         ],
@@ -36,101 +67,110 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table','form', 'echartsobj
                 };
                 var time = $('#create_time').val();
                 var platform = $('#c-order_platform').val();
-                // var options = {
-                //     type: 'post',
-                //     url: 'datacenter/operationanalysis/operationkanban/operationalreport/index',
-                //     data: {
+                var options = {
+                    type: 'post',
+                    url: 'datacenter/operationanalysis/operationkanban/operationalreport/index',
+                    data: {
+                        'key':'frame_sales_num',
+                        'platform': platform
+                    }
+                }
+                var options2 = {
+                    type: 'post',
+                    url: 'datacenter/operationanalysis/operationkanban/operationalreport/index',
+                    data: {
+                        'key':'decoration_sales_num',
+                        'platform': platform
+                    }
+                }                
+                EchartObj.api.ajax(options, chartOptions)
+                EchartObj.api.ajax(options2, chartOptions2)
+                // Backend.api.ajax({
+                //     url: "datacenter/operationanalysis/operationkanban/operationalreport/index",
+                //     data: { 
                 //         'time': time,
-                //         'platform': platform
+                //         'platform': platform    
                 //     }
-                // }
-                // EchartObj.api.ajax(options, chartOptions)
-                Backend.api.ajax({
-                    url: "datacenter/operationanalysis/operationkanban/operationalreport/index",
-                    data: { 
-                        'time': time,
-                        'platform': platform    
-                    }
-                },function (data, ret) {
-                    console.log(ret.rows);
-                    var order_status_length = ret.rows.order_status.status.length;
-                    for(var j=0;j<order_status_length;j++){
-                        var order_status = ret.rows.order_status.status;
-                        var order_num    = ret.rows.order_status.num;
-                        var order_money  = ret.rows.order_status.money;
-                        $("#order-table tbody").append('<tr><td>'+order_status[j]+'</td><td>'+order_num[j]+'</td><td>'+order_money[j]+'</td></tr>');
-                    }
-                    var shipping_amount_length = ret.rows.base_shipping_amount.shipping_amount.length;
-                    for(var n=0;n<shipping_amount_length;n++){
-                        var shipping_amount         = ret.rows.base_shipping_amount.shipping_amount;
-                        var shipping_amount_num     = ret.rows.base_shipping_amount.num;
-                        var shipping_amount_money   = ret.rows.base_shipping_amount.money;
-                        $("#shipping_amount_table tbody").append('<tr><td>'+shipping_amount[n]+'</td><td>'+shipping_amount_num[n]+'</td><td>'+shipping_amount_money[n]+'</td></tr>');
-                    }
-                    $('#general_order').text(ret.rows.general_order);
-                    $('#general_money').text(ret.rows.general_money);
-                    $('#wholesale_order').text(ret.rows.wholesale_order);
-                    $('#wholesale_money').text(ret.rows.wholesale_money);
-                    $('#celebrity_order').text(ret.rows.celebrity_order);
-                    $('#celebrity_money').text(ret.rows.celebrity_money);
-                    $('#reissue_order').text(ret.rows.reissue_order);
-                    $('#reissue_money').text(ret.rows.reissue_money);
-                    $('#fill_post_order').text(ret.rows.fill_post_order);
-                    $('#fill_post_money').text(ret.rows.fill_post_money);
-                    $('#general_order_percent').text(ret.rows.general_order_percent);
-                    $('#wholesale_order_percent').text(ret.rows.wholesale_order_percent);
-                    $('#celebrity_order_percent').text(ret.rows.celebrity_order_percent);
-                    $('#reissue_order_percent').text(ret.rows.reissue_order_percent);
-                    $('#fill_post_order_percent').text(ret.rows.fill_post_order_percent);
-                    $('#usd_order_num').text(ret.rows.usd_order_num);
-                    $('#usd_order_percent').text(ret.rows.usd_order_percent);
-                    $('#usd_order_money').text(ret.rows.usd_order_money);
-                    $('#usd_order_average_amount').text(ret.rows.usd_order_average_amount);
-                    $('#cad_order_num').text(ret.rows.cad_order_num);
-                    $('#cad_order_percent').text(ret.rows.cad_order_percent);
-                    $('#cad_order_money').text(ret.rows.cad_order_money);
-                    $('#cad_order_average_amount').text(ret.rows.cad_order_average_amount);
-                    $('#aud_order_num').text(ret.rows.aud_order_num);
-                    $('#aud_order_percent').text(ret.rows.aud_order_percent);
-                    $('#aud_order_money').text(ret.rows.aud_order_money);
-                    $('#aud_order_average_amount').text(ret.rows.aud_order_average_amount);
-                    $('#eur_order_num').text(ret.rows.eur_order_num);
-                    $('#eur_order_percent').text(ret.rows.eur_order_percent);
-                    $('#eur_order_money').text(ret.rows.eur_order_money);
-                    $('#eur_order_average_amount').text(ret.rows.eur_order_average_amount);
-                    $('#gbp_order_num').text(ret.rows.gbp_order_num);
-                    $('#gbp_order_percent').text(ret.rows.gbp_order_percent);
-                    $('#gbp_order_money').text(ret.rows.gbp_order_money);
-                    $('#gbp_order_average_amount').text(ret.rows.gbp_order_average_amount);
-                    $('#frame_money').text(ret.rows.frame_money);
-                    $('#frame_sales_num').text(ret.rows.frame_sales_num);
-                    $('#frame_avg_money').text(ret.rows.frame_avg_money);
-                    $('#frame_onsales_num').text(ret.rows.frame_onsales_num);
-                    $('#decoration_money').text(ret.rows.decoration_money);
-                    $('#decoration_sales_num').text(ret.rows.decoration_sales_num);
-                    $('#decoration_avg_money').text(ret.rows.decoration_avg_money);
-                    $('#decoration_onsales_num').text(ret.rows.decoration_onsales_num);
-                    $('#frame_in_print_num').text(ret.rows.frame_in_print_num);
-                    $('#frame_in_print_rate').text(ret.rows.frame_in_print_rate);
-                    $('#decoration_in_print_num').text(ret.rows.decoration_in_print_num);
-                    $('#decoration_in_print_rate').text(ret.rows.decoration_in_print_rate);
-                    $('#frame_new_money').text(ret.rows.frame_new_money);
-                    $('#decoration_new_money').text(ret.rows.decoration_new_money);
-                    $('#frame_order_customer').text(ret.rows.frame_order_customer);
-                    $('#frame_avg_customer').text(ret.rows.frame_avg_customer);
-                    $('#decoration_order_customer').text(ret.rows.decoration_order_customer);
-                    $('#decoration_avg_customer').text(ret.rows.decoration_avg_customer);
-                    $('#frame_new_num').text(ret.rows.frame_new_num);
-                    $('#decoration_new_num').text(ret.rows.decoration_new_num);
-                    $('#frame_new_in_print_num').text(ret.rows.frame_new_in_print_num);
-                    $('#frame_new_in_print_rate').text(ret.rows.frame_new_in_print_rate);
-                    $('#decoration_new_in_print_num').text(ret.rows.decoration_new_in_print_num);
-                    $('#decoration_new_in_print_rate').text(ret.rows.decoration_new_in_print_rate);
+                // },function (data, ret) {
+                //     console.log(ret.rows);
+                //     var order_status_length = ret.rows.order_status.status.length;
+                //     for(var j=0;j<order_status_length;j++){
+                //         var order_status = ret.rows.order_status.status;
+                //         var order_num    = ret.rows.order_status.num;
+                //         var order_money  = ret.rows.order_status.money;
+                //         $("#order-table tbody").append('<tr><td>'+order_status[j]+'</td><td>'+order_num[j]+'</td><td>'+order_money[j]+'</td></tr>');
+                //     }
+                //     var shipping_amount_length = ret.rows.base_shipping_amount.shipping_amount.length;
+                //     for(var n=0;n<shipping_amount_length;n++){
+                //         var shipping_amount         = ret.rows.base_shipping_amount.shipping_amount;
+                //         var shipping_amount_num     = ret.rows.base_shipping_amount.num;
+                //         var shipping_amount_money   = ret.rows.base_shipping_amount.money;
+                //         $("#shipping_amount_table tbody").append('<tr><td>'+shipping_amount[n]+'</td><td>'+shipping_amount_num[n]+'</td><td>'+shipping_amount_money[n]+'</td></tr>');
+                //     }
+                //     $('#general_order').text(ret.rows.general_order);
+                //     $('#general_money').text(ret.rows.general_money);
+                //     $('#wholesale_order').text(ret.rows.wholesale_order);
+                //     $('#wholesale_money').text(ret.rows.wholesale_money);
+                //     $('#celebrity_order').text(ret.rows.celebrity_order);
+                //     $('#celebrity_money').text(ret.rows.celebrity_money);
+                //     $('#reissue_order').text(ret.rows.reissue_order);
+                //     $('#reissue_money').text(ret.rows.reissue_money);
+                //     $('#fill_post_order').text(ret.rows.fill_post_order);
+                //     $('#fill_post_money').text(ret.rows.fill_post_money);
+                //     $('#general_order_percent').text(ret.rows.general_order_percent);
+                //     $('#wholesale_order_percent').text(ret.rows.wholesale_order_percent);
+                //     $('#celebrity_order_percent').text(ret.rows.celebrity_order_percent);
+                //     $('#reissue_order_percent').text(ret.rows.reissue_order_percent);
+                //     $('#fill_post_order_percent').text(ret.rows.fill_post_order_percent);
+                //     $('#usd_order_num').text(ret.rows.usd_order_num);
+                //     $('#usd_order_percent').text(ret.rows.usd_order_percent);
+                //     $('#usd_order_money').text(ret.rows.usd_order_money);
+                //     $('#usd_order_average_amount').text(ret.rows.usd_order_average_amount);
+                //     $('#cad_order_num').text(ret.rows.cad_order_num);
+                //     $('#cad_order_percent').text(ret.rows.cad_order_percent);
+                //     $('#cad_order_money').text(ret.rows.cad_order_money);
+                //     $('#cad_order_average_amount').text(ret.rows.cad_order_average_amount);
+                //     $('#aud_order_num').text(ret.rows.aud_order_num);
+                //     $('#aud_order_percent').text(ret.rows.aud_order_percent);
+                //     $('#aud_order_money').text(ret.rows.aud_order_money);
+                //     $('#aud_order_average_amount').text(ret.rows.aud_order_average_amount);
+                //     $('#eur_order_num').text(ret.rows.eur_order_num);
+                //     $('#eur_order_percent').text(ret.rows.eur_order_percent);
+                //     $('#eur_order_money').text(ret.rows.eur_order_money);
+                //     $('#eur_order_average_amount').text(ret.rows.eur_order_average_amount);
+                //     $('#gbp_order_num').text(ret.rows.gbp_order_num);
+                //     $('#gbp_order_percent').text(ret.rows.gbp_order_percent);
+                //     $('#gbp_order_money').text(ret.rows.gbp_order_money);
+                //     $('#gbp_order_average_amount').text(ret.rows.gbp_order_average_amount);
+                //     $('#frame_money').text(ret.rows.frame_money);
+                //     $('#frame_sales_num').text(ret.rows.frame_sales_num);
+                //     $('#frame_avg_money').text(ret.rows.frame_avg_money);
+                //     $('#frame_onsales_num').text(ret.rows.frame_onsales_num);
+                //     $('#decoration_money').text(ret.rows.decoration_money);
+                //     $('#decoration_sales_num').text(ret.rows.decoration_sales_num);
+                //     $('#decoration_avg_money').text(ret.rows.decoration_avg_money);
+                //     $('#decoration_onsales_num').text(ret.rows.decoration_onsales_num);
+                //     $('#frame_in_print_num').text(ret.rows.frame_in_print_num);
+                //     $('#frame_in_print_rate').text(ret.rows.frame_in_print_rate);
+                //     $('#decoration_in_print_num').text(ret.rows.decoration_in_print_num);
+                //     $('#decoration_in_print_rate').text(ret.rows.decoration_in_print_rate);
+                //     $('#frame_new_money').text(ret.rows.frame_new_money);
+                //     $('#decoration_new_money').text(ret.rows.decoration_new_money);
+                //     $('#frame_order_customer').text(ret.rows.frame_order_customer);
+                //     $('#frame_avg_customer').text(ret.rows.frame_avg_customer);
+                //     $('#decoration_order_customer').text(ret.rows.decoration_order_customer);
+                //     $('#decoration_avg_customer').text(ret.rows.decoration_avg_customer);
+                //     $('#frame_new_num').text(ret.rows.frame_new_num);
+                //     $('#decoration_new_num').text(ret.rows.decoration_new_num);
+                //     $('#frame_new_in_print_num').text(ret.rows.frame_new_in_print_num);
+                //     $('#frame_new_in_print_rate').text(ret.rows.frame_new_in_print_rate);
+                //     $('#decoration_new_in_print_num').text(ret.rows.decoration_new_in_print_num);
+                //     $('#decoration_new_in_print_rate').text(ret.rows.decoration_new_in_print_rate);
 
-                },function(data,ret){
-                    alert(ret.msg);
-                    return false;
-                });                       
+                // },function(data,ret){
+                //     alert(ret.msg);
+                //     return false;
+                // });                       
         },
         api: {
             formatter: {
