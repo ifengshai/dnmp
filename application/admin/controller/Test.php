f3f7d839e19712e3b29c8ac87af10ad8205f740a
wpl <wangpenglei@nextmar.com>
2020-03-28 10:22:58 +0800

    '添加选品数据'

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -14,28 +14,4 @@ class Test extends Backend
         $this->item = new \app\admin\model\itemmanage\Item();
     }
 
-    public function test()
-    {
-        $skus = $this->newproduct->column('sku');
-        $map['sku'] = ['not in', $skus];
-        $map['is_del'] = 1;
-        $map['category_id'] = ['<>',43];
-        $data = $this->item->where($map)->select();
-        $data = collection($data)->toArray();
-        $list = [];
-        foreach ($data as $k => $v) {
-            $list[$k]['origin_sku'] = $v['origin_sku'];
-            $list[$k]['sku'] = $v['sku'];
-            $list[$k]['name'] = $v['name'];
-            $list[$k]['category_id'] = $v['category_id'];
-            $list[$k]['price'] = $v['price'];
-            $list[$k]['attribute_id'] = $v['attribute_id'];
-            $list[$k]['brand_id'] = $v['brand_id'];
-            $list[$k]['stock'] = $v['stock'];
-            $list[$k]['item_status'] = 2;
-            $list[$k]['create_person'] = $v['create_person'];
-            $list[$k]['create_time'] = $v['create_time'];
-        }
-        $this->newproduct->saveAll($list);
-    }
 }
