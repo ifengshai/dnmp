15b973dc8537f8aeb6e415099c86c6af9ab71681
lixiang0401 <6225998@qq.com>
2020-03-28 09:04:17 +0800

    去掉 console.log

--- public/assets/js/backend/demand/it_web_demand.js
+++ public/assets/js/backend/demand/it_web_demand.js
@@ -524,7 +524,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 getcontent: {
                     //格式为：方法名+空格+DOM元素
                     'click .btn-getcontent': function (e, value, row, index) {
-                        console.log(row.title);
                         var str = '标题：'+row.title+'<br><hr>内容：'+value;
                         Layer.open({
                             closeBtn: 1,
