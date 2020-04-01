0747d8fe2a1eee78281b2565c70caa4371236467
wpl <wangpenglei@nextmar.com>
2020-04-01 14:43:28 +0800

    修改订单排序

--- public/assets/js/backend/order/printlabel/zeelool.js
+++ public/assets/js/backend/order/printlabel/zeelool.js
@@ -21,6 +21,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'bootstrap-table-jump
                 pk: 'entity_id',
                 sortName: 'total_qty_ordered',
                 sortOrder: 'asc',
+
                 columns: [
                     [
                         { checkbox: true },
@@ -30,7 +31,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'bootstrap-table-jump
                         { field: 'base_grand_total', title: __('订单金额'), operate: false, formatter: Controller.api.formatter.float_format },
                         { field: 'base_shipping_amount', title: __('运费'), operate: false, formatter: Controller.api.formatter.float_format },
 
-                        { field: 'total_qty_ordered', title: __('SKU数量'), operate: 'between', formatter: Controller.api.formatter.int_format },
+                        { field: 'total_qty_ordered', title: __('SKU数量'), sortable: true, operate: 'between', formatter: Controller.api.formatter.int_format },
                         { field: 'custom_print_label_new', title: __('打印标签'), custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
                         { field: 'custom_is_match_frame_new', title: __('配镜架'), custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
                         { field: 'custom_is_match_lens_new', title: __('配镜片'), custom: { 0: 'danger', 1: 'green' }, searchList: { 1: '是', 0: '否' }, formatter: Table.api.formatter.status },
@@ -45,8 +46,8 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'bootstrap-table-jump
                             }
                         },
                         { field: 'custom_order_prescription_type', title: __('处方类型'), addClass: 'selectpicker', data: 'multiple', operate: 'IN', custom: { 1: 'green', 2: 'green', 3: 'green', 4: 'green', 5: 'green', 6: 'green' }, searchList: { 1: '仅镜架', 2: '现货处方镜', 3: '定制处方镜', 4: '镜架+现货', 5: '镜架+定制', 6: '现片+定制片', '': '获取中' }, formatter: Table.api.formatter.status },
-                        { field: 'order_type', title: __('订单类型'), custom: { 1: 'blue', 2: 'blue', 3: 'blue', 4: 'blue' }, searchList: { 1: '普通订单', 2: '批发单', 3: '网红单', 4: '补发单' }, formatter: Table.api.formatter.status },
-                        { field: 'created_at', title: __('创建时间'), operate: 'RANGE', addclass: 'datetimerange' },
+                        { field: 'order_type', title: __('订单类型'), custom: { 1: 'blue', 2: 'blue', 3: 'blue', 4: 'blue', 5: 'blue' }, searchList: { 1: '普通订单', 2: '批发单', 3: '网红单', 4: '补发单', 5: '补差价' }, formatter: Table.api.formatter.status },
+                        { field: 'created_at', title: __('创建时间'), operate: 'RANGE', sortable: true, addclass: 'datetimerange' },
                         { field: 'sku', title: __('SKU'), operate: 'like', visible: false },
                         {
                             field: 'category_id', title: __('任务分类'), searchList: function (column) {
