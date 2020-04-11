730355ece177ca6f639b435d2ee0eb9b28c789da
crasphb <646054215@qq.com>
2020-04-11 16:41:06 +0800

    1

--- public/assets/js/backend/zendesk/zendesk.js
+++ public/assets/js/backend/zendesk/zendesk.js
@@ -278,7 +278,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jq-tags', 'jqui','te
                 }
             });
             $(document).on('click','.change-ticket',function(){
-                var index = Layer.getFrameIndex(window.name); //先得到当前iframe层的索引
+                var index = parent.Layer.getFrameIndex(window.name); //先得到当前iframe层的索引
 
                 var title = $(this).data('title');
                 // var status = $(this).data('status');
