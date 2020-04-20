1231cfef7a36b82c23d794a650379e6583e80510
crasphb <646054215@qq.com>
2020-04-20 15:47:02 +0800

    提交

--- public/assets/js/backend/saleaftermanage/work_order_list.js
+++ public/assets/js/backend/saleaftermanage/work_order_list.js
@@ -221,8 +221,8 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'jqui', 'form'], function ($,
                                     callback: function (data) {
                                     },
                                     visible: function (row) {
-                                        //草稿，待审核状态+需要审核+审核人(经理)，才有审核权限
-                                        if (row.work_status == 1 && row.is_check == 1 && (Config.admin_id == row.assign_user_id || Config.workorder.customer_manager == row.assign_user_id)) {
+                                        //待审核状态+需要审核+审核人(经理)，才有审核权限
+                                        if (row.work_status == 1 && row.is_check == 1 && (Config.admin_id == row.assign_user_id || Config.workorder.customer_manager == Config.admin_id)) {
                                             return true;
                                         } else {
                                             return false;
