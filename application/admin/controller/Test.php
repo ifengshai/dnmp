ea1d03b2b731aa2bf98e7157b44bceca0a3fa3c8
wpl <wangpenglei@nextmar.com>
2020-04-22 11:26:50 +0800

    添加根据URl获取用户id方法

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -3,12 +3,7 @@
 namespace app\admin\controller;
 
 use app\common\controller\Backend;
-use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
-use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
-use PhpOffice\PhpSpreadsheet\Reader\Xls;
-use PhpOffice\PhpSpreadsheet\Reader\Csv;
-
-use think\Exception;
+use app\Common\model\Auth;
 
 class Test extends Backend
 {
@@ -24,113 +19,8 @@ class Test extends Backend
     
     public function test()
     {
-        
+        $uid = Auth::getUsersId('purchase/purchase_order/index');
+        dump($uid);die;
     }
 
-    /**
-     * 批量导入
-     */
-    public function import()
-    {
-        $file = $this->request->request('file');
-        if (!$file) {
-            $this->error(__('Parameter %s can not be empty', 'file'));
-        }
-        $filePath = ROOT_PATH . DS . 'public' . DS . $file;
-        if (!is_file($filePath)) {
-            $this->error(__('No results were found'));
-        }
-        //实例化reader
-        $ext = pathinfo($filePath, PATHINFO_EXTENSION);
-        if (!in_array($ext, ['csv', 'xls', 'xlsx'])) {
-            $this->error(__('Unknown data format'));
-        }
-        if ($ext === 'csv') {
-            $file = fopen($filePath, 'r');
-            $filePath = tempnam(sys_get_temp_dir(), 'import_csv');
-            $fp = fopen($filePath, "w");
-            $n = 0;
-            while ($line = fgets($file)) {
-                $line = rtrim($line, "\n\r\0");
-                $encoding = mb_detect_encoding($line, ['utf-8', 'gbk', 'latin1', 'big5']);
-                if ($encoding != 'utf-8') {
-                    $line = mb_convert_encoding($line, 'utf-8', $encoding);
-                }
-                if ($n == 0 || preg_match('/^".*"$/', $line)) {
-                    fwrite($fp, $line . "\n");
-                } else {
-                    fwrite($fp, '"' . str_replace(['"', ','], ['""', '","'], $line) . "\"\n");
-                }
-                $n++;
-            }
-            fclose($file) || fclose($fp);
-
-            $reader = new Csv();
-        } elseif ($ext === 'xls') {
-            $reader = new Xls();
-        } else {
-            $reader = new Xlsx();
-        }
-
-        //导入文件首行类型,默认是注释,如果需要使用字段名称请使用name
-        //$importHeadType = isset($this->importHeadType) ? $this->importHeadType : 'comment';
-        //模板文件列名
-        $listName = ['用户名', '昵称',  'email', '所属组别'];
-        try {
-            if (!$PHPExcel = $reader->load($filePath)) {
-                $this->error(__('Unknown data format'));
-            }
-            $currentSheet = $PHPExcel->getSheet(0);  //读取文件中的第一个工作表
-            $allColumn = $currentSheet->getHighestDataColumn(); //取得最大的列号
-            $allRow = $currentSheet->getHighestRow(); //取得一共有多少行
-            $maxColumnNumber = Coordinate::columnIndexFromString($allColumn);
-
-            $fields = [];
-            for ($currentRow = 1; $currentRow <= 1; $currentRow++) {
-                for ($currentColumn = 1; $currentColumn <= $maxColumnNumber; $currentColumn++) {
-                    $val = $currentSheet->getCellByColumnAndRow($currentColumn, $currentRow)->getValue();
-                    $fields[] = $val;
-                }
-            }
-
-            //模板文件不正确
-            if ($listName !== $fields) {
-                throw new Exception("模板文件不正确！！");
-            }
-
-            $data = [];
-            for ($currentRow = 2; $currentRow <= $allRow; $currentRow++) {
-                for ($currentColumn = 1; $currentColumn <= $maxColumnNumber; $currentColumn++) {
-                    $val = $currentSheet->getCellByColumnAndRow($currentColumn, $currentRow)->getValue();
-                    $data[$currentRow - 2][$currentColumn - 1] = is_null($val) ? '' : $val;
-                }
-            }
-        } catch (Exception $exception) {
-            $this->error($exception->getMessage());
-        }
-
-
-        foreach ($data as $k => $v) {
-            $params['username'] = $v[0];
-            $params['nickname'] = $v[1];
-            $params['email'] = $v[2];
-            $params['salt'] = Random::alnum();
-            $params['password'] = md5(md5($params['username']) . $params['salt']);
-            $params['avatar'] = '/assets/img/avatar.png'; //设置新管理员默认头像。
-            $result = $this->model->isUpdate(false)->data($params)->save($params);
-            if ($result === false) {
-                $this->error($this->model->getError());
-            }
-            $group = [$v[3]];
-
-            //过滤不允许的组别,避免越权
-            $group = array_intersect($this->childrenGroupIds, $group);
-            $dataset = [];
-            foreach ($group as $value) {
-                $dataset[] = ['uid' => $this->model->id, 'group_id' => $value];
-            }
-            model('AuthGroupAccess')->saveAll($dataset);
-        }
-        echo 'ok';
-    }
 }
