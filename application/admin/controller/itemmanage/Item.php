ac060d30c55de0214f54b01ff9330faa2e93faad
fanzhigang <itxiaofan123@gmail.com>
2020-04-22 10:36:34 +0800

    测试git库连接

--- application/admin/controller/itemmanage/Item.php
+++ application/admin/controller/itemmanage/Item.php
@@ -55,7 +55,7 @@ class Item extends Backend
     {
         //设置过滤方法
         $this->request->filter(['strip_tags']);
-        if ($this->request->isAjax()) {
+        if ($this->request->isAjax()) {// 判断是否为Ajax调用
             //如果发送的来源是Selectpage，则转发到Selectpage
             if ($this->request->request('keyField')) {
                 return $this->selectpage();
