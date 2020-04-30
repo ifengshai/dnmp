706a0303ce1937cdf000ba8e029825c249d396f5
wpl <wangpenglei@nextmar.com>
2020-04-30 09:42:33 +0800

    1

--- application/admin/controller/purchase/PurchaseOrder.php
+++ application/admin/controller/purchase/PurchaseOrder.php
@@ -856,11 +856,8 @@ class PurchaseOrder extends Backend
         for ($i = 1; $i <= round($success_data['totalRecord'] / 50); $i++) {
             //根据不同的状态取订单数据
             $data[$i] = Alibaba::getOrderList($i, $params)->result;
-            sleep(60);
         }
-        if (!$data) {
-            dump($success_data);die;
-        }
+       
         foreach ($data as $key => $val) {
             if (!$val) {
                 continue;
