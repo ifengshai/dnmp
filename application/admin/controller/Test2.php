cd5efcd6cfa2a14634dc10c3e506e8ce8ca1ba7f
haha <1461069578@qq.com>
2020-04-30 17:18:59 +0800

    tijiao

---
+++ application/admin/controller/Test2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\common\controller\Backend;
+use app\Common\model\Auth;
+class Test2 extends Backend{
+    public function haha(){
+        echo 1111;
+    }
+}
