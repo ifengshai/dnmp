a6bea7f21ee2c2c6c7e52254afe537b5e9e9df2b
wpl <wangpenglei@nextmar.com>
2020-04-30 14:47:14 +0800

    1

--- public/assets/js/backend/demand/develop_demand.js
+++ public/assets/js/backend/demand/develop_demand.js
@@ -486,7 +486,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                     if (value == null || value == undefined) {
                         value = '';
                     }
-                    return '<div style="float: left;width: 100%;"><span class="btn-getcontent check_demand_content" data = "' + value + '" style="">查 看</span></div>';
+                    return '<div style="float: left;width: 100%;"><span class="btn-getcontent check_demand_content">查 看</span></div>';
                 },
                 getClear: function (value) {
 
