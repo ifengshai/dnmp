198acfde267930f7a6274a3e41e69f580f3ed0f8
crasphb <646054215@qq.com>
2020-05-06 10:45:12 +0800

    回复post

--- application/admin/model/saleaftermanage/WorkOrderList.php
+++ application/admin/model/saleaftermanage/WorkOrderList.php
@@ -637,7 +637,7 @@ class WorkOrderList extends Model
             }
             $postData = array_merge($postData, $postDataCommon);
             try {
-                $res = $this->httpRequest($siteType, 'magic/order/createOrder', $postData);
+                $res = $this->httpRequest($siteType, 'magic/order/createOrder', $postData, 'POST');
                 $increment_id = $res['increment_id'];
                 //replacement_order添加补发的订单号
                 WorkOrderChangeSku::where(['work_id' => $work_id, 'change_type' => 5])->setField('replacement_order', $increment_id);
