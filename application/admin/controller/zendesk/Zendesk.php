3c416a4a5297321b28b44cde92e9f3420ca12631
crasphb <646054215@qq.com>
2020-05-08 18:23:32 +0800

    1

--- application/admin/controller/zendesk/Zendesk.php
+++ application/admin/controller/zendesk/Zendesk.php
@@ -850,7 +850,7 @@ DOC;
      */
     public function asycTickets()
     {
-        for($i=121920;$i<122543;$i++){
+        for($i=121927;$i<122543;$i++){
             (new Notice(request(), ['type' => 'zeelool','id' => $i]))->create();
         }
     }
