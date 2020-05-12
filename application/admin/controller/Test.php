3d84bdc3923bb3e3bc6786e6ed638417c6d6d72a
lixiang0401 <6225998@qq.com>
2020-05-12 16:13:16 +0800

    批量传数据

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -9,6 +9,7 @@ use SchGroup\SeventeenTrack\Connectors\TrackingConnector;
 
 class Test extends Backend
 {
+    protected $noNeedLogin = ['*'];
     protected $apiKey = 'F26A807B685D794C676FA3CC76567035';
 
     public function _initialize()
