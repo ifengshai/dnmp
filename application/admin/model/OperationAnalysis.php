00806325d1a0c381c8142af0fc6573b36f308086
haha <1461069578@qq.com>
2020-05-13 14:28:55 +0800

    tijiao

--- application/admin/model/OperationAnalysis.php
+++ application/admin/model/OperationAnalysis.php
@@ -44,7 +44,8 @@ class OperationAnalysis extends Model
             $model = Db::connect('database.db_nihao');
             break;
             case 4:
-            $model = Db::connect('database.db_meeloog');    
+            $model = Db::connect('database.db_meeloog');
+            break;    
             default:
             $model = false;
             break;            
