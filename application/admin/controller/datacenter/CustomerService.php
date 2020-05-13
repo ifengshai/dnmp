18c9b0723aecdb5f739cc8cccb070614acd20407
haha <1461069578@qq.com>
2020-05-13 13:53:53 +0800

    tijiao

--- application/admin/controller/datacenter/CustomerService.php
+++ application/admin/controller/datacenter/CustomerService.php
@@ -50,16 +50,32 @@ class CustomerService extends Backend
                 $time = explode(' ', $params['time']);
                 $map['complete_time'] = ['between', [$time[0] . ' ' . $time[1], $time[3] . ' ' . $time[4]]];
             } else {
-                $map['complete_time'] = ['between', [date('Y-m-d 00:00:00'), date('Y-m-d H:i:s', time())]];
+                $map['complete_time'] = ['between', [date('Y-m-d 00:00:00',strtotime('-30 day')), date('Y-m-d H:i:s', time())]];
             }
             $order_platform = $params['platform'];
-            // //问题大分类统计、措施统计
-            $data = $this->get_workorder_data($order_platform, $map);
-            // //问题类型统计
-            $problem_data = $this->get_problem_type_data($platform, $map,1);
             //问题措施比统计
-            $problem_step_data = $this->get_problem_step_data($platform,$map,1);
+            $problem_step_data = $this->get_problem_step_data($order_platform,$map,1);
+           // $data = $this->get_workorder_data($order_platform, $map);
             if('echart1' == $params['key']){
+                //问题大分类统计、措施统计
+                $data = $this->get_workorder_data($order_platform, $map);
+                $customer_problem_classify = config('workorder.customer_problem_classify');
+                $column = array_keys($customer_problem_classify);
+                $columnData = [];
+                foreach($column as $k =>$v){
+                    $columnData[$k]['name'] = $v;
+                    $columnData[$k]['value'] = $data['problem_type'][$k];
+                }
+                $json['column'] = $column;
+                $json['columnData'] = $columnData;
+                return json(['code' => 1, 'data' => $json]);
+            }elseif('echart2' == $params['key']){
+            //问题类型统计
+            $problem_data = $this->get_problem_type_data($order_platform, $map,1);
+            }elseif('echart3' == $params['key']){
+                //问题大分类统计、措施统计
+                $data = $this->get_workorder_data($order_platform, $map);
+                $step = config('workorder.customer_problem_classify');
                 $json['column'] = ['订单修改', '物流仓储', '产品质量', '客户问题'];
                 $json['columnData'] = [
                     [
@@ -80,6 +96,8 @@ class CustomerService extends Backend
                     ]
                 ];
                 return json(['code' => 1, 'data' => $json]);
+            }elseif('echart4' == $params['key']){
+
             }
             if (false == $data) {
                 return $this->error('没有对应的时间数据，请重新尝试');
@@ -111,9 +129,9 @@ class CustomerService extends Backend
         $problem_arr = config('workorder.customer_problem_classify_arr');
         //问题结果
         $result = [];
-        foreach($problem_arr as $k=> $v){
+        foreach($problem_arr as $v){
             //问题大分类的统计 
-            $result['problem_type'][$k] = $this->model->where($where)->where($map)->where('problem_type_id','in',$v)->count('id');           
+            $result['problem_type'][] = $this->model->where($where)->where($map)->where('problem_type_id','in',$v)->count('id');           
         }
         //所有完成的work_id
         $all_work_id = $this->model->where($where)->where($map)->column('id');
