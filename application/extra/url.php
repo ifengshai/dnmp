13f8ee0c9dcb5af06a631d17c545312a682828cb
haha <1461069578@qq.com>
2020-05-23 17:12:47 +0800

    tijiao

--- application/extra/url.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//站点URL配置
-return [
-    'zeelool_url'   => 'https://z.zhangkuangyi.com/',
-    'voogueme_url'  => 'https://pc.voogueme.com/',
-    'nihao_url'     => 'https://www.nihaooptical.com/',
-    'wesee_url'     => 'https://www.weseeoptical.com/',
-];
