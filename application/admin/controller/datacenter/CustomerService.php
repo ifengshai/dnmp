a4136750847d0c217f7cec8c02c6e30b3dfaf5aa
haha <1461069578@qq.com>
2020-05-28 10:26:02 +0800

    tijiao

--- application/admin/controller/datacenter/CustomerService.php
+++ application/admin/controller/datacenter/CustomerService.php
@@ -715,6 +715,7 @@ class CustomerService extends Backend
                     'end'       => $end,
                     'platform'  => $platform
                     ]);
+                $this->view->assign(compact('workOrderNum', 'totalOrderMoney', 'replacementNum', 'refundMoneyNum', 'refundMoney'));    
             } elseif ($worklistOne && $worklistTwo) { //两个提交的数据
                 //取出总数
                 $workOrderNum       = $worklistOne['workOrderNum'] + $worklistTwo['workOrderNum'];
@@ -775,10 +776,10 @@ class CustomerService extends Backend
                      'info'         => $info,
                      'workArr'      => $workArr
                      ]);
+                $this->view->assign(compact('workOrderNum', 'totalOrderMoney', 'replacementNum', 'refundMoneyNum', 'refundMoney'));     
             }
-            
             $orderPlatformList = config('workorder.platform');
-            $this->view->assign(compact('orderPlatformList', 'workOrderNum', 'totalOrderMoney', 'replacementNum', 'refundMoneyNum', 'refundMoney'));
+            $this->view->assign(compact('orderPlatformList'));
         } else {
             //默认显示
             //根据筛选时间求出客服部门下面所有有数据人员
