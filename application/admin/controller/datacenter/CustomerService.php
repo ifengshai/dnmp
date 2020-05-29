f18bab800f92403613e85432f37d944d1d4e4957
haha <1461069578@qq.com>
2020-05-29 14:25:03 +0800

    tijiao

--- application/admin/controller/datacenter/CustomerService.php
+++ application/admin/controller/datacenter/CustomerService.php
@@ -382,7 +382,7 @@ class CustomerService extends Backend
         //$where['is_check'] = 1;
         $where['work_type'] = 1;
         //where('id','not in',[1,5,8]);
-        $where['work_status'] = ['in',[3,6]];
+        $where['work_status'] = ['in',[3,5,6]];
         //$where['work_status'] = ['neq',7];
         //求出措施的超时时间
         $time_out = config('workorder.step_time_out');
@@ -447,7 +447,7 @@ class CustomerService extends Backend
     public function warehouse_handle($map, $warehouse_problem_type)
     {
         $where['work_type'] = 2;
-        $where['work_status'] = ['in',[3,6]];
+        $where['work_status'] = ['in',[3,5,6]];
         //$where['work_status'] = ['lt',2];
         //求出主管的超时时间
         $time_out = config('workorder.warehouse_time_out');
