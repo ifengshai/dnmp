5724cfda429d784a3224c3f59f86831966d27f41
lixiang0401 <6225998@qq.com>
2020-06-01 14:05:39 +0800

    修改编辑权限

--- public/assets/js/backend/demand/it_web_demand.js
+++ public/assets/js/backend/demand/it_web_demand.js
@@ -438,13 +438,20 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                                     callback: function (data) {
                                     },
                                     visible: function(row){
-                                        if(row.status == 1 || row.status == 2){
-                                            if(row.demand_del && row.is_entry_user_hidden == 1){//操作权限
+                                        if(row.status == 4){
+                                            if(row.demand_del){//操作权限
                                                 return true;
                                             }
                                         }else{
                                             return false;
                                         }
+                                        /* if(row.status == 1 || row.status == 2){
+                                            if(row.demand_del && row.is_entry_user_hidden == 1){//操作权限
+                                                return true;
+                                            }
+                                        }else{
+                                            return false;
+                                        } */
                                     }
                                 },
                                 {
