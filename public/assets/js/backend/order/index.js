8a87d4cc1f3d2565ac53ecde72429f6c5065b089
wpl <wangpenglei@nextmar.com>
2020-06-03 09:50:42 +0800

    添加订单列表

--- public/assets/js/backend/order/index.js
+++ public/assets/js/backend/order/index.js
@@ -76,6 +76,21 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'bootstrap-table-jump
                 window.open(Config.moduleurl + '/order/index/batch_print_label_new?id_params=' + id_params + '&label=' + Config.label, '_blank');
             });
 
+            //批量导出xls 
+            $('.btn-batch-export-xls').click(function () {
+                var ids = Table.api.selectedids(table);
+                if (ids.length > 0) {
+                    window.open(Config.moduleurl + '/order/index/batch_export_xls?ids=' + ids, '_blank');
+                } else {
+                    var options = table.bootstrapTable('getOptions');
+                    var search = options.queryParams({});
+                    var filter = search.filter;
+                    var op = search.op;
+                    window.open(Config.moduleurl + '/order/index/batch_export_xls?filter=' + filter + '&op=' + op, '_blank');
+                }
+                
+            });
+
         },
         add: function () {
             Controller.api.bindevent();
