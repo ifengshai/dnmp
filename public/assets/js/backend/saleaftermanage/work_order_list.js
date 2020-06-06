29fffaed33e9bdc29e3436cb4511a11da3ffe258
haha <1461069578@qq.com>
2020-06-06 11:05:15 +0800

    tijiao

--- public/assets/js/backend/saleaftermanage/work_order_list.js
+++ public/assets/js/backend/saleaftermanage/work_order_list.js
@@ -1384,6 +1384,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'jqui', 'form'], function ($,
                     var prescription = prescriptions_add_edit[val];
 
                     var prescription_div = $(this).parents('.step7_function2').next('.step1_function3');
+                    console.log(prescription);
                     prescription_div.find('input').val('');
                     prescription_div.find('input[name="row[replacement][od_sph][]"]').val(prescription.od_sph);
                     prescription_div.find('input[name="row[replacement][os_sph][]"]').val(prescription.os_sph);
