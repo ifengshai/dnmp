0876d04f768da645ff25e732646f8c706858def7
haha <1461069578@qq.com>
2020-06-06 11:28:51 +0800

    tijiao

--- public/assets/js/backend/saleaftermanage/work_order_list.js
+++ public/assets/js/backend/saleaftermanage/work_order_list.js
@@ -888,7 +888,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'jqui', 'form'], function ($,
                     prescription_div.find('input[name="row[replacement][od_add][]"]').val(prescription.od_add);
                     prescription_div.find('input[name="row[replacement][os_add][]"]').val(prescription.os_add);
                 }
-                if (typeof(prescription.pd) == undefined || (prescription.pd != '')) {
+                if (typeof(prescription.pd) != undefined && (prescription.pd != '')) {
                     prescription_div.find('input[name="row[replacement][pd_r][]"]').val(prescription.pd);
                     //prescription_div.find('input[name="row[replacement][pd_l][]"]').attr('disabled',true);
                 }else{
@@ -1413,7 +1413,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'jqui', 'form'], function ($,
                         prescription_div.find('input[name="row[replacement][od_add][]"]').val(prescription.od_add);
                         prescription_div.find('input[name="row[replacement][os_add][]"]').val(prescription.os_add);
                     }
-                    if (typeof(prescription.pd) == undefined || prescription.pd != '') {
+                    if (typeof(prescription.pd) != undefined && prescription.pd != '') {
                         prescription_div.find('input[name="row[replacement][pd_r][]"]').val(prescription.pd);
                         //prescription_div.find('input[name="row[replacement][pd_l][]"]').attr('disabled',true);
                     } else {
