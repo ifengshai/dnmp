8b854f3133c153e03f15c1be89c414efa03e9d4f
wpl <wangpenglei@nextmar.com>
2020-06-08 16:02:16 +0800

    修改补差价链接

--- application/admin/controller/saleaftermanage/WorkOrderList.php
+++ application/admin/controller/saleaftermanage/WorkOrderList.php
@@ -1487,7 +1487,7 @@ class WorkOrderList extends Backend
         if ($row['work_platform'] == 1 && $row['replenish_money']) {
             $url = config('url.zeelool_url') . 'ios/activity/price_difference?customer_email=' . $row['email'] . '&origin_order_number=' . $row['platform_order'] . '&order_amount=' .$row['replenish_money'] . '&currency=' . $row['order_pay_currency'] . '&order_rate=' . $row['base_to_order_rate'];
         } elseif ($row['work_platform'] == 2 && $row['replenish_money']) {
-            $url = config('url.new_voogueme_url') . 'price-difference?20&customer_email=' . $row['email'] . '&origin_order_number=' . $row['platform_order'] . '&order_amount=' . $row['replenish_money'] . '&currency=' . $row['order_pay_currency'] . '&order_rate=' . $row['base_to_order_rate'];
+            $url = config('url.new_voogueme_url') . 'price-difference?customer_email=' . $row['email'] . '&origin_order_number=' . $row['platform_order'] . '&order_amount=' . $row['replenish_money'] . '&currency=' . $row['order_pay_currency'] . '&order_rate=' . $row['base_to_order_rate'];
         } elseif ($row['work_platform'] == 3 && $row['replenish_money']) {
             $url = config('url.nihao_url') . 'common/Differenceprice/difference_price?customer_email=' . $row['email'] . '&origin_order_number=' . $row['platform_order'] . '&order_amount=' . $row['replenish_money'] . '&currency=' . $row['order_pay_currency'] . '&order_rate=' . $row['base_to_order_rate'];
         }
