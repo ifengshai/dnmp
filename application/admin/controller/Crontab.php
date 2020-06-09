f92ac60f54777dfcfa014ee5294ddb0762dad297
wpl <wangpenglei@nextmar.com>
2020-06-09 14:50:20 +0800

    添加批发站定时任务

--- application/admin/controller/Crontab.php
+++ application/admin/controller/Crontab.php
@@ -31,120 +31,6 @@ class Crontab extends Backend
 
     protected $order_status =  "and status in ('processing','complete','creditcard_proccessing','free_processing')";
 
-     /**
-     * 定时处理 订单列表分类
-     * 1：仅镜架
-     * 2：仅现货处方镜
-     * 3：仅定制处方镜
-     * 4：镜架+现货
-     * 5：镜架+定制
-     * 6：现片+定制片
-     */
-    public function zeelool_order_custom_order_temp()
-    {
-        $order_entity_id_querySql = "select sfo.entity_id from sales_flat_order sfo where sfo.custom_order_prescription_type =1 and created_at between '2020-05-29 00:00:00' and '2020-05-30 23:00:00' order by entity_id desc limit 1000";
-        $order_entity_id_list = Db::connect('database.db_zeelool')->query($order_entity_id_querySql);
-        if (empty($order_entity_id_list)) {
-            echo '处理完毕！';
-            exit;
-        }
-
-        /**
-         * 1：仅镜架
-         * 2：仅现货处方镜
-         * 3：仅定制处方镜
-         * 4：镜架+现货
-         * 5：镜架+定制
-         * 6：现片+定制片
-         */
-        $type_1_entity_id = [];
-        $type_2_entity_id = [];
-        $type_3_entity_id = [];
-        $type_4_entity_id = [];
-        $type_5_entity_id = [];
-        $type_6_entity_id = [];
-        foreach ($order_entity_id_list as $key => $value) {
-            $items = Db::connect('database.db_zeelool')->table('sales_flat_order_item_prescription')->where('order_id=' . $value['entity_id'])->select();
-            if (!$items) {
-                continue;
-            }
-            $label = [];
-            foreach ($items as $k => $v) {
-                //如果镜片参数为真 或 不等于 Plastic Lenses 并且不等于 FRAME ONLY则此订单为含处方
-                if ($v['index_type'] == '' || $v['index_type'] == 'Plastic Lenses' || $v['index_type'] == 'FRAME ONLY' || $v['index_type'] == 'Frame Only' || $v['index_type'] == 'Frameonly' || ($v['index_type'] == 'Sunglasses Non Prescription' && !$v['options_color'])) {
-                    $label[] = 1; //仅镜架
-                } elseif (($v['index_type'] && $v['index_type'] != 'Plastic Lenses' && $v['index_type'] != 'FRAME ONLY' && $v['index_type'] != 'Frame Only' && $v['index_type'] != 'Frameonly' || ($v['index_type'] == 'Sunglasses Non Prescription' && $v['options_color'])) && $v['is_custom_lens'] == 0) {
-                    $label[] = 2; //现片含处方
-                } elseif (($v['index_type'] && $v['index_type'] != 'Plastic Lenses' && $v['index_type'] != 'FRAME ONLY' && $v['index_type'] != 'Frame Only' && $v['index_type'] != 'Frameonly' || ($v['index_type'] == 'Sunglasses Non Prescription' && $v['options_color'])) && $v['is_custom_lens'] == 1) {
-                    $label[] = 3; //定制含处方
-                }
-            }
-           
-            //如果订单包括 仅镜架和现货处方镜 类型则为 镜架 + 现货
-            if (in_array(1, $label) && in_array(2, $label) && !in_array(3, $label)) {
-                $type_4_entity_id[] = $value['entity_id']; //镜架 + 现货
-
-                //如果订单包括 仅镜架和定制处方镜 类型则为 镜架 + 定制
-            } elseif (in_array(1, $label) && in_array(3, $label) && !in_array(2, $label)) {
-                $type_5_entity_id[] = $value['entity_id']; //镜架 + 定制
-
-                //如果订单只有 仅镜架 类型则为 仅镜架
-            } elseif (in_array(1, $label) && !in_array(3, $label) && !in_array(2, $label)) {
-                $type_1_entity_id[] = $value['entity_id']; //仅镜架
-
-                //如果订单只有 现货 类型则为 现货处方镜
-            } elseif (!in_array(1, $label) && !in_array(3, $label) && in_array(2, $label)) {
-                $type_2_entity_id[] = $value['entity_id']; //仅现货处方镜
-
-                //如果订单只有 定制 类型则为 仅定制处方镜
-            } elseif (!in_array(1, $label) && in_array(3, $label) && !in_array(2, $label)) {
-                $type_3_entity_id[] = $value['entity_id']; //仅定制处方镜
-            } elseif (in_array(2, $label) && in_array(3, $label)) {
-                $type_6_entity_id[] = $value['entity_id']; //现片+定制片
-            } else {
-                $type_1_entity_id[] = $value['entity_id']; //仅镜架
-            }
-        }
-
-
-        if ($type_1_entity_id) {
-            $map['entity_id'] = ['in', $type_1_entity_id];
-            Db::connect('database.db_zeelool')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 1]);
-        }
-
-        if ($type_2_entity_id) {
-            $map['entity_id'] = ['in', $type_2_entity_id];
-            Db::connect('database.db_zeelool')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 2]);
-        }
-
-        if ($type_3_entity_id) {
-            $map['entity_id'] = ['in', $type_3_entity_id];
-            Db::connect('database.db_zeelool')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 3]);
-        }
-
-
-        if ($type_4_entity_id) {
-            $map['entity_id'] = ['in', $type_4_entity_id];
-            Db::connect('database.db_zeelool')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 4]);
-        }
-
-
-        if ($type_5_entity_id) {
-            $map['entity_id'] = ['in', $type_5_entity_id];
-            Db::connect('database.db_zeelool')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 5]);
-        }
-
-
-        if ($type_6_entity_id) {
-            $map['entity_id'] = ['in', $type_6_entity_id];
-            Db::connect('database.db_zeelool')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 6]);
-        }
-
-        echo "执行成功！！";
-    }
-
-
-
 
     /**
      * 定时处理 订单列表分类
@@ -1647,6 +1533,208 @@ order by sfoi.item_id asc limit 1000";
                 $wherePrescription['order_id'] = ['in', $orderArr];
                 Db::connect('database.db_meeloog')->table('sales_flat_order_item_prescription')->where($wherePrescription)->update(['frame_type_is_rimless' => 2]);
             }
+        } else {
+            echo '执行完毕！';
+        }
+    }
+
+
+    /**
+     * 定时处理 订单列表分类 meeloog站点
+     * 1：仅镜架
+     * 2：仅现货处方镜
+     * 3：仅定制处方镜
+     * 4：镜架+现货
+     * 5：镜架+定制
+     * 6：现片+定制片
+     */
+    public function wesee_order_custom_order_prescription()
+    {
+        $order_entity_id_querySql = "select sfo.entity_id from sales_flat_order sfo where sfo.custom_order_prescription_type is null order by entity_id desc limit 1000 ";
+        $order_entity_id_list = Db::connect('database.db_meeloog')->query($order_entity_id_querySql);
+        if (empty($order_entity_id_list)) {
+            echo '处理完毕！';
+            exit;
+        }
+
+        /**
+         * 1：仅镜架
+         * 2：仅现货处方镜
+         * 3：仅定制处方镜
+         * 4：镜架+现货
+         * 5：镜架+定制
+         * 6：现片+定制片
+         */
+        $type_1_entity_id = [];
+        $type_2_entity_id = [];
+        $type_3_entity_id = [];
+        $type_4_entity_id = [];
+        $type_5_entity_id = [];
+        $type_6_entity_id = [];
+        foreach ($order_entity_id_list as $key => $value) {
+            $items = Db::connect('database.db_meeloog')->table('sales_flat_order_item_prescription')->where('order_id=' . $value['entity_id'])->select();
+            if (!$items) {
+                continue;
+            }
+
+            $label = [];
+            foreach ($items as $k => $v) {
+                //如果镜片参数为真 或 不等于 Plastic Lenses 并且不等于 FRAME ONLY则此订单为含处方
+                if ($v['index_type'] == '' || $v['index_type'] == 'Plastic Lenses' || $v['index_type'] == 'FRAME ONLY' || $v['index_type'] == 'FRAME ONLY (Plastic lenses)') {
+                    $label[] = 1; //仅镜架
+                } elseif (($v['index_type'] && $v['index_type'] != 'Plastic Lenses' && $v['index_type'] != 'FRAME ONLY' && $v['index_type'] != 'FRAME ONLY (Plastic lenses)') && $v['is_custom_lens'] == 0) {
+                    $label[] = 2; //现片含处方
+                } elseif (($v['index_type'] && $v['index_type'] != 'Plastic Lenses' && $v['index_type'] != 'FRAME ONLY' && $v['index_type'] != 'FRAME ONLY (Plastic lenses)') && $v['is_custom_lens'] == 1) {
+                    $label[] = 3; //定制含处方
+                }
+            }
+
+            //如果订单包括 仅镜架和现货处方镜 类型则为 镜架 + 现货
+            if (in_array(1, $label) && in_array(2, $label) && !in_array(3, $label)) {
+                $type_4_entity_id[] = $value['entity_id']; //镜架 + 现货
+
+                //如果订单包括 仅镜架和定制处方镜 类型则为 镜架 + 定制
+            } elseif (in_array(1, $label) && in_array(3, $label) && !in_array(2, $label)) {
+                $type_5_entity_id[] = $value['entity_id']; //镜架 + 定制
+
+                //如果订单只有 仅镜架 类型则为 仅镜架
+            } elseif (in_array(1, $label) && !in_array(3, $label) && !in_array(2, $label)) {
+                $type_1_entity_id[] = $value['entity_id']; //仅镜架
+
+                //如果订单只有 现货 类型则为 现货处方镜
+            } elseif (!in_array(1, $label) && !in_array(3, $label) && in_array(2, $label)) {
+                $type_2_entity_id[] = $value['entity_id']; //仅现货处方镜
+
+                //如果订单只有 定制 类型则为 仅定制处方镜
+            } elseif (!in_array(1, $label) && in_array(3, $label) && !in_array(2, $label)) {
+                $type_3_entity_id[] = $value['entity_id']; //仅定制处方镜
+            } elseif (in_array(2, $label) && in_array(3, $label)) {
+                $type_6_entity_id[] = $value['entity_id']; //现片+定制片
+            } else {
+                $type_1_entity_id[] = $value['entity_id']; //仅镜架
+            }
+        }
+
+        if ($type_1_entity_id) {
+            $map['entity_id'] = ['in', $type_1_entity_id];
+            Db::connect('database.db_meeloog')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 1]);
+        }
+
+        if ($type_2_entity_id) {
+            $map['entity_id'] = ['in', $type_2_entity_id];
+            Db::connect('database.db_meeloog')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 2]);
+        }
+
+        if ($type_3_entity_id) {
+            $map['entity_id'] = ['in', $type_3_entity_id];
+            Db::connect('database.db_meeloog')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 3]);
+        }
+
+
+        if ($type_4_entity_id) {
+            $map['entity_id'] = ['in', $type_4_entity_id];
+            Db::connect('database.db_meeloog')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 4]);
+        }
+
+
+        if ($type_5_entity_id) {
+            $map['entity_id'] = ['in', $type_5_entity_id];
+            Db::connect('database.db_meeloog')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 5]);
+        }
+
+
+        if ($type_6_entity_id) {
+            $map['entity_id'] = ['in', $type_6_entity_id];
+            Db::connect('database.db_meeloog')->table('sales_flat_order')->where($map)->update(['custom_order_prescription_type' => 6]);
+        }
+
+        echo "执行成功！！";
+    }
+
+    /**
+     * 定时处理订单处方表序列化数据
+     */
+    public function wesee_order_item_process()
+    {
+        $max_item_id_querySql = "select max(boi.item_id) max_item_id from sales_flat_order_item_prescription boi";
+        $max_item_id_list = Db::connect('database.db_weseeoptical')->query($max_item_id_querySql);
+        if ($max_item_id_list) {
+            $max_item_id = $max_item_id_list[0]['max_item_id'];
+        }
+
+        $max_item_id = $max_item_id > 0 ? $max_item_id : 0;
+        $order_item_prescription_querySql = "select sfoi.item_id,sfoi.order_id,sfoi.product_id,sfoi.`name`,sfoi.sku,sfoi.product_options,sfoi.created_at,sfoi.qty_ordered,sfoi.quote_item_id
+from sales_flat_order_item sfoi where sfoi.item_id > $max_item_id
+order by sfoi.item_id asc limit 1000";
+        $order_item_list = Db::connect('database.db_weseeoptical')->query($order_item_prescription_querySql);
+        foreach ($order_item_list as $order_item_key => $order_item_value) {
+            $product_options = unserialize($order_item_value['product_options']);
+
+            $final_params['coatiing_name'] = substr($product_options['info_buyRequest']['tmplens']['coatiing_name'], 0, 100);
+            $final_params['index_type'] = substr($product_options['info_buyRequest']['tmplens']['index_type'], 0, 100);
+
+            $final_params['frame_price'] = $product_options['info_buyRequest']['tmplens']['frame_price'];
+            $final_params['index_price'] = $product_options['info_buyRequest']['tmplens']['index_price'];
+            $final_params['coatiing_price'] = $product_options['info_buyRequest']['tmplens']['coatiing_price'];
+
+            $items[$order_item_key]['frame_regural_price'] = $final_params['frame_regural_price'] = $product_options['info_buyRequest']['tmplens']['frame_regural_price'];
+            $items[$order_item_key]['is_special_price'] = $final_params['is_special_price'] = $product_options['info_buyRequest']['tmplens']['is_special_price'];
+            $items[$order_item_key]['index_price_old'] = $final_params['index_price_old'] = $product_options['info_buyRequest']['tmplens']['index_price_old'];
+            $items[$order_item_key]['index_name'] = $final_params['index_name'] = $product_options['info_buyRequest']['tmplens']['index_name'];
+            $items[$order_item_key]['index_id'] = $final_params['index_id'] = $product_options['info_buyRequest']['tmplens']['index_id'];
+            $items[$order_item_key]['lens'] = $final_params['lens'] = $product_options['info_buyRequest']['tmplens']['lens'];
+            $items[$order_item_key]['lens_old'] = $final_params['lens_old'] = $product_options['info_buyRequest']['tmplens']['lens_old'];
+            $items[$order_item_key]['total'] = $final_params['total'] = $product_options['info_buyRequest']['tmplens']['total'];
+            $items[$order_item_key]['total_old'] = $final_params['total_old'] = $product_options['info_buyRequest']['tmplens']['total_old'];
+            $items[$order_item_key]['sph_degree'] = $final_params['degrees'] = $product_options['info_buyRequest']['tmplens']['degrees'];
+
+            $items[$order_item_key]['order_id'] = $order_item_value['order_id'];
+            $items[$order_item_key]['item_id'] = $order_item_value['item_id'];
+            $items[$order_item_key]['product_id'] = $order_item_value['product_id'];
+            $items[$order_item_key]['name'] = $order_item_value['name'];
+            $items[$order_item_key]['sku'] = $order_item_value['sku'];
+            $items[$order_item_key]['created_at'] = $order_item_value['created_at'];
+            $items[$order_item_key]['qty_ordered'] = $order_item_value['qty_ordered'];
+            $items[$order_item_key]['quote_item_id'] = $order_item_value['quote_item_id'];
+
+            $items[$order_item_key]['coatiing_name'] = $final_params['coatiing_name'];
+            $items[$order_item_key]['index_type'] = $final_params['index_type'];
+            $items[$order_item_key]['prescription_type'] = $final_params['prescription_type'];
+
+            $items[$order_item_key]['frame_price'] = $final_params['frame_price'] ? $final_params['frame_price'] : 0;
+            $items[$order_item_key]['index_price'] = $final_params['index_price'] ? $final_params['index_price'] : 0;
+            $items[$order_item_key]['coatiing_price'] = $final_params['coatiing_price'] ? $final_params['coatiing_price'] : 0;
+
+
+            /**
+             * 判断定制现片逻辑
+             * 1、渐进镜 Progressive
+             * 2、偏光镜 镜片类型包含Polarized
+             * 3、染色镜 镜片类型包含Lens with Color Tint
+             * 4、当cyl<=-4或cyl>=4
+             */
+         
+
+            if (strpos($final_params['index_type'], 'Lens with Color Tint') !== false) {
+                $items[$order_item_key]['is_custom_lens'] = 1;
+            }
+
+            if ($final_params['degrees']) {
+                $items[$order_item_key]['is_custom_lens'] = 1;
+            }
+
+            unset($final_params);
+            unset($prescription_params);
+            unset($product_options);
+        }
+        if ($items) {
+            $result = Db::connect('database.db_weseeoptical')->table('sales_flat_order_item_prescription')->insertAll($items);
+            if ($result) {
+                echo '<br>执行成功';
+            } else {
+                echo '<br>执行失败';
+            }
+          
         } else {
             echo '执行完毕！';
         }
