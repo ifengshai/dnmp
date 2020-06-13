e34da55f4b26bd1303dd45c5533dc97607778b89
1461069578@qq.com <1461069578@qq.com>
2020-06-14 00:47:09 +0800

    tijiao

--- application/admin/controller/logistics/LogisticsStatistic.php
+++ application/admin/controller/logistics/LogisticsStatistic.php
@@ -55,7 +55,7 @@ class LogisticsStatistic extends Backend
             unset($result['deliverd_order_num_all']);
             unset($result['rate']);
             //所有的物流渠道
-            $column = $this->model->distinct(true)->field('shipment_type')->whereNotIn('shipment_type', ['', 'CPC', 'EYB'])->column('shipment_type');
+            $column = $this->orderNode->distinct(true)->field('shipment_type')->whereNotIn('shipment_type', ['', 'CPC', 'EYB'])->column('shipment_type');
             if ('echart1' == $params['key']) {
                 //妥投订单数
                 foreach ($column as $k => $v) {
