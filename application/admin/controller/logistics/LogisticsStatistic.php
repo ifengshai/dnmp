e22185e8e34836493a9c4057ac4657d5a8ff1b7f
lei121 <15978896108@163.com>
2020-06-13 18:30:26 +0800

    修改

--- application/admin/controller/logistics/LogisticsStatistic.php
+++ application/admin/controller/logistics/LogisticsStatistic.php
@@ -44,7 +44,7 @@ class LogisticsStatistic extends Backend
                 $timeOne = explode(' ', $params['time']);
                 $map['create_time'] = ['between', [$timeOne[0] . ' ' . $timeOne[1], $timeOne[3] . ' ' . $timeOne[4]]];
             } else {
-                $map['create_time'] = ['between', [date('Y-m-d 00:00:00', strtotime('-30 day')), date('Y-m-d H:i:s', time())]];
+                $map['create_time'] = ['between', [date('Y-m-d 00:00:00', strtotime('-10 day')), date('Y-m-d H:i:s', time())]];
             }
             $site = $params['platform'] ?: 10;
             $result = $this->logistics_data($site, $map);
