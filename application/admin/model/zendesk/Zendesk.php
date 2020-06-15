5cee516d9f6ed1e3b918bf1c73baf1c1bcc87137
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-06-15 18:05:31 +0800

    1

--- application/admin/model/zendesk/Zendesk.php
+++ application/admin/model/zendesk/Zendesk.php
@@ -250,9 +250,10 @@ class Zendesk extends Model
             //创建所有的tasks
             //获取所有的agents
             $agents = Db::name('zendesk_agents')->alias('z')->join(['fa_admin'=>'a'],'z.admin_id=a.id')->field('z.*,a.userid')->select();
+            dump($agents);exit;
             //查询该用户今天是否休息
             $userlist_arr = array_filter(array_column($agents,'userid'));
-            dump($userlist_arr);exit;
+            
             $userlist_str = implode(',',$userlist_arr);
             $time = strtotime(date('Y-m-d 0:0:0',time()));
             $ding = new \app\api\controller\Ding;
