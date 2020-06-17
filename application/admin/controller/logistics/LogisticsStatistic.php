d7f54b8f18d2fc1031c865053069899298daabac
haha <1461069578@qq.com>
2020-06-17 13:55:59 +0800

    tijiao

--- application/admin/controller/logistics/LogisticsStatistic.php
+++ application/admin/controller/logistics/LogisticsStatistic.php
@@ -60,7 +60,8 @@ class LogisticsStatistic extends Backend
             unset($result['deliverd_order_num_all']);
             unset($result['rate']);
             //所有的物流渠道
-            $column = $this->orderNode->distinct(true)->where($whereSite)->field('shipment_data_type')->whereNotIn('shipment_data_type', ['', 'CPC', 'EYB','China Post','CHINA_EMS','USPS_3'])->column('shipment_data_type');
+            //$column = $this->orderNode->distinct(true)->where($whereSite)->field('shipment_data_type')->whereNotIn('shipment_data_type', ['', 'CPC', 'EYB','China Post','CHINA_EMS','USPS_3'])->column('shipment_data_type');
+            $column = $this->orderNode->distinct(true)->where($whereSite)->field('shipment_data_type')->column('shipment_data_type');
             if ('echart1' == $params['key']) {
                 //妥投订单数
                 foreach ($column as $k => $v) {
@@ -492,8 +493,10 @@ class LogisticsStatistic extends Backend
         $fourteen_time_out = config('logistics.delievered_time_out')['fourteen'];
         //20天妥投时间
         $twenty_time_out = config('logistics.delievered_time_out')['twenty'];
-        $orderNode['order_node'] = ['egt', 3];
-        $all_shipment_type = $this->orderNode->where($whereSite)->distinct(true)->field('shipment_data_type')->whereNotIn('shipment_data_type', ['', 'CPC', 'EYB','China Post','CHINA_EMS','USPS_3'])->select();
+        //$orderNode['order_node'] = ['egt', 3];
+        $orderNode['node_type'] = ['egt', 7];
+        //$all_shipment_type = $this->orderNode->where($whereSite)->distinct(true)->field('shipment_data_type')->whereNotIn('shipment_data_type', ['', 'CPC', 'EYB','China Post','CHINA_EMS','USPS_3'])->select();
+        $all_shipment_type = $this->orderNode->where($whereSite)->distinct(true)->field('shipment_data_type')->select();
         if ($all_shipment_type) {
             $arr = $rs = $rate = [];
             //$rate['serven'] = $rate['fourteen'] = $rate['twenty'] = $rate['gtTwenty'] = 0;
