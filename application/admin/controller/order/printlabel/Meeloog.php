409cc894dfd679d7992a77223640ae3641a1dfd6
wpl <wangpenglei@nextmar.com>
2020-06-18 14:29:07 +0800

    忽略权限

--- application/admin/controller/order/printlabel/Meeloog.php
+++ application/admin/controller/order/printlabel/Meeloog.php
@@ -29,6 +29,13 @@ class Meeloog extends Backend
      */
     protected $model = null;
 
+    /**
+     * 无需鉴权的方法,但需要登录
+     * @var array
+     */
+    protected $noNeedRight = ['*'];
+
+
     protected $searchFields = 'entity_id';
 
     public function _initialize()
