4db385db83b29ce2fee220c08e4427b6862d472a
jianghaohui <jianghaohui@nextmar.com>
2020-06-19 10:30:14 +0800

    工单问题类型管理

---
+++ application/admin/model/saleaftermanage/Workorderconfig.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\admin\model\saleaftermanage;
+
+use think\Model;
+
+
+class Workorderconfig extends Model
+{
+
+    
+
+    
+
+    // 表名
+    protected $name = 'work_order_problem_type';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = false;
+
+    // 定义时间戳字段名
+    protected $createTime = false;
+    protected $updateTime = false;
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [
+
+    ];
+    
+
+    
+
+
+
+
+
+
+
+}
