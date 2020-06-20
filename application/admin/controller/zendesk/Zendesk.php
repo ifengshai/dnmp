3edff8d76f8f5ee153d83324443b0fa97bb54769
wpl <wangpenglei@nextmar.com>
2020-06-20 15:00:43 +0800

    1

--- application/admin/controller/zendesk/Zendesk.php
+++ application/admin/controller/zendesk/Zendesk.php
@@ -25,7 +25,7 @@ class Zendesk extends Backend
 {
     protected $model = null;
     protected $relationSearch = true;
-    protected $noNeedLogin = ['asycTicketsUpdate','asycTicketsVooguemeUpdate','asycTicketsAll','asycTicketsAll2','asycTicketsAll3','asyncTicketHttps'];
+    protected $noNeedLogin = ['*'];
 
     public function _initialize()
     {
