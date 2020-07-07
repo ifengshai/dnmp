9739d5db788d08219df6f4aed1882baae979b58f
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-07-07 18:12:24 +0800

    修改地址

--- public/assets/js/backend/saleaftermanage/work_order_list.js
+++ public/assets/js/backend/saleaftermanage/work_order_list.js
@@ -576,7 +576,10 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'jqui', 'form'], function ($,
                         //是否自动审核完成  end
                         //修改地址
                         if(id == 13){
-                            $("#user_address").show();
+                            $(".step7").show()
+                            $(".step7 .step-container div").hide()
+                            $(".step7 #user_address").show();
+                            $(".step7 #user_address div:not(.dropdown-menu)").show();
                             $("#label_name").html('修改地址');
                             $("#toolbar1").hide();
                             var site_type = $('#work_platform').val();
@@ -1485,7 +1488,10 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'jqui', 'form'], function ($,
                         }                        
                         //编辑页面的修改地址
                         if(id == 13){
-                            $("#user_address").show();
+                            $(".step7").show()
+                            $(".step7 .step-container div").hide()
+                            $(".step7 #user_address").show();
+                            $(".step7 #user_address div:not(.dropdown-menu)").show();
                             $("#label_name").html('修改地址');
                             $("#toolbar1").hide();
                             var site_type = $('#work_platform').val();
