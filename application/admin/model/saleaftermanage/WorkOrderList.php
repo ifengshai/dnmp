e824643126d360a60b200640dd2d9ff5bb7e0a53
13073732573 <1461069578@qq.com>
2020-07-08 11:28:42 +0800

    tijiao

--- application/admin/model/saleaftermanage/WorkOrderList.php
+++ application/admin/model/saleaftermanage/WorkOrderList.php
@@ -408,7 +408,8 @@ class WorkOrderList extends Model
                     $coatingId = $changeLens['coating_type'][$key];
 
                     $lensCoatName = $this->getLensCoatingName($type, $lensId, $coatingId, $colorId, $recipe_type,$work->is_new_version);
-                    dump($lensCoatName);
+                    var_dump($type,$lensId,$coatingId,$colorId,$recipe_type,$work->is_new_version);
+                    var_dump($lensCoatName);
                     exit;
                     $data = [
                         'work_id' => $work_id,
