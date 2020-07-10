c5c656fbbaffca3cd7ae55b71365224ef60198e9
wpl <wangpenglei@nextmar.com>
2020-07-10 11:48:54 +0800

    1

--- application/admin/model/saleaftermanage/SaleAfterTask.php
+++ application/admin/model/saleaftermanage/SaleAfterTask.php
@@ -626,7 +626,7 @@ class SaleAfterTask extends Model
                 ->join('sales_flat_shipment_track s', 'o.entity_id=s.order_id', 'left')
                 ->join('sales_flat_order_payment p', 'o.entity_id=p.parent_id', 'left')
                 ->where('customer_email', $customer_email)
-                ->field('o.base_to_order_rate,o.base_total_paid,o.base_total_due,o.entity_id,o.mw_rewardpoint,o.mw_rewardpoint_discount_show,o.status,o.coupon_code,o.coupon_rule_name,o.store_id,o.increment_id,o.customer_email,o.customer_firstname,o.customer_lastname,o.order_currency_code,o.total_item_count,o.grand_total,o.base_grand_total,o.base_shipping_amount,o.shipping_description,o.base_total_paid,o.base_total_due,o.created_at,round(o.total_qty_ordered,0) total_qty_ordered,o.order_type,s.track_number,s.title,p.base_amount_paid,p.base_amount_ordered,p.base_amount_authorized,p.method,p.last_trans_id,p.additional_information
+                ->field('o.is_modify_address,o.base_to_order_rate,o.base_total_paid,o.base_total_due,o.entity_id,o.mw_rewardpoint,o.mw_rewardpoint_discount_show,o.status,o.coupon_code,o.coupon_rule_name,o.store_id,o.increment_id,o.customer_email,o.customer_firstname,o.customer_lastname,o.order_currency_code,o.total_item_count,o.grand_total,o.base_grand_total,o.base_shipping_amount,o.shipping_description,o.base_total_paid,o.base_total_due,o.created_at,round(o.total_qty_ordered,0) total_qty_ordered,o.order_type,s.track_number,s.title,p.base_amount_paid,p.base_amount_ordered,p.base_amount_authorized,p.method,p.last_trans_id,p.additional_information
            ')
                 ->group('o.entity_id')
                 ->order('o.entity_id desc')->select();
