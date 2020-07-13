565ae96897f8c25988955316bd9f53ccba33234c
jianghaohui <jianghaohui@nextmar.com>
2020-07-13 17:53:23 +0800

    补货需求单

--- application/admin/controller/purchase/NewProductReplenishOrder.php
+++ application/admin/controller/purchase/NewProductReplenishOrder.php
@@ -11,7 +11,7 @@ use app\common\controller\Backend;
  */
 class NewProductReplenishOrder extends Backend
 {
-    
+
     /**
      * NewProductReplenishOrder模型对象
      * @var \app\admin\model\purchase\NewProductReplenishOrder
@@ -22,90 +22,17 @@ class NewProductReplenishOrder extends Backend
     {
         parent::_initialize();
         $this->model = new \app\admin\model\purchase\NewProductReplenishOrder;
+        $this->supplier = new \app\admin\model\purchase\Supplier;
 
     }
-    
+
     /**
      * 默认生成的控制器所继承的父类中有index/add/edit/del/multi五个基础方法、destroy/restore/recyclebin三个回收站方法
      * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
 
-    /***
-     * 编辑之后提交审核
-     */
-    public function audit()
-    {
-        if ($this->request->isAjax()) {
-            $id = $this->request->param('ids');
-            $row = $this->model->get($id, 'itemAttribute');
-            if ($row['item_status'] != 1) {
-                $this->error('此商品状态不能提交审核');
-            }
-            // if (false == $row['itemAttribute']['frame_images']) {
-            //     $this->error('请先上传商品图片');
-            // }
-            $map['id'] = $id;
-            $data['item_status'] = 2;
-            $res = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
-            if ($res) {
-                $this->success('提交审核成功');
-            } else {
-                $this->error('提交审核失败');
-            }
-        } else {
-            $this->error('404 Not found');
-        }
-    }
-    /***
-     * 提交审核之后审核通过
-     */
-    public function passAudit()
-    {
-        if ($this->request->isAjax()) {
-            $id = $this->request->param('ids');
-            $row = $this->model->get($id);
-            if ($row['item_status'] != 2) {
-                $this->error('此商品状态不能审核通过');
-            }
-            $map['id'] = $id;
-            $data['item_status'] = 3;
-            $data['check_time']  = date("Y-m-d H:i:s", time());
-            $res = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
-            if ($res) {
-                //添加到商品平台sku
-                (new ItemPlatformSku())->addPlatformSku($row);
-                $this->success('审核通过成功');
-            } else {
-                $this->error('审核通过失败');
-            }
-        } else {
-            $this->error('404 Not found');
-        }
-    }
-    /***
-     * 提交审核之后审核拒绝
-     */
-    public function auditRefused()
-    {
-        if ($this->request->isAjax()) {
-            $id = $this->request->param('ids');
-            $row = $this->model->get($id);
-            if ($row['item_status'] != 2) {
-                $this->error('此商品状态不能审核拒绝');
-            }
-            $map['id'] = $id;
-            $data['item_status'] = 4;
-            $res = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
-            if ($res) {
-                $this->success('审核拒绝成功');
-            } else {
-                $this->error('审核拒绝失败');
-            }
-        } else {
-            $this->error('404 Not found');
-        }
-    }
+
     /***
      * 多个一起审核通过
      */
@@ -113,22 +40,21 @@ class NewProductReplenishOrder extends Backend
     {
         if ($this->request->isAjax()) {
             $map['id'] = ['in', $ids];
-            $row = $this->model->where($map)->field('id,item_status')->select();
+            $row = $this->model->where($map)->field('id,status,is_verify')->select();
             foreach ($row as $v) {
-                if ($v['item_status'] != 2) {
+                if ($v['status'] != 1) {
+                    $this->error('只有待分配状态才能操作！！');
+                }
+                if ($v['is_verify'] != 0) {
                     $this->error('只有待审核状态才能操作！！');
                 }
             }
-            $data['item_status'] = 3;
-            $data['check_time']  = date("Y-m-d H:i:s", time());
+            $data['is_verify'] = 1;
+            $data['check_time'] = date("Y-m-d H:i:s", time());
+
             $res = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
-            if ($res != false) {
-                $row = $this->model->where('id', 'in', $ids)->field('sku,name,frame_is_rimless')->select();
-                if ($row) {
-                    foreach ($row as $val) {
-                        (new ItemPlatformSku())->addPlatformSku($val);
-                    }
-                }
+
+            if ($res !== false) {
                 $this->success('审核成功');
             } else {
                 $this->error('审核失败');
@@ -137,6 +63,7 @@ class NewProductReplenishOrder extends Backend
             $this->error('404 Not found');
         }
     }
+
     /***
      * 多个一起审核拒绝
      */
@@ -144,13 +71,18 @@ class NewProductReplenishOrder extends Backend
     {
         if ($this->request->isAjax()) {
             $map['id'] = ['in', $ids];
-            $row = $this->model->where($map)->field('id,item_status')->select();
+            $row = $this->model->where($map)->field('id,status,is_verify')->select();
             foreach ($row as $v) {
-                if ($v['item_status'] != 2) {
+                if ($v['status'] != 1) {
+                    $this->error('只有待分配状态才能操作！！');
+                }
+                if ($v['is_verify'] != 0) {
                     $this->error('只有待审核状态才能操作！！');
                 }
             }
-            $data['item_status'] = 4;
+            $data['is_verify'] = 2;
+            $data['check_time'] = date("Y-m-d H:i:s", time());
+
             $res = $this->model->allowField(true)->isUpdate(true, $map)->save($data);
             if ($res !== false) {
                 $this->success('审核拒绝成功');
@@ -160,6 +92,79 @@ class NewProductReplenishOrder extends Backend
         } else {
             $this->error('404 Not found');
         }
+    }
+
+
+    /**
+     * Created by Phpstorm.
+     * User: jhh
+     * Date: 2020/7/13
+     * Time: 11:22
+     */
+    public function distribution()
+    {
+        //设置过滤方法
+        $this->request->filter(['strip_tags']);
+        if ($this->request->isAjax()) {
+            //如果发送的来源是Selectpage，则转发到Selectpage
+            if ($this->request->request('keyField')) {
+                return $this->selectpage();
+            }
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            $total = $this->model->where(['is_del' => 1,'is_verify'=>1])
+                ->where($where)
+                ->order($sort, $order)
+                ->count();
+
+            $list = $this->model->where(['is_del' => 1,'is_verify'=>1])
+                ->where($where)
+                ->order($sort, $order)
+                ->limit($offset, $limit)
+                ->select();
+
+            $list = collection($list)->toArray();
+            foreach ($list as $k => $v) {
+//                $list[$k]['supplier'] = $this->supplier->getSupplierData();
+                $list[$k]['supplier'] = ['0'=>['name'=>'思蒙眼镜','num'=>10],'1'=>['name'=>'兴亮眼镜','num'=>11]];
+            }
+            $result = array("total" => $total, "rows" => $list);
+
+            return json($result);
+        }
+        return $this->view->fetch('distribution');
+    }
+
+    /**
+     * Created by Phpstorm.
+     * User: jhh
+     * Date: 2020/7/13
+     * Time: 15:23
+     */
+    public function distribution_confirm()
+    {
+        $ids = $this->request->post("ids/a");
+        dump($ids);die;
+        if (!$ids) {
+            $this->error('缺少参数！！');
+        }
+
+    }
+
+    /**
+     * Created by Phpstorm.
+     * User: jhh
+     * Date: 2020/7/13
+     * Time: 17:22
+     */
+    public function distribute_detail()
+    {
+        $supplier = $this->supplier->getSupplierData();
+        $this->assign('supplier',$supplier);
+        return $this->view->fetch();
+    }
+    public function handle()
+    {
+        dump(11);
     }
 
 }
--- public/assets/js/backend/purchase/new_product_replenish_order.js
+++ public/assets/js/backend/purchase/new_product_replenish_order.js
@@ -5,6 +5,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             // 初始化表格参数配置
             Table.api.init({
                 searchFormVisible: true,
+
                 extend: {
                     index_url: 'purchase/new_product_replenish_order/index' + location.search,
                     add_url: 'purchase/new_product_replenish_order/add',
@@ -26,12 +27,42 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                     [
                         {checkbox: true},
                         {field: 'id', title: __('Id')},
-                        {field: 'sku', title: __('SKU')},
-                        {field: 'replenishment_num', title: __('Replenishment_num')},
-                        {field: 'status', title: __('状态'), custom: { 1: 'blue', 2: 'danger', 3: 'orange', 4: 'red'}, searchList: { 1: '待分配', 2: '待处理', 3: '部分处理', 4: '已处理' }, formatter: Table.api.formatter.status },
-                        {field: 'is_verify', title: __('审核状态'),searchList: { 0: '未通过', 1: '通过'}, formatter: Table.api.formatter.status},
-                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                        {field: 'sku', title: __(' sku')},
+                        {field: 'replenishment_num', title: __('Replenishment_num'), operate: false},
+                        {
+                            field: 'status',
+                            title: __('状态'),
+                            custom: {1: 'blue', 2: 'danger', 3: 'orange', 4: 'red'},
+                            searchList: {1: '待分配', 2: '待处理', 3: '部分处理', 4: '已处理'},
+                            formatter: Table.api.formatter.status
+                        },
+                        {
+                            field: 'is_verify',
+                            title: __('审核状态'),
+                            custom: {0: 'blue', 1: 'green', 2: 'danger'},
+                            searchList: {0: '待审核', 1: '审核通过', 2: '审核拒绝'},
+                            formatter: Table.api.formatter.status
+                        },
+                        {
+                            field: 'create_time',
+                            title: __('Create_time'),
+                            operate: 'RANGE',
+                            addclass: 'datetimerange',
+                            formatter: Table.api.formatter.datetime
+                        },
+                        // {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate},
+                        {
+                            field: 'operate', title: __('Operate'),
+                            table: table,
+                            events: Table.api.events.operate,
+                            formatter: function (value, row, index) {
+                                var that = $.extend({}, this);
+                                $(table).data("operate-edit", null); // 列表页面隐藏 .编辑operate-edit  - 删除按钮operate-del
+                                that.table = table;
+                                return Table.api.formatter.operate.call(that, value, row, index);
+                            }
+                            // formatter: Table.api.formatter.operate
+                        }
                     ]
                 ]
             });
@@ -46,8 +77,24 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                     __('确定要审核通过吗'),
                     function (index) {
                         Backend.api.ajax({
-                            url: "itemmanage/item/morePassAudit",
-                            data: { ids: ids }
+                            url: "purchase/new_product_replenish_order/morePassAudit",
+                            data: {ids: ids}
+                        }, function (data, ret) {
+                            table.bootstrapTable('refresh');
+                            Layer.close(index);
+                        });
+                    }
+                );
+            });
+            //商品审核拒绝
+            $(document).on('click', '.btn-refused', function () {
+                var ids = Table.api.selectedids(table);
+                Layer.confirm(
+                    __('确定要审核拒绝吗'),
+                    function (index) {
+                        Backend.api.ajax({
+                            url: "purchase/new_product_replenish_order/moreAuditRefused",
+                            data: {ids: ids}
                         }, function (data, ret) {
                             table.bootstrapTable('refresh');
                             Layer.close(index);
@@ -61,6 +108,125 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         },
         edit: function () {
             Controller.api.bindevent();
+        },
+        distribution: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                singleSelect: true,
+                showJumpto: true,
+                searchFormVisible: true,
+                extend: {
+                    index_url: 'purchase/new_product_replenish_order/distribution' + location.search,
+                    table: 'item',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {
+
+                            field: '', title: __('序号'), formatter: function (value, row, index) {
+                                var options = table.bootstrapTable('getOptions');
+                                var pageNumber = options.pageNumber;
+                                var pageSize = options.pageSize;
+
+                                //return (pageNumber - 1) * pageSize + 1 + index;
+                                return 1 + index;
+                            }, operate: false,visible:false
+                        },
+                        {field: 'id', title: __('Id'), operate: false},
+                        {field: 'sku', title: __(' sku'), operate: 'LIKE'},
+                        {field: 'replenishment_num', title: __('Replenishment_num'), operate: false},
+                        {
+                            field: 'supplier',
+                            title: __('分配数量'),
+                            operate: false,
+                            formatter: function (value, rows) {
+                                var all_user_name = '';
+                                if (value.length > 0) {
+                                    for (i = 0, len = value.length; i < len; i++) {
+                                        // all_user_name += '<div class="step_recept"><b class="step">' + value[i].name + '：</b><input id="'+ rows +'" type="text" class="form-control" style="display: inline-block;width: 180px;text-align: center;" value="'+ value[i].num +'"></div>';
+                                        all_user_name += '<div class="step_recept"><b class="step">' + value[i].name + '：</b><b class="recept text-red">'+ value[i].num +'</b></div>';
+                                    }
+                                }
+                                return all_user_name;
+                            },
+                        },
+                        {
+                            field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate,
+                            buttons: [
+                                // {
+                                //     name: 'passAudit',
+                                //     text: '确认分配',
+                                //     title: __('确认分配'),
+                                //     classname: 'btn btn-xs btn-success btn-confirmdis',
+                                //     icon: 'fa fa-pencil',
+                                //     visible: function (row) {
+                                //         //返回true时按钮显示,返回false隐藏
+                                //         if (row.status == 1) {
+                                //             return true;
+                                //         } else {
+                                //             return false;
+                                //         }
+                                //     }
+                                // },
+                                {
+                                    name: 'distribution_detail',
+                                    text: __('确认分配'),
+                                    title: __('确认分配'),
+                                    icon: 'fa fa-pencil',
+                                    classname: 'btn btn-xs btn-success btn-dialog',
+                                    url: 'purchase/new_product_replenish_order/distribute_detail',
+                                    callback: function (data) {
+                                    }
+                                },
+
+                            ],
+                            formatter: Table.api.formatter.operate
+                        }
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+
+            //需求单确认分配
+            // $(document).on('click', '.btn-confirmdis', function () {
+            //     var ids = Table.api.selectedids(table);
+            //     Layer.confirm(
+            //         __('确定分配吗'),
+            //         function (index) {
+            //             Backend.api.ajax({
+            //                 url: "purchase/new_product_replenish_order/distribution_confirm",
+            //                 data: {ids: ids,}
+            //             }, function (data, ret) {
+            //                 table.bootstrapTable('refresh');
+            //                 Layer.close(index);
+            //             });
+            //         }
+            //     );
+            // });
+        },
+        distribute_detail: function () {
+            Controller.api.bindevent();
+            $(document).on('click', '.btn-add', function () {
+                var content = $('#table-content table tbody').html();
+                $('.caigou table tbody').append(content);
+
+            })
+
+            $(document).on('click', '.btn-del', function () {
+                $(this).parent().parent().remove();
+            })
         },
         api: {
             bindevent: function () {
