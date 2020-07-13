64efc26680644e0949a8619db288645a16318fd9
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-07-13 17:38:50 +0800

    修改地址

--- application/admin/controller/saleaftermanage/WorkOrderList.php
+++ application/admin/controller/saleaftermanage/WorkOrderList.php
@@ -2488,17 +2488,17 @@ class WorkOrderList extends Backend
             $work_id = input('work_id');
 
             try {
+                //获取网站数据库地址,获取地址信息
+                $res = $this->model->getAddress($siteType, $incrementId);
                 //判断是否是新建状态
                 $work_status = $this->model->where('id',$work_id)->value('work_status');
                 if($work_status == 1){
                     //获取魔晶数据库中地址
                     $address = Db::name('work_order_change_sku')->where('work_id',$work_id)->value('userinfo_option');
                     $address = unserialize($address);
-                    $address['address_type'] = $address['address_id'];
-                    $res['address'][0] = $address;
-                }else{
-                    //获取网站数据库地址,获取地址信息
-                    $res = $this->model->getAddress($siteType, $incrementId);
+                    $res['address'][$address['address_id']] = $address;
+                    $address_type = $address['address_id'] == 0 ? 'shipping' : 'billing';
+                    $res['address'][$address['address_id']]['address_type'] = $address_type;
                 }
             } catch (\Exception $e) {
                 $this->error($e->getMessage());
