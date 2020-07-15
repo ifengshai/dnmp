91fab11b6cf53ab12eb4f9586ee10b7a3d7d772d
lixiang0401 <6225998@qq.com>
2020-07-15 15:56:54 +0800

    1

--- application/admin/controller/zendesk/Notice.php
+++ application/admin/controller/zendesk/Notice.php
@@ -993,7 +993,7 @@ class Notice extends Controller
      */
     public function asyncUpdate()
     {
-        $params = 'type:ticket updated_at>=2020-07-08T04:00:00Z updated_at<=2020-07-08T07:00:00Z order_by:updated_at sort:asc';
+        $params = 'type:ticket updated_at>=2020-07-08T07:00:00Z updated_at<=2020-07-08T23:00:00Z order_by:updated_at sort:asc';
         //Get all tickets
         $tickets = $this->client->search()->find($params);
 
