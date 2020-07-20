c7eac62f611b2cd910392443f1edfe4d58c902a0
wpl <wangpenglei@nextmar.com>
2020-07-20 10:34:09 +0800

    1

--- application/admin/controller/warehouse/TransferOrder.php
+++ application/admin/controller/warehouse/TransferOrder.php
@@ -15,7 +15,7 @@ use think\exception\ValidateException;
  */
 class TransferOrder extends Backend
 {
-    
+
     /**
      * TransferOrder模型对象
      * @var \app\admin\model\warehouse\TransferOrder
@@ -27,14 +27,18 @@ class TransferOrder extends Backend
         parent::_initialize();
         $this->model = new \app\admin\model\warehouse\TransferOrder;
 
+        //获取所有站点
+        $this->magentoplatform = new \app\admin\model\platformManage\MagentoPlatform();
+        $this->magentoplatformarr = $this->magentoplatform->column('name', 'id');
+        $this->assign('magentoplatformarr', $this->magentoplatformarr);
     }
-    
+
     /**
      * 默认生成的控制器所继承的父类中有index/add/edit/del/multi五个基础方法、destroy/restore/recyclebin三个回收站方法
      * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
-    
+
 
     /**
      * 查看
@@ -45,24 +49,22 @@ class TransferOrder extends Backend
         // $this->relationSearch = true;
         //设置过滤方法
         $this->request->filter(['strip_tags']);
-        if ($this->request->isAjax())
-        {
+        if ($this->request->isAjax()) {
             //如果发送的来源是Selectpage，则转发到Selectpage
-            if ($this->request->request('keyField'))
-            {
+            if ($this->request->request('keyField')) {
                 return $this->selectpage();
             }
             list($where, $sort, $order, $offset, $limit) = $this->buildparams();
             $total = $this->model
-                    ->where($where)
-                    ->order($sort, $order)
-                    ->count();
+                ->where($where)
+                ->order($sort, $order)
+                ->count();
 
             $list = $this->model
-                    ->where($where)
-                    ->order($sort, $order)
-                    ->limit($offset, $limit)
-                    ->select();
+                ->where($where)
+                ->order($sort, $order)
+                ->limit($offset, $limit)
+                ->select();
             $list = collection($list)->toArray();
             $result = array("total" => $total, "rows" => $list);
 
@@ -93,7 +95,21 @@ class TransferOrder extends Backend
                         $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : $name) : $this->modelValidate;
                         $this->model->validateFailException(true)->validate($validate);
                     }
+                    $params['create_time'] = date('Y-m-d H:i:s');
+                    $params['create_person'] = session('admin.nickname');
                     $result = $this->model->allowField(true)->save($params);
+                    if (false !== $result) {
+                        $sku = $this->request->post("sku/a");
+                        $num = $this->request->post("num/a");
+                        $data = [];
+                        foreach (array_filter($sku) as $k => $v) {
+                            $data[$k]['sku'] = trim($v);
+                            $data[$k]['num'] = trim($num[$k]);
+                            $data[$k]['transfer_order_id'] = $this->model->id;
+                        }
+                        //批量添加
+                        $this->purchase_order_item->saveAll($data);
+                    }
                     Db::commit();
                 } catch (ValidateException $e) {
                     Db::rollback();
@@ -113,6 +129,11 @@ class TransferOrder extends Backend
             }
             $this->error(__('Parameter %s can not be empty', ''));
         }
+
+        //生成采购编号
+        $transfer_order_number = 'TO' . date('YmdHis') . rand(100, 999) . rand(100, 999);
+        $this->assign('transfer_order_number', $transfer_order_number);
+
         return $this->view->fetch();
     }
 
@@ -144,7 +165,21 @@ class TransferOrder extends Backend
                         $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : $name) : $this->modelValidate;
                         $row->validateFailException(true)->validate($validate);
                     }
+                    $params['create_time'] = date('Y-m-d H:i:s');
+                    $params['create_person'] = session('admin.nickname');
                     $result = $row->allowField(true)->save($params);
+                    if (false !== $result) {
+                        $sku = $this->request->post("sku/a");
+                        $num = $this->request->post("num/a");
+                        $data = [];
+                        foreach (array_filter($sku) as $k => $v) {
+                            $data[$k]['sku'] = trim($v);
+                            $data[$k]['num'] = trim($num[$k]);
+                            $data[$k]['transfer_order_id'] = $this->model->id;
+                        }
+                        //批量添加
+                        $this->purchase_order_item->saveAll($data);
+                    }
                     Db::commit();
                 } catch (ValidateException $e) {
                     Db::rollback();
@@ -166,5 +201,26 @@ class TransferOrder extends Backend
         }
         $this->view->assign("row", $row);
         return $this->view->fetch();
+    }
+
+    /**
+     * 获取对应站点虚拟库存
+     *
+     * @Description
+     * @author wpl
+     * @since 2020/07/20 10:01:39 
+     * @return void
+     */
+    public function getSkuData()
+    {
+        if ($this->request->isPost()) {
+            $sku = input('sku');
+            $platform_type = input('platform_type');
+            $item_platform = new \app\admin\model\itemmanage\ItemPlatformSku();
+            $stock = $item_platform->where(['sku' => $sku, 'platform_type' => $platform_type])->value('stock');
+            $this->success('', '', $stock);
+        } else {
+            $this->error(__('No rows were updated'));
+        }
     }
 }
--- public/assets/js/backend/warehouse/transfer_order.js
+++ public/assets/js/backend/warehouse/transfer_order.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jqui'], function ($, undefined, Backend, Table, Form) {
 
     var Controller = {
         index: function () {
@@ -26,9 +26,9 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 sortName: 'id',
                 columns: [
                     [
-                        {checkbox: true},
-                        {field: 'id', title: __('Id')},
-                        {field: 'transfer_order_number', title: __('Transfer_order_number')},
+                        { checkbox: true },
+                        { field: 'id', title: __('Id') },
+                        { field: 'transfer_order_number', title: __('Transfer_order_number') },
                         // {field: 'call_out_site', title: __('Call_out_site')},
                         // {field: 'call_in_site', title: __('Call_in_site')},
                         // {field: 'remark', title: __('Remark')},
@@ -37,9 +37,9 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                             searchList: { 0: '新建', 1: '待审核', 2: '已审核', 3: '已拒绝', 4: '已取消' },
                             formatter: Table.api.formatter.status
                         },
-                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'create_person', title: __('Create_person')},
-                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                        { field: 'create_time', title: __('Create_time'), operate: 'RANGE', addclass: 'datetimerange', formatter: Table.api.formatter.datetime },
+                        { field: 'create_person', title: __('Create_person') },
+                        { field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate }
                     ]
                 ]
             });
@@ -49,13 +49,86 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         },
         add: function () {
             Controller.api.bindevent();
+            $(document).on('click', '.btn-add', function () {
+                var content = $('#table-content table tbody').html();
+                $('.table_list table tbody').append(content);
+                Controller.api.bindevent();
+            })
+
+            //删除商品数据
+            $(document).on('click', '.btn-del', function () {
+                $(this).parent().parent().remove();
+            })
+
+            //获取sku信息
+            $(document).on('change', '.sku', function () {
+                var sku = $(this).val();
+                var platform_type = $('.call_out_site.selectpicker').val();
+                var _this = $(this);
+                if (!sku || !platform_type) {
+                    Toastr.error('SKU和调出虚拟仓不能为空');
+                    return false;
+                }
+                Backend.api.ajax({
+                    url: 'warehouse/transfer_order/getSkuData',
+                    data: { sku: sku, platform_type: platform_type }
+                }, function (data, ret) {
+                    _this.parent().parent().find('.sku_stock').val(data);
+                }, function (data, ret) {
+                    Fast.api.error(ret.msg);
+                });
+
+            })
         },
         edit: function () {
             Controller.api.bindevent();
+            $(document).on('click', '.btn-add', function () {
+                var content = $('#table-content table tbody').html();
+                $('.table_list table tbody').append(content);
+                Controller.api.bindevent();
+            })
+
+            //删除商品数据
+            $(document).on('click', '.btn-del', function () {
+                $(this).parent().parent().remove();
+            })
         },
         api: {
             bindevent: function () {
                 Form.api.bindevent($("form[role=form]"));
+
+                //模糊匹配订单
+                $('.sku').autocomplete({
+                    source: function (request, response) {
+                        $.ajax({
+                            type: "POST",
+                            url: "ajax/ajaxGetLikeOriginSku",
+                            dataType: "json",
+                            cache: false,
+                            async: false,
+                            data: {
+                                origin_sku: request.term
+                            },
+                            success: function (json) {
+                                var data = json.data;
+                                response($.map(data, function (item) {
+                                    return {
+                                        label: item,//下拉框显示值
+                                        value: item,//选中后，填充到input框的值
+                                        //id:item.bankCodeInfo//选中后，填充到id里面的值
+                                    };
+                                }));
+                            }
+                        });
+                    },
+                    delay: 10,//延迟100ms便于输入
+                    select: function (event, ui) {
+                        $("#bankUnionNo").val(ui.item.id);//取出在return里面放入到item中的属性
+                    },
+                    scroll: true,
+                    pagingMore: true,
+                    max: 5000
+                });
             }
         }
     };
