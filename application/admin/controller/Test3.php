7f96b11033cd884764aa49de814520af595c13ce
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-07-21 17:40:54 +0800

    1

--- application/admin/controller/Test3.php
+++ application/admin/controller/Test3.php
@@ -255,15 +255,21 @@ class Test3 extends Backend
         $arr = array();
         $i = 0;
         foreach ($zendesk as $item){
+            $item['id'] = 97833;
             //查询该邮件的负责人的站点
             $admin_type = Db::name('zendesk_agents')->where('admin_id',$item['assign_id'])->value('type');
             if($admin_type){
                 if($admin_type != $item['type']){
                     //查询该评论的最后一条记录
                     $due_id = Db::name('zendesk_comments')->where(['zid'=>$item['id'],'is_admin'=>1,'due_id'=>['neq',0]])->order('id','desc')->value('due_id');
+                    dump($due_id);exit;
                     if(!$due_id){
+
                         $i++;
                         echo $item['id']."\n";
+                    }else{
+                        //更新
+                        //Db::name('zendesk')->where('id',$item['id'])->update(['assign_id'=>$due_id]);
                     }
 
                 }
