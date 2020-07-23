c0de2ac42d816c38142ba0c62efc455af34657ea
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-07-23 11:14:15 +0800

    1

--- application/admin/controller/zendesk/Zendesk.php
+++ application/admin/controller/zendesk/Zendesk.php
@@ -896,6 +896,7 @@ DOC;
                     //new的邮件分配，去查找曾经负责该用户的处理人，如果是当前用户就能申请，如果不是不能申请
                     //判断是否处理过该用户的邮件
                     $zendesk_id = Zendesk::where(['email'=>$item['email'],'type'=>$item['type']])->order('id','desc')->column('id');
+                    dump($zendesk_id);exit;
                     if($zendesk_id){
                         //查询接触过该用户邮件的最后一条评论
                         $commentAuthorId = Db::name('zendesk_comments')
