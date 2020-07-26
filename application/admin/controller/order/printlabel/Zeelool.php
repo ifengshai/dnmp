30651aebb3d4a65bf4c9d4d2de4b9704e1ce4e0d
lixiang <6225998@qq.com>
2020-07-26 11:18:28 +0800

    1

--- application/admin/controller/order/printlabel/Zeelool.php
+++ application/admin/controller/order/printlabel/Zeelool.php
@@ -1368,6 +1368,7 @@ where cpev.attribute_id in(161,163,164) and cpev.store_id=0 and cpev.entity_id=$
             '500006786',
             '400295439',
             '400295735',
+            '400295535',
         ];
 
         $map['sfo.increment_id'] = ['in', $str];
