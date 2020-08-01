ab4e0a705b10f4ce647115cb561172835f97cb36
wpl <wangpenglei@nextmar.com>
2020-08-01 10:19:51 +0800

    1

--- application/admin/controller/itemmanage/Itempresell.php
+++ application/admin/controller/itemmanage/Itempresell.php
@@ -62,10 +62,10 @@ class Itempresell extends Backend
                 unset($map['platform_type']);
             }
             //默认显示 开启过预售的SKU
-            if ((!$filter['presell_status'] && $filter['presell_status'] != 0) || !$filter) {
+            if (!isset($filter['presell_status'])) {
                 $map['presell_status'] = ['in', [1, 2]];
             }
-      
+
             list($where, $sort, $order, $offset, $limit) = $this->buildparams();
             $total = $this->model
                 ->where($map)
