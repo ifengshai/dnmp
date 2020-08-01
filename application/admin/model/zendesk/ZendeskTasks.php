72ccffba58f6acfa18b8bfd830ab917afebd6037
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-08-01 17:52:03 +0800

    1

--- application/admin/model/zendesk/ZendeskTasks.php
+++ application/admin/model/zendesk/ZendeskTasks.php
@@ -38,7 +38,7 @@ class ZendeskTasks extends Model
         if($admin_id){
             $where['admin_id'] = $admin_id;
         }
-        $where['target_count']  = array('exp',Db::raw(' < reply_count '));
+        $where['target_count']  = array('exp',Db::raw(' > reply_count '));
         if($platform){
             $where['type'] = $platform;
         }
