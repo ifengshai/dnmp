3126d92d72318b4e90fffbf02b5b013678543174
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-08-04 10:00:39 +0800

    邮件里添加分配人

--- public/assets/js/backend/zendesk/zendesk.js
+++ public/assets/js/backend/zendesk/zendesk.js
@@ -35,6 +35,12 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jq-tags', 'jqui','te
                             align: 'left',
                             searchList: $.getJSON('zendesk/zendesk_agents/getAgentsList')
                         },
+                        {
+                            field: 'admin.nickname',
+                            title: __('Due_id'),
+                            align: 'left',
+                            searchList: $.getJSON('zendesk/zendesk_agents/getAgentsList')
+                        },
                         {field: 'status', title: __('Status'), custom: { 1: 'danger', 2: 'success', 3: 'blue', 4: 'orange', 5: 'gray'}, searchList: { 1: 'New', 2: 'Open', 3: 'Pending', 4: 'Solved', 5: 'Close'}, formatter: Table.api.formatter.status },
                         {
                             field: 'tags', title: __('Tags'), searchList: function (column) {
