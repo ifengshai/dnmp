a8c6f3d3d14324738e567869d5892973b25d79ac
wpl <wangpenglei@nextmar.com>
2020-08-04 11:37:58 +0800

    1

--- application/api/controller/SelfApi.php
+++ application/api/controller/SelfApi.php
@@ -640,20 +640,12 @@ class SelfApi extends Api
     {
         if ($this->request->isPost()) {
             $site = $this->request->post('site'); //站点
-            $sku = $this->request->post('sku'); //true_sku
-            $qty = $this->request->post('qty'); //扣减数量 占用增加数量
             $orderid = $this->request->post('orderid'); //订单id
             $order_number = $this->request->post('order_number'); //订单号
-            if (!$sku) {
-                $this->error(__('缺少SKU参数'), [], 400);
-            }
-
+            $order_data = $this->request->post('order_data'); //订单json数据
+            $order_data = '[{"sku":"ZOP008546-01","qty":2},{"sku":"ZOP008617-01","qty":3},{"sku":"ZWO483822-02","qty":5}]';
             if (!$site) {
                 $this->error(__('缺少站点参数'), [], 400);
-            }
-
-            if (!$qty) {
-                $this->error(__('缺少数量参数'), [], 400);
             }
 
             if (!$orderid) {
@@ -663,67 +655,83 @@ class SelfApi extends Api
             if (!$order_number) {
                 $this->error(__('缺少订单号参数'), [], 400);
             }
+
+            if (!$order_data) {
+                $this->error(__('缺少订单号参数'), [], 400);
+            }
+
+            $order_data = json_decode($order_data);
+
             $item = new \app\admin\model\itemmanage\Item();
             $platform = new \app\admin\model\itemmanage\ItemPlatformSku();
+            //订单json数据 包含sku qty
+            $order_data = json_decode($order_data, true);
+            $skus = array_column($order_data, 'sku');
+            //查询所有true sku
+            $platform_data = $platform->where(['platform_sku' => ['in', $skus], 'platform_type' => $site])->column('*', 'platform_sku');
+            $true_skus = array_column($platform_data, 'sku');
             //查询sku可用库存
-            $res = $item->where(['is_del' => 1, 'is_open' => 1, 'sku' => $sku])->field('available_stock')->find();
-            //查询此站点sku预售情况
-            $list = $platform->where(['sku' => $sku, 'platform_type' => $site])->find();
-            if (!$res || !$list) {
-                $this->error(__('未查询到数据'), [], 400);
-            }
+            $res = $item->where(['is_del' => 1, 'is_open' => 1, 'sku' => ['in', $true_skus]])->column('available_stock', 'sku');
 
-            //扣减对应站点虚拟仓库存
-            $platform_res = $platform->where(['sku' => $sku, 'platform_type' => $site])->setDec('stock', $qty);
-            if ($platform_res === false) {
-                file_put_contents('/www/wwwroot/mojing/runtime/log/set_goods_stock.log', '扣减虚拟库存失败：site:' . $site . '|订单id:' . $orderid . '|sku:' . $sku . "\r\n", FILE_APPEND);
-                $this->error(__('虚拟库存扣减失败'), [], 400);
-            }
+            foreach ($order_data as $k => $v) {
+                $true_sku = $platform_data[$v['sku']]['sku'];
+                $qty = $v['qty'];
+                //扣减对应站点虚拟仓库存
+                $platform_res = $platform->where(['sku' => $true_sku, 'platform_type' => $site])->setDec('stock', $qty);
+                if ($platform_res !== false) {
+                    //扣减可用库存 增加订单占用库存
+                    $item_res = $item->where(['is_del' => 1, 'is_open' => 1, 'sku' => $true_sku])->dec('available_stock', $qty)->inc('occupy_stock', $qty)->update();
+                } else {
+                    file_put_contents('/www/wwwroot/mojing/runtime/log/set_goods_stock.log', '扣减虚拟库存失败：site:' . $site . '|订单id:' . $orderid . '|sku:' . $true_sku . "\r\n", FILE_APPEND);
+                }
 
-            //扣减可用库存 增加订单占用库存
-            $item_res = $item->where(['is_del' => 1, 'is_open' => 1, 'sku' => $sku])->dec('available_stock', $qty)->inc('occupy_stock', $qty)->update();
+                //如果可用库存不足 判断此sku 对应站点是否开启预售
+                if ($res[$true_sku] < $qty) {
+                    //判断是否开启预售 并且在有效时间内 并且预售剩余数量大于0
+                    if ($platform_data[$v['sku']]['presell_status'] == 1 && strtotime($platform_data[$v['sku']]['presell_create_time']) <= time() && strtotime($platform_data[$v['sku']]['presell_end_time']) >= time() && $platform_data[$v['sku']]['presell_residue_num'] > 0) {
+                        $available_stock = $res[$true_sku];
+                        //判断可用库存小于0时 应扣减预售数量为当前qty 否则预售数量等于 qty 减去现有的可用库存
+                        if ($available_stock <= 0) {
+                            $presell_num = $qty;
+                        } else {
+                            $presell_num = $qty - $available_stock;
+                        }
 
-            //如果可用库存不足 判断此sku 对应站点是否开启预售
-            if ($res->available_stock < $qty) {
-                //判断是否开启预售 并且在有效时间内 并且预售剩余数量大于0
-                if ($list->presell_status == 1 && strtotime($list->presell_create_time) <= time() && strtotime($list->presell_end_time) >= time() && $list->presell_residue_num > 0) {
-                    $available_stock = $res->available_stock;
-                    //判断可用库存小于0时 应扣减预售数量为当前qty 否则预售数量等于 qty 减去现有的可用库存
-                    if ($available_stock <= 0) {
-                        $presell_num = $qty;
-                    } else {
-                        $presell_num = $qty - $res->available_stock;
+                        //判断如果剩余预售数量 大于 应扣减预售数量时 剩余预售数量= 现有剩余预售数量减去应扣减预售数量   否则 剩余预售数量全部扣减为0
+                        if ($platform_data[$v['sku']]['presell_residue_num'] >= $presell_num) {
+                            $presell_residue_num = $platform_data[$v['sku']]['presell_residue_num'] - $presell_num;
+                        } else {
+                            $presell_residue_num = 0;
+                        }
+                        //扣减剩余预售数量
+                        $platform_res = $platform->where(['sku' => $true_sku, 'platform_type' => $site])->update(['presell_residue_num' => $presell_residue_num]);
+                        if ($platform_res === false) {
+                            file_put_contents('/www/wwwroot/mojing/runtime/log/set_goods_stock.log', '扣减预售数量：site:' . $site . '|订单id:' . $orderid . '|sku:' . $true_sku . '|扣减预售数量：' . $presell_residue_num . "\r\n", FILE_APPEND);
+                        }
                     }
+                }
 
-                    //判断如果剩余预售数量 大于 应扣减预售数量时 剩余预售数量= 现有剩余预售数量减去应扣减预售数量   否则 剩余预售数量全部扣减为0
-                    if ($list->presell_residue_num >= $presell_num) {
-                        $presell_residue_num = $list->presell_residue_num - $presell_num;
-                    } else {
-                        $presell_residue_num = 0;
-                    }
-                    //扣减剩余预售数量
-                    $platform_res = $platform->where(['sku' => $sku, 'platform_type' => $site])->update(['presell_residue_num' => $presell_residue_num]);
-                    if ($platform_res === false) {
-                        file_put_contents('/www/wwwroot/mojing/runtime/log/set_goods_stock.log', '扣减预售数量：site:' . $site . '|订单id:' . $orderid . '|sku:' . $sku . '|扣减预售数量：' . $presell_residue_num . "\r\n", FILE_APPEND);
-                    }
+                if (false !== $item_res) {
+                    //生成扣减库存日志
+                    (new StockLog())->setData([
+                        'type'                      => 1,
+                        'site'                      => $site,
+                        'one_type'                  => 1,
+                        'sku'                       => $true_sku,
+                        'order_number'              => $order_number,
+                        'public_id'                 => $orderid,
+                        'occupy_stock_change'       => $qty,
+                        'available_stock_change'    => -$qty,
+                        'create_person'             => session('admin.nickname'),
+                        'create_time'               => date('Y-m-d H:i:s'),
+                        'remark'                    => '生成订单扣减可用库存,增加占用库存'
+                    ]);
+                } else {
+                    file_put_contents('/www/wwwroot/mojing/runtime/log/set_goods_stock.log', '可用库存扣减失败：site:' . $site . '|订单id:' . $orderid . '|sku:' . $true_sku . "\r\n", FILE_APPEND);
                 }
             }
 
             if (false !== $item_res) {
-                //生成扣减库存日志
-                (new StockLog())->setData([
-                    'type'                      => 1,
-                    'site'                      => $site,
-                    'one_type'                  => 1,
-                    'sku'                       => $sku,
-                    'order_number'              => $order_number,
-                    'public_id'                 => $orderid,
-                    'occupy_stock_change'       => $qty,
-                    'available_stock_change'    => -$qty,
-                    'create_person'             => session('admin.nickname'),
-                    'create_time'               => date('Y-m-d H:i:s'),
-                    'remark'                    => '生成订单扣减可用库存,增加占用库存'
-                ]);
                 $this->success('处理成功', [], 200);
             } else {
                 $this->error('处理失败', [], 400);
@@ -731,5 +739,54 @@ class SelfApi extends Api
         }
     }
 
-    
+    /**
+     * 获取sku是否有库存
+     *
+     * @Description
+     * @author wpl
+     * @since 2020/08/04 10:00:37 
+     * @return void
+     */
+    public function get_goods_stock()
+    {
+        if ($this->request->isPost()) {
+            $site = $this->request->request('site'); //站点
+            $skus = $this->request->request('skus/a'); // sku 数组
+            if (!$site) {
+                $this->error(__('缺少站点参数'), [], 400);
+            }
+
+            if (!$skus) {
+                $this->error(__('缺少sku参数'), [], 400);
+            }
+            $platform = new \app\admin\model\itemmanage\ItemPlatformSku();
+            //查询所有true sku
+            $platform_data = $platform->where(['platform_sku' => ['in', $skus], 'platform_type' => $site])->select();
+            $platform_data = collection($platform_data)->toArray();
+            if (!$platform_data) {
+                $this->error(__('未查询到数据'), [], 400);
+            }
+            $list = [];
+            foreach ($platform_data as $k => $v) {
+                //判断是否开启预售
+                if ($v['presell_status'] == 1 && strtotime($v['presell_create_time']) <= time() && strtotime($v['presell_end_time']) >= time()) {
+                    $list[$k]['stock'] = $v['stock'] + $v['presell_residue_num'];
+                } else {
+                    $list[$k]['stock'] = $v['stock'];
+                }
+                $list[$k]['sku'] = $v['platform_sku'];
+                if ($list[$k]['stock'] <= 0) {
+                    $list[$k]['is_sell_out'] = 1;
+                } else {
+                    $list[$k]['is_sell_out'] = 0;
+                }
+            }
+
+            if ($list) {
+                $this->success('处理成功', $list, 200);
+            } else {
+                $this->error('处理失败', [], 400);
+            }
+        }
+    }
 }
