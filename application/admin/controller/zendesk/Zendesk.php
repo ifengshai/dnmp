ef177c8cc66a6414fac84ec1cb6222c215ab4043
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-08-10 15:56:47 +0800

    1

--- application/admin/controller/zendesk/Zendesk.php
+++ application/admin/controller/zendesk/Zendesk.php
@@ -1298,7 +1298,6 @@ DOC;
             }
         }
         $issueList = Db::name('zendesk_agents')->column('admin_id,nickname');
-        dump($issueList);exit;
         $this->assign('issueList',$issueList);
         return $this->view->fetch();
     }
