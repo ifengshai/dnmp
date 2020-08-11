cd6dbb48cf4cf9aa840e385fffa92eb880d6f652
wpl <wangpenglei@nextmar.com>
2020-08-11 11:36:27 +0800

    1

--- application/admin/controller/saleaftermanage/WorkOrderList.php
+++ application/admin/controller/saleaftermanage/WorkOrderList.php
@@ -1449,8 +1449,10 @@ class WorkOrderList extends Backend
             //判断是否开启预售 并且预售时间是否满足 并且预售数量是否足够
             $res = $itemPlatFormSku->where(['outer_sku_status' => 1, 'platform_sku' => $sku,'platform_type' => $siteType])->find();
             //判断是否开启预售
-            if ($res['presell_status'] == 1 && strtotime($res['presell_create_time']) <= time() && strtotime($res['presell_end_time']) >= time()) {
+            if ($res['stock'] >= 0 && $res['presell_status'] == 1 && strtotime($res['presell_create_time']) <= time() && strtotime($res['presell_end_time']) >= time()) {
                 $stock = $res['stock'] + $res['presell_residue_num'];
+            } elseif($res['stock'] < 0 && $res['presell_status'] == 1 && strtotime($res['presell_create_time']) <= time() && strtotime($res['presell_end_time']) >= time()) {
+                $stock = $res['presell_residue_num'];
             } else {
                 $stock = $res['stock'];
             }
