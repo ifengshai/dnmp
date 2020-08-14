1dbb7e59c82fe6e95389268cda25c80c7064a9a9
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-08-14 15:04:49 +0800

    处理量修改

--- application/admin/model/zendesk/ZendeskComments.php
+++ application/admin/model/zendesk/ZendeskComments.php
@@ -24,7 +24,34 @@ class ZendeskComments extends Model
     {
         return $this->hasOne(ZendeskAgents::class,'admin_id','due_id');
     }
-   
+    /*
+      * 统计处理量
+      * */
+    public function dealnum_statistical($platform = 0,$time_str = '',$group_id = 0,$admin_id = 0){
+        if($platform){
+            $where['platform'] = $platform;
+        }
+        if($time_str){
+            $createat = explode(' ', $time_str);
+            $where['create_time'] = ['between', [$createat[0] . ' ' . $createat[1], $createat[3]  . ' ' . $createat[4]]];
+        }else{
+            $start = date('Y-m-d', strtotime('-6 day'));
+            $end   = date('Y-m-d 23:59:59');
+            $where['create_time'] = ['between', [$start,$end]];
+        }
+        if($group_id){
+            //查询客服类型
+            $group_admin_id = Db::name('admin')->where(['group_id'=>$group_id])->column('id');
+            $where['due_id'] = array('in',$group_admin_id);
+        }
+        if($admin_id){
+            $where['due_id'] = $admin_id;
+        }
+        $where['is_admin'] = 1;
+        $where['is_public'] = 1;
+        $count = $this->where($where)->count();
+        return $count;
+    }
 
     
 
