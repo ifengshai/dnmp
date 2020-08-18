09b4d363800f222dfe3a42437fe70f109c38a405
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-08-18 11:23:36 +0800

    1

--- vendor/overtrue/http/src/Client.php
+++ vendor/overtrue/http/src/Client.php
@@ -88,6 +88,21 @@ class Client
     {
         return $this->request($uri, 'POST', \array_merge($options, ['form_params' => $data]), $async);
     }
+    /**
+     * JSON request.
+     *
+     * @param string       $url
+     * @param string|array $data
+     * @param array        $query
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\Overtrue\Http\Support\Collection|array|object|string
+     */
+    public function postJson(string $url, array $data = [], array $query = [])
+    {
+        return $this->request($url, 'POST', ['query' => $query, 'json' => $data]);
+    }
 
     /**
      * @param string $uri
