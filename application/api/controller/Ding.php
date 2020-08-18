ef5a30cebbcc0704805ac8f5da8bee5236b0055f
wpl <wangpenglei@nextmar.com>
2020-08-18 13:45:14 +0800

    1

--- application/api/controller/Ding.php
+++ application/api/controller/Ding.php
@@ -532,6 +532,12 @@ class Ding extends Controller
                 break;
         }
         if ($send_ids && $msg) {
+            //排除谢梦飞账号
+            if (in_array(240, $send_ids)) {
+                $key = array_search(240, $send_ids);
+                unset($send_ids[$key]);
+                $send_ids = array_values($send_ids);
+            }
             return self::cc_ding(
                 $send_ids,
                 '【' . self::siteType($demand->site_type) . '】【' . self::demandType($demand->type) . '】' . $msg,
