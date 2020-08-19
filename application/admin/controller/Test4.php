a1c69128913ddefe806188e4970123d0bfb57715
wpl <wangpenglei@nextmar.com>
2020-08-19 17:01:29 +0800

    1

--- application/admin/controller/Test4.php
+++ application/admin/controller/Test4.php
@@ -180,12 +180,9 @@ class Test4 extends Backend
         $data = Db::name('zzzzaaa_temp')->select();
         foreach ($data as $k => $v) {
             if ($v['status'] == 1) {
-                $itemPlatformSKU->where(['platform_type' => $v['site'], 'sku' => trim($v['sku'])])->update(['outer_sku_status' => 1, 'is_upload' => 1]);
-            } else {
-                $itemPlatformSKU->where(['platform_type' => $v['site'], 'sku' => trim($v['sku'])])->update(['outer_sku_status' => 2, 'is_upload' => 1]);
+               $res = $itemPlatformSKU->where(['platform_type' => $v['site'], 'sku' => trim($v['sku'])])->find();
+               echo $v['sku'] . '||' . $v['site'] . "\n";
             }
-
-            echo $k . "\n";
             usleep(50000);
         }
         echo 'ok';
