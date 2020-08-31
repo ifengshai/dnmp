5f62bc553eb45394134b806d831b7967f2ec6e25
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-08-31 15:14:26 +0800

    1

--- application/admin/model/saleaftermanage/WorkOrderList.php
+++ application/admin/model/saleaftermanage/WorkOrderList.php
@@ -307,7 +307,6 @@ class WorkOrderList extends Model
                 $response = $client->request('POST', $url, array('form_params' => $params));
             }
             $body = $response->getBody();
-            dump($body);exit;
             //file_put_contents('/www/wwwroot/mojing/runtime/log/a.txt',$body,FILE_APPEND);
             $stringBody = (string) $body;
             $res = json_decode($stringBody, true);
