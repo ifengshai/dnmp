12c6d29d34aff52ebd5de86aa346f09cac1f6b92
jianghaohui <jianghaohui@nextmar.com>
2020-09-04 17:42:39 +0800

    选品-分仓流程优化需求

--- application/admin/controller/NewProduct.php
+++ application/admin/controller/NewProduct.php
@@ -1328,7 +1328,8 @@ class NewProduct extends Backend
         $platform_type = input('label');
         //统计计划补货数据
         $list = $this->model
-            ->where(['is_show' => 1, 'type' => 2,'website_type'=>$platform_type])
+            ->where(['is_show' => 1, 'type' => 2])
+            // ->where(['is_show' => 1, 'type' => 2,'website_type'=>$platform_type]) //分站点统计补货需求 2020.9.4改为计划补货 不分站点
             ->whereTime('create_time', 'between', [date('Y-m-d H:i:s', strtotime("-1 month")), date('Y-m-d H:i:s')])
             ->group('sku')
             ->column("sku,sum(replenish_num) as sum");
@@ -1339,7 +1340,7 @@ class NewProduct extends Backend
 
         //统计各个站计划某个sku计划补货的总数 以及比例 用于回写平台sku映射表中
         $sku_list = $this->model
-            ->where(['is_show' => 1, 'type' => 2,'website_type'=>$platform_type])
+            ->where(['is_show' => 1, 'type' => 2])
             ->whereTime('create_time', 'between', [date('Y-m-d H:i:s', strtotime("-1 month")), date('Y-m-d H:i:s')])
             ->field('id,sku,website_type,replenish_num')
             ->select();
@@ -1387,7 +1388,7 @@ class NewProduct extends Backend
             $result = $this->order->allowField(true)->saveAll($arr);
             //更新计划补货列表
             $ids = $this->model
-                ->where(['is_show' => 1, 'type' => 2,'website_type'=>$platform_type])
+                ->where(['is_show' => 1, 'type' => 2])
                 ->whereTime('create_time', 'between', [date('Y-m-d H:i:s', strtotime("-1 month")), date('Y-m-d H:i:s')])
                 ->setField('is_show', 0);
             Db::commit();
@@ -1542,6 +1543,29 @@ class NewProduct extends Backend
         $writer = new $class($spreadsheet);
 
         $writer->save('php://output');
+    }
+    
+    //运营补货需求购物车删除（真删除）
+    public function replenish_cart_del($ids = "")
+    {
+
+        Db::startTrans();
+        try {
+            $res = Db::name('new_product_mapping')->where('id',$ids)->delete();
+            Db::commit();
+        } catch (PDOException $e) {
+            Db::rollback();
+            $this->error($e->getMessage());
+        } catch (Exception $e) {
+            Db::rollback();
+            $this->error($e->getMessage());
+        }
+        if ($res) {
+            $this->success();
+        } else {
+            $this->error(__('No rows were deleted'));
+        }
+        $this->error(__('Parameter %s can not be empty', 'ids'));
     }
 
     //数据已跑完 2020 08.25 14:47
