d9e770c686ad1c18b57f706c03bc5830c8c2227c
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-09-15 14:13:50 +0800

    1

--- application/admin/model/saleaftermanage/WorkOrderList.php
+++ application/admin/model/saleaftermanage/WorkOrderList.php
@@ -426,6 +426,7 @@ class WorkOrderList extends Model
                 //循环插入数据
                 $changeSkuIds = [];
                 $changeSkuData = [];
+                dump($changeLens['recipe_type']);exit;
                 foreach ($original_skus as $key => $val) {
                     if (!$val) {
                         exception('sku不能为空');
