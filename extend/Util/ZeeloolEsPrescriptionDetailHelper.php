f2a0308d25e597283782f250500f0b113738a642
wpl <wangpenglei@nextmar.com>
2020-09-18 11:23:14 +0800

    1

--- extend/Util/ZeeloolEsPrescriptionDetailHelper.php
+++ extend/Util/ZeeloolEsPrescriptionDetailHelper.php
@@ -191,14 +191,12 @@ class ZeeloolEsPrescriptionDetailHelper
 				$items[$item_key]['os_add'] = $final_params['os_add'];
 				$items[$item_key]['od_add'] = $final_params['od_add'];
 			} else {
-				if ((float) $final_params['os_add'] != 0) {
+				if ((float) $final_params['os_add'] > 0) {
 					$items[$item_key]['total_add'] = $final_params['os_add'];
-				}
-
-				if ((float) $final_params['od_add'] != 0) {
+				} else {
 					$items[$item_key]['total_add'] = $final_params['od_add'];
 				}
-				
+
 			}
 
 			if ($final_params['pdcheck'] == 'on') {
