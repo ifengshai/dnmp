0293b8a7a0f14942d9195d96932aa31e555a10a2
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-10-09 16:33:44 +0800

    1

--- application/admin/controller/Test5.php
+++ application/admin/controller/Test5.php
@@ -244,6 +244,7 @@ class Test5 extends Backend
         $connect = Db::connect('database.db_zeelool');
         //查询时间
         $date_time = $this->zeelool->query("SELECT DATE_FORMAT(created_at, '%Y-%m-%d') AS date_time FROM `sales_flat_order` where created_at between '2018-01-01' and '2018-12-31' GROUP BY DATE_FORMAT(created_at, '%Y%m%d') order by DATE_FORMAT(created_at, '%Y%m%d') asc");
+        dump($date_time);exit;
         foreach ($date_time as $val){
             $arr['site'] = 1;
             $arr['day_date'] = $val['date_time'];
