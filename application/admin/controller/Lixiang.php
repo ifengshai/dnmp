e537360c5b8000e4ab5948bf627be6fd252790aa
lixiang0401 <6225998@qq.com>
2020-10-09 16:58:43 +0800

    12

--- application/admin/controller/Lixiang.php
+++ application/admin/controller/Lixiang.php
@@ -9,7 +9,7 @@ use think\Db;
 use SchGroup\SeventeenTrack\Connectors\TrackingConnector;
 use fast\Trackingmore;
 
-class Ceshi1 extends Backend
+class Lixiang extends Backend
 {
     protected $noNeedLogin = ['*'];
     protected $apiKey = 'F26A807B685D794C676FA3CC76567035';
