b2c075d88decc7c21ce8b7c49baeac8556b05b80
wpl <wangpenglei@nextmar.com>
2020-10-14 15:11:58 +0800

    添加商品数据概览

--- application/admin/controller/operatedatacenter/goodsdata/GoodsDataView.php
+++ application/admin/controller/operatedatacenter/goodsdata/GoodsDataView.php
@@ -20,4 +20,71 @@ class GoodsDataView extends Backend
         $this->assignconfig('label', $label);
         return $this->view->fetch();
     }
+
+    /**
+     * 镜框销量/客单价趋势
+     *
+     * @Description
+     * @author wpl
+     * @since 2020/10/14 15:02:02 
+     * @return void
+     */
+    public function goods_sales_data_line()
+    {
+        if ($this->request->isAjax()) {
+            $json['xColumnName'] = ['2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', '2020-07-08'];
+            $json['columnData'] = [
+                [
+                    'type' => 'line',
+                    'data' => [430, 550, 800, 650, 410, 520, 430, 870],
+                    'name' => '镜框销量',
+                    'yAxisIndex' => 0,
+                    'smooth' => true //平滑曲线
+                ],
+                [
+                    'type' => 'line',
+                    'data' => [10, 26, 45, 40, 40, 65, 73, 80],
+                    'name' => '副单价',
+                    'yAxisIndex' => 1,
+                    'smooth' => true //平滑曲线
+                ],
+
+            ];
+
+            return json(['code' => 1, 'data' => $json]);
+        }
+    }
+
+    /**
+     * 各品类商品销量趋势
+     *
+     * @Description
+     * @author wpl
+     * @since 2020/10/14 15:02:23 
+     * @return void
+     */
+    public function goods_type_data_line()
+    {
+        if ($this->request->isAjax()) {
+            $json['xcolumnData'] = ['2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', '2020-07-08'];
+            $json['column'] = ['平光镜', '太阳镜'];
+            $json['columnData'] = [
+                [
+                    'type' => 'line',
+                    'data' => [430, 550, 800, 650, 410, 520, 430, 870],
+                    'name' => '平光镜',
+                    'smooth' => true //平滑曲线
+                ],
+                [
+                    'type' => 'line',
+                    'data' => [100, 260, 450, 400, 400, 650, 730, 800],
+                    'name' => '太阳镜',
+                    'smooth' => true //平滑曲线
+                ],
+
+            ];
+
+            return json(['code' => 1, 'data' => $json]);
+        }
+    }
 }
--- public/assets/js/backend/operatedatacenter/goodsdata/goods_data_view.js
+++ public/assets/js/backend/operatedatacenter/goodsdata/goods_data_view.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'echartsobj'], function ($, undefined, Backend, Table, Form, EchartObj) {
 
     var Controller = {
         index: function () {
@@ -21,6 +21,8 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 $('.nihao-div').show();
             }
 
+            Controller.api.formatter.line_chart();
+            Controller.api.formatter.goods_type_chart();
         },
         add: function () {
             Controller.api.bindevent();
@@ -31,24 +33,83 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         api: {
             formatter: {
                 line_chart: function () {
-                    //订单数据概况折线图
                     var chartOptions = {
                         targetId: 'echart',
                         downLoadTitle: '图表',
-                        type: 'line'
+                        type: 'bar',
+                        bar: {
+                            tooltip: { //提示框组件。
+                                trigger: 'axis', // 触发类型。可选项item:数据项图形触发，主要在散点图，饼图等无类目轴的图表中使用。axis:坐标轴触发，主要在柱状图，折线图等会使用类目轴的图表中使用。
+                                axisPointer: { //坐标轴指示器配置项。
+                                    type: 'shadow' //指示器类型。可选项'line' 直线指示器。'shadow' 阴影指示器。'cross' 十字准星指示器。其实是种简写，表示启用两个正交的轴的 axisPointer。
+                                },
+                                formatter: function (param) { //格式化提示信息
+                                    console.log(param);
+                                    return param[0].name + '<br/>' + param[0].seriesName + '：' + param[0].value + '<br/>' + param[1].seriesName + '：' + param[1].value;
+                                }
+                            },
+                            grid: { //直角坐标系内绘图网格
+                                top: '10%', //grid 组件离容器上侧的距离。
+                                left: '5%', //grid 组件离容器左侧的距离。
+                                right: '10%', //grid 组件离容器右侧的距离。
+                                bottom: '10%', //grid 组件离容器下侧的距离。
+                                containLabel: true //grid 区域是否包含坐标轴的刻度标签。
+                            },
+                            legend: { //图例配置
+                                padding: 5,
+                                top: '2%',
+                                data: ['镜框销量', '副单价']
+                            },
+                            xAxis: [
+                                {
+                                    type: 'category'
+                                }
+                            ],
+                            yAxis: [
+                                {
+                                    type: 'value',
+                                    name: '镜框销量',
+                                    axisLabel: {
+                                        formatter: '{value} 个'
+                                    }
+                                },
+                                {
+                                    type: 'value',
+                                    name: '副单价',
+                                    axisLabel: {
+                                        formatter: '{value} ¥'
+                                    }
+                                }
+                            ],
+                        }
                     };
 
                     var options = {
                         type: 'post',
-                        url: 'operatedatacenter/goodsdata/goods_data_view/goods_data_view_line',
+                        url: 'operatedatacenter/goodsdata/goods_data_view/goods_sales_data_line',
                         data: {
-                            order_platform: $("#order_platform").val(),
-                            time_str: $("#time_str").val(),
-                            type: $("#type").val()
+
                         }
+
                     }
                     EchartObj.api.ajax(options, chartOptions)
-                }
+                },
+                goods_type_chart: function () {
+                    var chartOptions = {
+                        targetId: 'echart2',
+                        downLoadTitle: '图表',
+                        type: 'line'
+                    };
+                    var options = {
+                        type: 'post',
+                        url: 'operatedatacenter/goodsdata/goods_data_view/goods_type_data_line',
+                        data: {
+
+                        }
+
+                    }
+                    EchartObj.api.ajax(options, chartOptions)
+                },
             },
             bindevent: function () {
                 Form.api.bindevent($("form[role=form]"));
