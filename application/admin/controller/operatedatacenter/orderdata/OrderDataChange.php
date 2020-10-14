cb7fc3af6cf6d640f6aa98e80126a85602f8bbb5
wpl <wangpenglei@nextmar.com>
2020-10-14 15:46:52 +0800

    添加订单转化率分析

--- application/admin/controller/operatedatacenter/orderdata/OrderDataChange.php
+++ application/admin/controller/operatedatacenter/orderdata/OrderDataChange.php
@@ -9,13 +9,80 @@ use think\Request;
 class OrderDataChange extends Backend
 {
     /**
-     * 显示资源列表
+     * 订单数据-转化率分析
      *
      * @return \think\Response
      */
     public function index()
     {
-        //
+       
+        return $this->view->fetch();
+    }
+
+    /**
+     * 会话/销售额
+     *
+     * @Description
+     * @author wpl
+     * @since 2020/10/14 15:02:23 
+     * @return void
+     */
+    public function order_sales_data_line()
+    {
+        if ($this->request->isAjax()) {
+            $json['xcolumnData'] = ['2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', '2020-07-08'];
+            $json['column'] = ['会话数', '订单金额'];
+            $json['columnData'] = [
+                [
+                    'type' => 'line',
+                    'data' => [430, 550, 800, 650, 410, 520, 430, 870],
+                    'name' => '会话数',
+                    'smooth' => true //平滑曲线
+                ],
+                [
+                    'type' => 'line',
+                    'data' => [100, 260, 450, 400, 400, 650, 730, 800],
+                    'name' => '订单金额',
+                    'smooth' => true //平滑曲线
+                ],
+
+            ];
+
+            return json(['code' => 1, 'data' => $json]);
+        }
+    }
+
+    /**
+     * 购物车数量/订单数量
+     *
+     * @Description
+     * @author wpl
+     * @since 2020/10/14 15:02:23 
+     * @return void
+     */
+    public function order_num_data_line()
+    {
+        if ($this->request->isAjax()) {
+            $json['xcolumnData'] = ['2020-07-01', '2020-07-02', '2020-07-03', '2020-07-04', '2020-07-05', '2020-07-06', '2020-07-07', '2020-07-08'];
+            $json['column'] = ['购物车数量', '订单数量'];
+            $json['columnData'] = [
+                [
+                    'type' => 'line',
+                    'data' => [430, 550, 800, 650, 410, 520, 430, 870],
+                    'name' => '购物车数量',
+                    'smooth' => true //平滑曲线
+                ],
+                [
+                    'type' => 'line',
+                    'data' => [100, 260, 450, 400, 400, 650, 730, 800],
+                    'name' => '订单数量',
+                    'smooth' => true //平滑曲线
+                ],
+
+            ];
+
+            return json(['code' => 1, 'data' => $json]);
+        }
     }
 
 
--- public/assets/js/backend/operatedatacenter/orderdata/order_data_change.js
+++ public/assets/js/backend/operatedatacenter/orderdata/order_data_change.js
@@ -1,9 +1,17 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'echartsobj'], function ($, undefined, Backend, Table, Form, EchartObj) {
 
     var Controller = {
         index: function () {
+            Controller.api.bindevent();
+
             // 初始化表格参数配置
             Table.api.init({
+                commonSearch: false,
+                search: false,
+                showExport: false,
+                showColumns: false,
+                showToggle: false,
+                pagination: false,
                 extend: {
                     index_url: 'operatedatacenter/orderdata/order_data_change/index' + location.search,
                     add_url: 'operatedatacenter/orderdata/order_data_change/add',
@@ -23,15 +31,26 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 sortName: 'id',
                 columns: [
                     [
-                        {checkbox: true},
-                        {field: 'id', title: __('Id')},
-                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                        { checkbox: true },
+                        { field: 'id', title: __('日期') },
+                        { field: 'id', title: __('会话数') },
+                        { field: 'id', title: __('加购率') },
+                        { field: 'id', title: __('会话转化率') },
+                        { field: 'id', title: __('订单数') },
+                        { field: 'id', title: __('客单价') },
+                        { field: 'id', title: __('新增购物车数量') },
+                        { field: 'id', title: __('更新购物车数量') },
+                        { field: 'id', title: __('订单金额') },
+                        { field: 'id', title: __('注册量') }
                     ]
                 ]
             });
 
             // 为表格绑定事件
             Table.api.bindevent(table);
+
+            Controller.api.formatter.order_sales_data_line();
+            Controller.api.formatter.order_num_data_line();
         },
         add: function () {
             Controller.api.bindevent();
@@ -40,6 +59,41 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             Controller.api.bindevent();
         },
         api: {
+            formatter: {
+
+                order_sales_data_line: function () {
+                    var chartOptions = {
+                        targetId: 'echart',
+                        downLoadTitle: '图表',
+                        type: 'line'
+                    };
+                    var options = {
+                        type: 'post',
+                        url: 'operatedatacenter/orderdata/order_data_change/order_sales_data_line',
+                        data: {
+
+                        }
+
+                    }
+                    EchartObj.api.ajax(options, chartOptions)
+                },
+                order_num_data_line: function () {
+                    var chartOptions = {
+                        targetId: 'echart2',
+                        downLoadTitle: '图表',
+                        type: 'line'
+                    };
+                    var options = {
+                        type: 'post',
+                        url: 'operatedatacenter/orderdata/order_data_change/order_num_data_line',
+                        data: {
+
+                        }
+
+                    }
+                    EchartObj.api.ajax(options, chartOptions)
+                },
+            },
             bindevent: function () {
                 Form.api.bindevent($("form[role=form]"));
             }
