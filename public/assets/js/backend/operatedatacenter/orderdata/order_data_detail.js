623e199940a74fdf227d41a506320b3fd4bd3c1c
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-10-16 18:54:28 +0800

    1

--- public/assets/js/backend/operatedatacenter/orderdata/order_data_detail.js
+++ public/assets/js/backend/operatedatacenter/orderdata/order_data_detail.js
@@ -13,23 +13,61 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                     table: 'order_data_detail',
                 }
             });
-
+            Controller.api.formatter.daterangepicker($("div[role=form]"));
             var table = $("#table");
-
             // 初始化表格
             table.bootstrapTable({
                 url: $.fn.bootstrapTable.defaults.extend.index_url,
-                pk: 'id',
-                sortName: 'id',
+                pk: 'increment_id',
+                sortName: 'increment_id',
                 columns: [
                     [
-                        {checkbox: true},
-                        {field: 'id', title: __('Id')},
-                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    
+                        {field: 'increment_id', title: __('订单编号')},
+                        {field: 'created_at', title: __('订单时间')},
+                        {field: 'base_grand_total', title: __('订单金额')},
+                        {field: 'base_shipping_amount', title: __('邮费')},
+                        {field: 'status', title: __('订单状态')},
+                        //{field: 'assign_id', title: __('Assgin_id'),operate: false,visible:false},
+                        {field: 'store_id', title: __('设备类型'), custom: { 1: 'danger', 2: 'success', 3: 'blue', 4: 'orange'}, searchList: { 1: 'PC', 4: 'M', 5: 'IOS', 6: 'Android'}, formatter: Table.api.formatter.store_id,visible: false},
+                        {field: 'protect_code',title: __('使用的code码'),visible: false},
+                        {field: 'shipping_method', title: __('快递类别'), custom: { 1: 'danger', 2: 'success'}, searchList: { 1: '平邮', 2: '商业快递'}, formatter: Table.api.formatter.store_id,visible: false},
+                        {field: 'shipping_name',title: __('收获姓名'),visible: false},
+                        {field: 'customer_email',title: __('支付邮箱'),visible: false},
+                        {field: 'customer_type',title: __('客户类型'),visible: false},
+                        {field: 'discount_rate',title: __('折扣百分比'),visible: false},
+                        {field: 'discount_money',title: __('折扣金额'),visible: false},
+                        {field: 'is_refund', title: __('有无退款'), custom: { 1: 'danger', 2: 'success'}, searchList: { 1: '有', 2: '无'}, formatter: Table.api.formatter.store_id,visible: false},
+                        {field: 'country_id',title: __('收获国家'),visible: false},
+                        {field: 'payment_method',title: __('支付方式'),visible: false},
+                        {field: 'frame_price',title: __('镜框价格'),visible: false},
+                        {field: 'frame_num',title: __('镜框数量'),visible: false},
+                        {field: 'lens_num',title: __('镜片数量'),visible: false},
+                        {field: 'is_box_num',title: __('配饰数量'),visible: false},
+                        {field: 'lens_price',title: __('镜片价格'),visible: false},
+                        {field: 'telephone',title: __('客户电话'),visible: false},
+                        {field: 'sku',title: __('商品sku'),visible: false},
+                        {field: 'register_time',title: __('注册时间'),visible: false},
+                        {field: 'register_email',title: __('注册邮箱'),visible: false},
+                        {field: 'work_list_num',title: __('工单数'),visible: false},
                     ]
                 ]
             });
-
+        
+            $('#table tr').find(`th`).hide();
+            $('#table tr').find(`td`).hide();
+            $('.nav-choose ul li ul li').click(function(e){
+                var clickNum = $(e.target).index();
+                if ($(e.target).children('input').prop('checked')) {
+                    $(e.target).children('input').prop('checked',false)
+                    $('#table tr').find(`th:eq(${clickNum})`).hide();
+                    $('#table tr').find(`td:eq(${clickNum})`).hide();
+                } else {
+                    $(e.target).children('input').prop('checked',true)
+                    $('#table tr').find(`th:eq(${clickNum})`).show();
+                    $('#table tr').find(`td:eq(${clickNum})`).show();
+                }
+            })
             // 为表格绑定事件
             Table.api.bindevent(table);
         },
@@ -40,6 +78,52 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             Controller.api.bindevent();
         },
         api: {
+            formatter: {
+                daterangepicker: function (form) {
+                    //绑定日期时间元素事件
+                    if ($(".datetimerange", form).size() > 0) {
+                        require(['bootstrap-daterangepicker'], function () {
+                            var ranges = {};
+                            ranges[__('Today')] = [Moment().startOf('day'), Moment().endOf('day')];
+                            ranges[__('Yesterday')] = [Moment().subtract(1, 'days').startOf('day'), Moment().subtract(1, 'days').endOf('day')];
+                            ranges[__('Last 7 Days')] = [Moment().subtract(6, 'days').startOf('day'), Moment().endOf('day')];
+                            ranges[__('Last 30 Days')] = [Moment().subtract(29, 'days').startOf('day'), Moment().endOf('day')];
+                            ranges[__('This Month')] = [Moment().startOf('month'), Moment().endOf('month')];
+                            ranges[__('Last Month')] = [Moment().subtract(1, 'month').startOf('month'), Moment().subtract(1, 'month').endOf('month')];
+                            var options = {
+                                timePicker: false,
+                                autoUpdateInput: false,
+                                timePickerSeconds: true,
+                                timePicker24Hour: true,
+                                autoApply: true,
+                                locale: {
+                                    format: 'YYYY-MM-DD HH:mm:ss',
+                                    customRangeLabel: __("Custom Range"),
+                                    applyLabel: __("Apply"),
+                                    cancelLabel: __("Clear"),
+                                },
+                                ranges: ranges,
+                                timePicker: true,
+                                timePickerIncrement: 1
+                            };
+                            var origincallback = function (start, end) {
+                                $(this.element).val(start.format(this.locale.format) + " - " + end.format(this.locale.format));
+                                $(this.element).trigger('blur');
+                            };
+                            $(".datetimerange", form).each(function () {
+                                var callback = typeof $(this).data('callback') == 'function' ? $(this).data('callback') : origincallback;
+                                $(this).on('apply.daterangepicker', function (ev, picker) {
+                                    callback.call(picker, picker.startDate, picker.endDate);
+                                });
+                                $(this).on('cancel.daterangepicker', function (ev, picker) {
+                                    $(this).val('').trigger('blur');
+                                });
+                                $(this).daterangepicker($.extend({}, options, $(this).data()), callback);
+                            });
+                        });
+                    }
+                },
+            },
             bindevent: function () {
                 Form.api.bindevent($("form[role=form]"));
             }
