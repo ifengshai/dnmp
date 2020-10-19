163161306ee15acbea4adaaba669d0a2a611cec4
lzh <1319617481@qq.com>
2020-10-19 18:06:25 +0800

    导出z站产品结构分析表格

--- application/admin/controller/Test01.php
+++ application/admin/controller/Test01.php
@@ -18,6 +18,7 @@ class Test01 extends Backend
 
     public function test01()
     {
+        set_time_limit (0);
         $_item_platform_sku = new \app\admin\model\itemmanage\ItemPlatformSku();
         $sku_data = $_item_platform_sku
             ->field('sku,grade,platform_sku')
