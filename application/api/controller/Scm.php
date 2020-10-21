39345af062880c9a490b6c68b203ca62b0e43e83
lzh <1319617481@qq.com>
2020-10-21 14:09:29 +0800

    供应链接口类调整

--- application/api/controller/Scm.php
+++ application/api/controller/Scm.php
@@ -11,6 +11,36 @@ class Scm extends Api
 {
     protected $noNeedLogin = ['login'];
     protected $noNeedRight = '*';
+    protected $menu = [//PDA菜单
+        [
+            'title'=>'配货管理',
+            'menu'=>[
+                ['name'=>'配货', 'link'=>''],
+                ['name'=>'镜片分拣', 'link'=>''],
+                ['name'=>'配镜片', 'link'=>''],
+                ['name'=>'加工', 'link'=>''],
+                ['name'=>'成品质检', 'link'=>''],
+                ['name'=>'合单', 'link'=>''],
+                ['name'=>'审单', 'link'=>''],
+                ['name'=>'跟单', 'link'=>''],
+                ['name'=>'工单', 'link'=>'']
+            ],
+        ],
+        [
+            'title'=>'质检管理',
+            'menu'=>[
+                ['name'=>'质检单', 'link'=>'warehouse/check'],
+                ['name'=>'物流检索', 'link'=>'warehouse/logistics_info/index']
+            ],
+        ],
+        [
+            'title'=>'出入库管理',
+            'menu'=>[
+                ['name'=>'出库单', 'link'=>'warehouse/outstock'],
+                ['name'=>'入库单', 'link'=>'warehouse/instock']
+            ],
+        ],
+    ];
 
     public function _initialize()
     {
@@ -18,6 +48,9 @@ class Scm extends Api
 
         //校验Token
         $this->auth->match($this->noNeedLogin) || $this->check() || $this->error(__('Token invalid, please log in again'), [], 401);
+
+        //校验请求类型
+        $this->request->isPost() || $this->error(__('Request method must be post'), [], 402);
     }
 
     /**
@@ -43,15 +76,15 @@ class Scm extends Api
     {
         $account = $this->request->request('account');
         $password = $this->request->request('password');
-        empty($account) && $this->error(__('Username can not be empty'), [], 402);
-        empty($password) && $this->error(__('Password can not be empty'), [], 403);
+        empty($account) && $this->error(__('Username can not be empty'), [], 403);
+        empty($password) && $this->error(__('Password can not be empty'), [], 404);
 
         if ($this->auth->login($account, $password)) {
             $user = $this->auth->getUserinfo();
             $data = ['token' => $user['token']];
             $this->success(__('Logged in successful'), $data,200);
         } else {
-            $this->error($this->auth->getError(), [], 404);
+            $this->error($this->auth->getError(), [], 405);
         }
     }
 
@@ -62,40 +95,9 @@ class Scm extends Api
      */
     public function index()
     {
-        $list = [
-            [
-               'title'=>'配货管理',
-               'menu'=>[
-                   ['name'=>'配货', 'link'=>''],
-                   ['name'=>'镜片分拣', 'link'=>''],
-                   ['name'=>'配镜片', 'link'=>''],
-                   ['name'=>'加工', 'link'=>''],
-                   ['name'=>'成品质检', 'link'=>''],
-                   ['name'=>'合单', 'link'=>''],
-                   ['name'=>'审单', 'link'=>''],
-                   ['name'=>'跟单', 'link'=>''],
-                   ['name'=>'工单', 'link'=>'']
-               ],
-            ],
-            [
-                'title'=>'质检管理',
-                'menu'=>[
-                    ['name'=>'质检单', 'link'=>'warehouse/check'],
-                    ['name'=>'物流检索', 'link'=>'warehouse/logistics_info/index']
-                ],
-            ],
-            [
-                'title'=>'出入库管理',
-                'menu'=>[
-                    ['name'=>'出库单', 'link'=>'warehouse/outstock'],
-                    ['name'=>'入库单', 'link'=>'warehouse/instock']
-                ],
-            ],
-        ];
-
         //重新组合菜单
-        $data = [];
-        foreach($list as $key=>$value){
+        $list = [];
+        foreach($this->menu as $key=>$value){
             foreach($value['menu'] as $k=>$val){
                 //校验菜单展示权限
                 if(!$this->auth->check($val['link'])){
@@ -104,11 +106,11 @@ class Scm extends Api
                 unset($val['link']);
             }
             if(!empty($value['menu'])){
-                $data[] = $value;
+                $list[] = $value;
             }
         }
 
-        $this->success('', ['list' => $data],200);
+        $this->success('', ['list' => $list],200);
     }
 
 }
