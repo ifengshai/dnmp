cd6651118a1b1eeffb32a74d752aa5ec078a971f
lzh <1319617481@qq.com>
2020-10-21 10:50:52 +0800

    pc端： 质检单列表、编辑页面 出库单列表、详情页面 入库单列表 接口： 首页

--- application/api/controller/Scm.php
+++ application/api/controller/Scm.php
@@ -55,4 +55,59 @@ class Scm extends Api
         }
     }
 
+    /**
+     * 首页
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        $list = [
+            [
+               'title'=>'配货管理',
+               'menu'=>[
+                   ['name'=>'配货', 'link'=>''],
+                   ['name'=>'镜片分拣', 'link'=>''],
+                   ['name'=>'配镜片', 'link'=>''],
+                   ['name'=>'加工', 'link'=>''],
+                   ['name'=>'成品质检', 'link'=>''],
+                   ['name'=>'合单', 'link'=>''],
+                   ['name'=>'审单', 'link'=>''],
+                   ['name'=>'跟单', 'link'=>''],
+                   ['name'=>'工单', 'link'=>'']
+               ],
+            ],
+            [
+                'title'=>'质检管理',
+                'menu'=>[
+                    ['name'=>'质检单', 'link'=>'warehouse/check'],
+                    ['name'=>'物流检索', 'link'=>'warehouse/logistics_info/index']
+                ],
+            ],
+            [
+                'title'=>'出入库管理',
+                'menu'=>[
+                    ['name'=>'出库单', 'link'=>'warehouse/outstock'],
+                    ['name'=>'入库单', 'link'=>'warehouse/instock']
+                ],
+            ],
+        ];
+
+        //重新组合菜单
+        $data = [];
+        foreach($list as $key=>$value){
+            foreach($value['menu'] as $k=>$val){
+                //校验菜单展示权限
+                if(!$this->auth->check($val['link'])){
+                    unset($list[$key]['menu'][$k]);
+                }
+            }
+            if(!empty($value['menu'])){
+                $data[] = $value;
+            }
+        }
+
+        $this->success('', $data,200);
+    }
+
 }
