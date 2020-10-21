709255cdc49ec83552aed8ed5f22f0ff41d1897e
wpl <wangpenglei@nextmar.com>
2020-10-21 14:50:45 +0800

    1

--- application/admin/controller/shell/TrackReg.php
+++ application/admin/controller/shell/TrackReg.php
@@ -147,7 +147,7 @@ class TrackReg extends Backend
         } elseif (stripos($title, 'cpc') !== false) {
             $carrierId = 'cpc';
             $title = 'Canada Post';
-        }
+        } 
         $carrier = [
             'dhl' => '100001',
             'chinapost' => '03011',
@@ -155,7 +155,8 @@ class TrackReg extends Backend
             'cpc' => '03041',
             'fedex' => '100003',
             'usps' => '21051',
-            'yanwen' => '190012'
+            'yanwen' => '190012',
+            'eub' => '03011',
         ];
         if ($carrierId) {
             return ['title' => $title, 'carrierId' => $carrier[$carrierId]];
