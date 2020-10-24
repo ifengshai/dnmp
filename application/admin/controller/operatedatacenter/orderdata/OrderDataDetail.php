b79f06599de80d06616b9c126e574d59e1241fe0
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-10-24 14:33:33 +0800

    1

--- application/admin/controller/operatedatacenter/orderdata/OrderDataDetail.php
+++ application/admin/controller/operatedatacenter/orderdata/OrderDataDetail.php
@@ -88,11 +88,11 @@ class OrderDataDetail extends Backend
             $list = collection($list)->toArray();
             $i = 0;
             foreach ($list as $key=>$value){
-                $data[$key]['increment_id'] = $value['increment_id'];
-                $data[$key]['created_at'] = $value['created_at'];
-                $data[$key]['base_grand_total'] = $value['base_grand_total'];
-                $data[$key]['base_shipping_amount'] = $value['base_shipping_amount'];
-                $data[$key]['status'] = $value['status'];
+                $list[$key]['increment_id'] = $value['increment_id'];
+                $list[$key]['created_at'] = $value['created_at'];
+                $list[$key]['base_grand_total'] = $value['base_grand_total'];
+                $list[$key]['base_shipping_amount'] = $value['base_shipping_amount'];
+                $list[$key]['status'] = $value['status'];
                 switch ($value['store_id']){
                     case 1:
                         $store_id = 'PC';
@@ -107,15 +107,15 @@ class OrderDataDetail extends Backend
                         $store_id = 'Android';
                         break;
                 }
-                $data[$key]['store_id'] = $store_id;
-                $data[$key]['protect_code'] = $value['protect_code'];
-                $data[$key]['shipping_method'] = $value['shipping_method'];  //快递类别
+                $list[$key]['store_id'] = $store_id;
+                $list[$key]['protect_code'] = $value['protect_code'];
+                $list[$key]['shipping_method'] = $value['shipping_method'];  //快递类别
                 //收货信息
                 $shipping_where['address_type'] = 'shipping';
                 $shipping_where['parent_id'] = $value['entity_id'];
                 $shipping = $web_model->table('sales_flat_order_address')->where($shipping_where)->field('firstname,lastname,telephone,country_id')->find();
-                $data[$key]['shipping_name'] = $shipping['firstname'].''.$shipping['lastname'];  //收货姓名
-                $data[$key]['customer_email'] = $value['customer_email'];   //支付邮箱
+                $list[$key]['shipping_name'] = $shipping['firstname'].''.$shipping['lastname'];  //收货姓名
+                $list[$key]['customer_email'] = $value['customer_email'];   //支付邮箱
                 //客户信息
                 if($value['customer_id']){
                     $customer_where['entity_id'] = $value['customer_id'];
@@ -138,9 +138,9 @@ class OrderDataDetail extends Backend
                     $register_time = '';
                     $register_email = '';
                 }
-                $data[$key]['customer_type'] = $group;   //客户类型
-                $data[$key]['discount_rate'] = $value['base_grand_total'] ? round(($value['base_discount_amount']/$value['base_grand_total']),2).'%' : 0;  //折扣百分比
-                $data[$key]['discount_money'] = round($value['base_discount_amount'],2);  //折扣金额
+                $list[$key]['customer_type'] = $group;   //客户类型
+                $list[$key]['discount_rate'] = $value['base_grand_total'] ? round(($value['base_discount_amount']/$value['base_grand_total']),2).'%' : 0;  //折扣百分比
+                $list[$key]['discount_money'] = round($value['base_discount_amount'],2);  //折扣金额
                 $work_list_where['platform_order'] = $value['increment_id'];
                 $work_list = Db::name('work_order_list')->where($work_list_where)->field('id,is_refund')->select();
                 $work_list = collection($work_list)->toArray();
@@ -151,31 +151,31 @@ class OrderDataDetail extends Backend
                 }else{
                     $is_refund = '无';
                 }
-                $data[$key]['is_refund'] = $is_refund;  //是否退款
-                $data[$key]['country_id'] = $shipping['country_id'];   //收货国家
+                $list[$key]['is_refund'] = $is_refund;  //是否退款
+                $list[$key]['country_id'] = $shipping['country_id'];   //收货国家
                 //支付信息
                 $payment_where['parent_id'] = $value['entity_id'];
                 $payment = $web_model->table('sales_flat_order_payment')->where($payment_where)->value('method');
-                $data[$key]['payment_method'] =  $payment == 'oceanpayment_creditcard' ? '钱海' : 'Paypal';  //支付方式
+                $list[$key]['payment_method'] =  $payment == 'oceanpayment_creditcard' ? '钱海' : 'Paypal';  //支付方式
                 //处方信息
                 $prescription_where['order_id'] = $value['entity_id'];
                 $frame_price = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->sum('frame_price');
-                $data[$key]['frame_price'] = round($frame_price,2);
-                $data[$key]['frame_num'] = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->count();
-                $data[$key]['lens_num'] = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->where('index_id','neq','')->count();
-                $data[$key]['is_box_num'] = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->where('goods_type',6)->count();
+                $list[$key]['frame_price'] = round($frame_price,2);
+                $list[$key]['frame_num'] = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->count();
+                $list[$key]['lens_num'] = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->where('index_id','neq','')->count();
+                $list[$key]['is_box_num'] = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->where('goods_type',6)->count();
                 $lens_price = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->sum('index_price');
-                $data[$key]['lens_price'] = round($lens_price,2);
-                $data[$key]['telephone'] = $shipping['telephone'];
+                $list[$key]['lens_price'] = round($lens_price,2);
+                $list[$key]['telephone'] = $shipping['telephone'];
                 $skus = $web_model->table('sales_flat_order_item_prescription')->where($prescription_where)->column('sku');
                 $skus = collection($skus)->toArray();
-                $data[$key]['sku'] = implode(',',$skus);
-                $data[$key]['register_time'] = $register_time;
-                $data[$key]['register_email'] = $register_email;
-                $data[$key]['work_list_num'] = $work_list_num;
+                $list[$key]['sku'] = implode(',',$skus);
+                $list[$key]['register_time'] = $register_time;
+                $list[$key]['register_email'] = $register_email;
+                $list[$key]['work_list_num'] = $work_list_num;
                 $i++;
             }
-            $result = array("total" => $total, "rows" => $data);
+            $result = array("total" => $total, "rows" => $list);
 
             return json($result);
         }
