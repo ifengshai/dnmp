bf78045ac5b006cba9d0cf4135b7a40963fe3a1d
lzh <1319617481@qq.com>
2020-10-26 17:04:27 +0800

    配货操作日志 标记异常 配镜片扫码、配镜片提交 加工扫码、加工提交
    接口完成

---
+++ application/admin/model/order/order/NewOrderItemOption.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\admin\model\order\order;
+
+use think\Model;
+
+class NewOrderItemOption extends Model
+{
+    //数据库
+    protected $connection = 'database.db_new_order';
+
+    // 表名
+    protected $table = 'order_item_option';
+
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = false;
+
+    // 定义时间戳字段名
+    protected $createTime = false;
+    protected $updateTime = false;
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [];
+    
+}
