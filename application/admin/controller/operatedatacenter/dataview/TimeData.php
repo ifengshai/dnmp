5a5d9511c8109c5e47f3bb8e7087d64fc52f10ed
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-10-26 14:49:51 +0800

    1

--- application/admin/controller/operatedatacenter/dataview/TimeData.php
+++ application/admin/controller/operatedatacenter/dataview/TimeData.php
@@ -3,7 +3,9 @@
 namespace app\admin\controller\operatedatacenter\dataview;
 
 use app\common\controller\Backend;
+use think\Cache;
 use think\Controller;
+use think\Db;
 use think\Request;
 
 class TimeData extends Backend
@@ -15,9 +17,111 @@ class TimeData extends Backend
      */
     public function index()
     {
+        $time_str = input('time_str');
+        if(!$time_str){
+            $start = date('Y-m-d 00:00:00', strtotime('-6 day'));
+            $end   = date('Y-m-d 23:59:59');
+            $time_between = $start.' - '.$end;
+            $time_show = '';
+        }else{
+            $time_between = $time_str;
+            $time_show = $time_str;
+        }
+        $web_site = input('order_platform') ? input('order_platform') : 1;
+        //获取session
+        $session = $this->initializeAnalytics($web_site,$start,$end);
+        dump($session);exit;
+        //查询对应平台权限
+        $this->magentoplatform = new \app\admin\model\platformmanage\MagentoPlatform();
+        $magentoplatformarr = $this->magentoplatform->getAuthSite();
+        foreach ($magentoplatformarr as $key=>$val){
+            if(!in_array($val['name'],['zeelool','voogueme','nihao'])){
+                unset($magentoplatformarr[$key]);
+            }
+        }
+        $this->view->assign(compact('data', 'total', 'coating_arr','coating_count','web_site','time_show','magentoplatformarr'));
         return $this->view->fetch();
     }
+    protected function initializeAnalytics($site,$start_time,$end_time)
+    {
+        $client = new \Google_Client();
+        $client->setAuthConfig('./oauth/oauth-credentials.json');
+        $client->addScope(\Google_Service_Analytics::ANALYTICS_READONLY);
+        // Create an authorized analytics service object.
+        $analytics = new \Google_Service_AnalyticsReporting($client);
+        // $analytics = $this->initializeAnalytics();
+        // Call the Analytics Reporting API V4.
+        $response = $this->getReport_session($site,$analytics, $start_time, $end_time);
 
+        // dump($response);die;
+
+        // Print the response.
+        $result = $this->printResults($response);
+
+        return $result;
+    }
+    protected function getReport_session($site, $analytics, $startDate, $endDate)
+    {
+        if ($site == 1) {
+            $VIEW_ID = config('ZEELOOL_GOOGLE_ANALYTICS_VIEW_ID');
+        } elseif ($site == 2) {
+            $VIEW_ID = config('VOOGUEME_GOOGLE_ANALYTICS_VIEW_ID');
+        } elseif ($site == 3) {
+            $VIEW_ID = config('NIHAO_GOOGLE_ANALYTICS_VIEW_ID');
+        }
+
+        $dateRange = new \Google_Service_AnalyticsReporting_DateRange();
+        $dateRange->setStartDate($startDate);
+        $dateRange->setEndDate($endDate);
+
+        $adCostMetric = new \Google_Service_AnalyticsReporting_Metric();
+        $adCostMetric->setExpression("ga:sessions");
+        $adCostMetric->setAlias("ga:sessions");
+        // $adCostMetric->setExpression("ga:adCost");
+        // $adCostMetric->setAlias("ga:adCost");
+        $sessionDayDimension = new \Google_Service_AnalyticsReporting_Dimension();
+        $sessionDayDimension->setName("ga:day");
+        $sessionDayDimension->setName("ga:dateHour");
+
+        // Create the ReportRequest object.
+        $request = new \Google_Service_AnalyticsReporting_ReportRequest();
+        $request->setViewId($VIEW_ID);
+        $request->setDateRanges($dateRange);
+        $request->setMetrics(array($adCostMetric));
+        $request->setDimensions(array($sessionDayDimension));
+
+        $body = new \Google_Service_AnalyticsReporting_GetReportsRequest();
+        $body->setReportRequests(array($request));
+        return $analytics->reports->batchGet($body);
+    }
+    protected function printResults($reports)
+    {
+        $finalResult = array();
+        for ($reportIndex = 0; $reportIndex < count($reports); $reportIndex++) {
+            $report = $reports[$reportIndex];
+            $header = $report->getColumnHeader();
+            $dimensionHeaders = $header->getDimensions();
+            $metricHeaders = $header->getMetricHeader()->getMetricHeaderEntries();
+            $rows = $report->getData()->getRows();
+            for ($rowIndex = 0; $rowIndex < count($rows); $rowIndex++) {
+                $row = $rows[$rowIndex];
+                $dimensions = $row->getDimensions();
+                $metrics = $row->getMetrics();
+                for ($i = 0; $i < count($dimensionHeaders) && $i < count($dimensions); $i++) {
+                    $finalResult[$rowIndex][$dimensionHeaders[$i]] = $dimensions[$i];
+                }
+
+                for ($j = 0; $j < count($metrics); $j++) {
+                    $values = $metrics[$j]->getValues();
+                    for ($k = 0; $k < count($values); $k++) {
+                        $entry = $metricHeaders[$k];
+                        $finalResult[$rowIndex][$entry->getName()] = $values[$k];
+                    }
+                }
+            }
+            return $finalResult;
+        }
+    }
     /**
      * 销售量
      *
