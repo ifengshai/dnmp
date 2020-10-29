ff3169eefb741952d4b7b3378f59d476b93fc2e2
wpl <wangpenglei@nextmar.com>
2020-10-29 16:52:33 +0800

    1

--- application/admin/controller/shell/OrderData.php
+++ application/admin/controller/shell/OrderData.php
@@ -118,6 +118,7 @@ class OrderData extends Backend
                         //拆解对象为数组，并根据业务需求处理数据
                         $payload = json_decode($message->payload, true);
                         $key = $message->key;
+                        dump($payload);
                         file_put_contents('/www/wwwroot/mojing_test/runtime/log/canal.log', serialize($payload) . "\n");
                         //根据kafka中不同key，调用对应方法传递处理数据
                         //对该条message进行处理，比如用户数据同步， 记录日志。
