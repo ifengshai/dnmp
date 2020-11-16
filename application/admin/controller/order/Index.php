73b1ea9315f61a3fe023d3cbe046352109df9d39
743090740@qq.com <lover1118to0000>
2020-11-16 17:47:07 +0800

    memory_limit  设置扩大

--- application/admin/controller/order/Index.php
+++ application/admin/controller/order/Index.php
@@ -324,7 +324,7 @@ class Index extends Backend  /*这里继承的是app\common\controller\Backend*/
      */
     public function account_order()
     {
-        ini_set('memory_limit','256M');
+        ini_set('memory_limit','512M');
         $label = $this->request->get('label', 1);
         //设置过滤方法
         $this->request->filter(['strip_tags']);
