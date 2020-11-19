2f52ce57c974c2349a93f7711d6ff6fa152dbca4
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-11-19 15:42:35 +0800

    用户价值rfm

--- application/admin/controller/operatedatacenter/userdata/UserValueRfm.php
+++ application/admin/controller/operatedatacenter/userdata/UserValueRfm.php
@@ -15,108 +15,122 @@ class UserValueRfm extends Backend
         $this->zeelool = new \app\admin\model\order\order\Zeelool();
         $this->voogueme = new \app\admin\model\order\order\Voogueme();
         $this->nihao = new \app\admin\model\order\order\Nihao();
-        $this->zeeloolOperate  = new \app\admin\model\operatedatacenter\Zeelool;
-        $this->vooguemeOperate  = new \app\admin\model\operatedatacenter\Voogueme;
-        $this->nihaoOperate  = new \app\admin\model\operatedatacenter\Nihao;
+        $this->zeeloolOperate = new \app\admin\model\operatedatacenter\Zeelool;
+        $this->vooguemeOperate = new \app\admin\model\operatedatacenter\Voogueme;
+        $this->nihaoOperate = new \app\admin\model\operatedatacenter\Nihao;
         $this->magentoplatform = new \app\admin\model\platformmanage\MagentoPlatform();
     }
 
     /**
-     * 订单数据概况
+     * 用户平台贡献分布
      *
      * @return \think\Response
      */
-    public function index()
+    public function user_contribution_distribution()
     {
         //查询对应平台权限
         $magentoplatformarr = $this->magentoplatform->getAuthSite();
-        foreach ($magentoplatformarr as $key=>$val){
-            if(!in_array($val['name'],['zeelool','voogueme','nihao'])){
+        foreach ($magentoplatformarr as $key => $val) {
+            if (!in_array($val['name'], ['zeelool', 'voogueme', 'nihao'])) {
                 unset($magentoplatformarr[$key]);
             }
         }
-        $this->assign('magentoplatformarr',$magentoplatformarr);
+        $this->assign('magentoplatformarr', $magentoplatformarr);
         return $this->view->fetch();
     }
+
     /*
      * ajax获取用户消费金额分布
      * */
-    public function ajax_user_order_amount(){
+    public function ajax_user_order_amount()
+    {
+
         if ($this->request->isAjax()) {
             $params = $this->request->param();
             $order_platform = $params['order_platform'];
-            $data1 = $this->getOrderAmountUserNum($order_platform,1);
-            $data2 = $this->getOrderAmountUserNum($order_platform,2);
-            $data3 = $this->getOrderAmountUserNum($order_platform,3);
-            $data4 = $this->getOrderAmountUserNum($order_platform,4);
-            $data5 = $this->getOrderAmountUserNum($order_platform,5);
-            $data6 = $this->getOrderAmountUserNum($order_platform,6);
-            $data = [$data1['percent'].'%',$data2['percent'].'%',$data3['percent'].'%',$data4['percent'].'%',$data5['percent'].'%',$data6['percent'].'%'];
+            $data1 = $this->getOrderAmountUserNum($order_platform, 1);
+            $data2 = $this->getOrderAmountUserNum($order_platform, 2);
+            $data3 = $this->getOrderAmountUserNum($order_platform, 3);
+            $data4 = $this->getOrderAmountUserNum($order_platform, 4);
+            $data5 = $this->getOrderAmountUserNum($order_platform, 5);
+            $data6 = $this->getOrderAmountUserNum($order_platform, 6);
 
-            $json['xColumnName'] = ['0-40','40-80','80-150','150-200','200-300','300+'];
-            $json['columnData'] = [
-                [
-                    'type' => 'bar',
-                    'barWidth' => '20%',
-                    'data' => $data,
-                    'name' => '占比'
-                ],
-
-            ];
-            return json(['code' => 1, 'data'=>$json]);
+            $data = array($data1['count'], $data2['count'], $data3['count'], $data4['count'], $data5['count'], $data6['count'],
+            );
+            $json['firtColumnName'] = ['0-40', '40-80', '80-150', '150-200', '200-300', '300+'];
+            $json['columnData'] = [[
+                'type' => 'bar',
+                'barWidth' => '40%',
+                'data' => $data,
+                'name' => $data1['sum_count'],
+                'itemStyle' => [
+                    'normal' => [
+                        'label' => [
+                            'show' => true,
+                            'position' => 'right',
+                            'formatter'=>"{c}"."人",
+                            'textStyle'=>[
+                                'color'=> 'black'
+                            ],
+                        ],
+                    ]
+                ]
+            ]];
+            return json(['code' => 1, 'data' => $json]);
         }
     }
+
     /*
      * 获取金额分布人数
      * type  1:[0-40)  2:[40-80)  3:[80-150)   4:[150-200)    5:[200-300)   6:[300,10000000)
     */
-    public function getOrderAmountUserNum($order_platform,$type){
-        if($order_platform == 2){
+    public function getOrderAmountUserNum($order_platform, $type)
+    {
+        if ($order_platform == 2) {
             $order_model = $this->voogueme;
-        }elseif($order_platform == 3){
+        } elseif ($order_platform == 3) {
             $order_model = $this->nihao;
-        }else{
+        } else {
             $order_model = $this->zeelool;
         }
         $today = date('Y-m-d');
         $start = date('Y-m-d', strtotime("$today -12 month"));
         $end = date('Y-m-d 23:59:59', strtotime($today));
-        $where['created_at'] = ['between', [$start,$end]];
+        $where['created_at'] = ['between', [$start, $end]];
         $where['status'] = ['in', ['free_processing', 'processing', 'complete', 'paypal_reversed', 'payment_review', 'paypal_canceled_reversal']];
-        switch ($type){
+        switch ($type) {
             case 1:
-                $order_where['base_grand_total'] = ['>=',0];
-                $order_where['base_grand_total'] = ['<',40];
+                $order_where1['base_grand_total'] = ['>=', 0];
+                $order_where2['base_grand_total'] = ['<', 40];
                 break;
             case 2:
-                $order_where['base_grand_total'] = ['>=',40];
-                $order_where['base_grand_total'] = ['<',80];
+                $order_where1['base_grand_total'] = ['>=', 40];
+                $order_where2['base_grand_total'] = ['<', 80];
                 break;
             case 3:
-                $order_where['base_grand_total'] = ['>=',80];
-                $order_where['base_grand_total'] = ['<',150];
+                $order_where1['base_grand_total'] = ['>=', 80];
+                $order_where2['base_grand_total'] = ['<', 150];
                 break;
             case 4:
-                $order_where['base_grand_total'] = ['>=',150];
-                $order_where['base_grand_total'] = ['<',200];
+                $order_where1['base_grand_total'] = ['>=', 150];
+                $order_where2['base_grand_total'] = ['<', 200];
                 break;
             case 5:
-                $order_where['base_grand_total'] = ['>=',200];
-                $order_where['base_grand_total'] = ['<',300];
+                $order_where1['base_grand_total'] = ['>=', 200];
+                $order_where2['base_grand_total'] = ['<', 300];
                 break;
             case 6:
-                $order_where['base_grand_total'] = ['>=',300];
-                $order_where['base_grand_total'] = ['<',1000000];
+                $order_where1['base_grand_total'] = ['>=', 300];
+                $order_where2['base_grand_total'] = ['<', 1000000];
                 break;
             default:
                 break;
         }
-        $count = $order_model->where($where)->where($order_where)->count();
+        $count = $order_model->where($where)->where($order_where1)->where($order_where2)->count();
         $sum_count = $order_model->where($where)->count();
-        $percent = $sum_count ? round($count/$sum_count*100,0) : 0;
         $arr = [
-            'count'=>$count,
-            'percent'=>$percent,
+            'count' => $count,
+            'sum_count' => $sum_count,
         ];
         return $arr;
     }
