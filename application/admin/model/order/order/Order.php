5d2c4e05eae6dd8538c3ee3e71e6dfd7ea0efae6
wpl <wangpenglei@nextmar.com>
2020-11-30 17:44:45 +0800

    跑销量数据

--- application/admin/model/order/order/Order.php
+++ application/admin/model/order/order/Order.php
@@ -71,4 +71,51 @@ class Order extends Model
             ->sum('b.qty_ordered');
         return $res;
     }
+
+
+    /**
+     * 根据SKU统计订单SKU销量
+     *
+     * @Description
+     * @author wpl
+     * @since 2020/08/01 11:57:38 
+     * @param [type] $sku sku
+     * @param [type] $where 条件
+     * @param [type] $site 站点
+     * @return void
+     */
+    public function getSkuSalesNumTest($sku, $where = [],$site)
+    {
+        if ($site == 1) {
+            $model = $this;
+        } elseif ($site == 2) {
+            $model = new \app\admin\model\order\order\Voogueme();
+        } elseif ($site == 3) {
+            $model = new \app\admin\model\order\order\Nihao();
+        } elseif ($site == 4) {
+            $model = new \app\admin\model\order\order\Meeloog();
+        } elseif ($site == 5) {
+            $model = new \app\admin\model\order\order\Weseeoptical();
+        }  elseif ($site == 9) {
+            $model = new \app\admin\model\order\order\ZeeloolEs();
+        }  elseif ($site == 10) {
+            $model = new \app\admin\model\order\order\ZeeloolDe();
+        }  elseif ($site == 11) {
+            $model = new \app\admin\model\order\order\ZeeloolJp();
+        } 
+
+        if ($sku) {
+            $map['b.sku'] = $sku;
+        } else {
+            $map['b.sku'] = ['not like', '%Price%'];
+        }
+        $map['a.status'] = ['in', ['free_processing', 'processing', 'paypal_reversed', 'paypal_canceled_reversal', 'complete']];
+        $res = $model
+            ->where($map)
+            ->where($where)
+            ->alias('a')
+            ->join(['sales_flat_order_item' => 'b'], 'a.entity_id=b.order_id')
+            ->sum('b.qty_ordered');
+        return $res;
+    }
 }
