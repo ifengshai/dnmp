fbd8f0377ac4ec0440f0b824080406aff1734fb0
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-12-02 15:56:22 +0800

    1

--- application/admin/controller/operatedatacenter/userdata/UserValueRfm.php
+++ application/admin/controller/operatedatacenter/userdata/UserValueRfm.php
@@ -321,7 +321,7 @@ class UserValueRfm extends Backend
         }
         $web_model->table('customer_entity')->query("set time_zone='+8:00'");
         $today = date('Y-m-d');
-        $start = date('Y-m-d', strtotime("$today -14 day"));
+        $start = date('Y-m-d', strtotime("$today -8 day"));
 //        $end = date('Y-m-d 23:59:59', strtotime($today));
         $end = date('2020-11-25 23:59:59', strtotime($today));
         /*dump($start);
