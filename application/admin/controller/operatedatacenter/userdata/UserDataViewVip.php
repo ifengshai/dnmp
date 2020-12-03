d233571e398dc3fbe1ed8b75dbea79acdaa82d40
miaojingjing <%dw4JWlzr1@8d&FN$$%b>
2020-12-03 14:30:48 +0800

    1

--- application/admin/controller/operatedatacenter/userdata/UserDataViewVip.php
+++ application/admin/controller/operatedatacenter/userdata/UserDataViewVip.php
@@ -26,32 +26,6 @@ class UserDataViewVip extends Backend
     }
 
     /**
-     *  获取指定日期段内每一天的日期
-     * @param Date $startdate 开始日期
-     * @param Date $enddate 结束日期
-     * @return Array
-     *
-     * Created by Phpstorm.
-     * User: jhh
-     * Date: 2020/10/13
-     * Time: 16:06:51
-     */
-    function getDateFromRange($startdate, $enddate)
-    {
-        $stimestamp = strtotime($startdate);
-        $etimestamp = strtotime($enddate);
-        // 计算日期段内有多少天
-        $days = ($etimestamp - $stimestamp) / 86400 + 1;
-        // 保存每天日期
-        $date = array();
-        for ($i = 0; $i < $days; $i++) {
-            $date[] = date('Y-m-d', $stimestamp + (86400 * $i));
-        }
-        return $date;
-    }
-
-
-    /**
      * Created by Phpstorm.
      * User: jhh
      * Date: 2020/10/13
