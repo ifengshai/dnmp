7e2124b09f28a15d58bc9e19e0b11785fe5263bc
wpl <wangpenglei@nextmar.com>
2020-12-04 15:25:29 +0800

    1

--- application/admin/controller/shell/OrderData.php
+++ application/admin/controller/shell/OrderData.php
@@ -1661,7 +1661,7 @@ class OrderData extends Backend
                     $options['sku'] = $val['sku'];
                     $options['qty'] = $val['qty_ordered'];
                     $options['base_row_total'] = $val['base_row_total'];
-                    $options['product_id'] = $val['product_id'];
+                    // $options['product_id'] = $val['product_id'];
                     $order_prescription_type = $options['order_prescription_type'];
                     unset($options['order_prescription_type']);
                     if ($options) {
