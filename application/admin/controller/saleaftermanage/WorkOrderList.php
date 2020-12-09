eac69d7611e6fba2632e6713a699d0fae9e48dbd
lzh <1319617481@qq.com>
2020-12-09 15:14:40 +0800

    解决工单编辑默认展示修改地址措施报错

--- application/admin/controller/saleaftermanage/WorkOrderList.php
+++ application/admin/controller/saleaftermanage/WorkOrderList.php
@@ -2256,10 +2256,8 @@ class WorkOrderList extends Backend
                     //获取魔晶数据库中地址
                     $address = Db::name('work_order_change_sku')->where('work_id', $work_id)->value('userinfo_option');
                     $address = unserialize($address);
-                    print_r($address);
-                    exit;
                     $address['address_type'] = $address['address_id'] == 0 ? 'shipping' : 'billing';
-                    $res['address'][$address['address_id']] = $address;
+                    $res['address'] = $address;
                 }
             } catch (\Exception $e) {
                 $this->error($e->getMessage());
