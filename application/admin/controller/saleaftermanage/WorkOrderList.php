d1a6569eb9a26a50a238e6bb4e2e50f9476b8a24
lzh <1319617481@qq.com>
2020-12-11 17:05:52 +0800

    解决工单详情展示修改地址信息不对的问题

--- application/admin/controller/saleaftermanage/WorkOrderList.php
+++ application/admin/controller/saleaftermanage/WorkOrderList.php
@@ -2270,7 +2270,7 @@ class WorkOrderList extends Backend
 
                 //判断是否是新建状态
                 $work_status = $this->model->where('id', $work_id)->value('work_status');
-                if (1 == $work_status) {
+                if (0 < $work_status) {
                     //获取魔晶数据库中地址
                     $address = Db::name('work_order_change_sku')->where('work_id', $work_id)->value('userinfo_option');
                     $address = unserialize($address);
