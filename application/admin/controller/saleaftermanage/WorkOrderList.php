86ed40dca1fa3bceebe3274b8b1f8086d1a40439
lzh <1319617481@qq.com>
2020-12-14 13:53:54 +0800

    测试

--- application/admin/controller/saleaftermanage/WorkOrderList.php
+++ application/admin/controller/saleaftermanage/WorkOrderList.php
@@ -1319,13 +1319,6 @@ class WorkOrderList extends Backend
                     }
                 }
 
-                if('360000212' == $platform_order){
-                    print_r($params['is_check']);
-                    print_r('------------');
-                    print_r($params['assign_user_id']);
-                    exit;
-                }
-
                 //没有审核人则不需要审核
                 if (!$params['assign_user_id']) {
                     $params['is_check'] = 0;
