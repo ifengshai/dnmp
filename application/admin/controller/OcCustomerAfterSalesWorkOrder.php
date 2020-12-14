9261b2fcea9848bea2797d56f413375704d47b8a
743090740@qq.com <lover1118to0000>
2020-12-14 17:56:20 +0800

    接口地址修改

--- application/admin/controller/OcCustomerAfterSalesWorkOrder.php
+++ application/admin/controller/OcCustomerAfterSalesWorkOrder.php
@@ -125,7 +125,7 @@ class OcCustomerAfterSalesWorkOrder extends Backend
             $save_question = $this->model->isUpdate(true, $where)->save(['status'=>$params['pm_audit_status'],'completed_at'=>date('Y-m-d H:i:s',time()),'handler_name'=>$this->auth->nickname]);
             if ($save_question){
                 //如果更新成功  提交接口
-                $url  = 'https://z.zhaokuangyi.com/magic/customer/updateTicket';
+                $url  =  config('url.zeelool_url').'/magic/customer/updateTicket';
                 $value['ticket_id'] = $params['ids'];
                 $value['status'] = $params['pm_audit_status'];
                 $curl = curl_init();
