de42cb8abc101832996ebba7f5aa48e59b0d5ee9
743090740@qq.com <lover1118to0000>
2020-12-15 17:12:42 +0800

    客户问题列表弹窗

--- application/admin/controller/OcCustomerAfterSalesWorkOrder.php
+++ application/admin/controller/OcCustomerAfterSalesWorkOrder.php
@@ -149,6 +149,10 @@ class OcCustomerAfterSalesWorkOrder extends Backend
             }
             $this->success('操作成功');
         }
+        $type = input('param.type');
+        if (empty($type)){
+            $type =0;
+        }
         $row  = \app\common\model\OcCustomerAfterSalesWorkOrder::get($ids)->toArray();
         if (!empty($row['images'])){
             $photo_href  =explode('|',$row['images']);
@@ -191,7 +195,31 @@ class OcCustomerAfterSalesWorkOrder extends Backend
             }
 
         }
+
         $row['email_message'] = $email;
+
+        if ($row['status'] ==1){
+            \db('oc_customer_after_sales_work_order')->where('id',$ids)->update(['status'=>2]);
+            $url  = config('url.zeelool_url').'magic/customer/updateTicket';
+            $value['ticket_id'] = $ids;
+            $value['status'] = 2;
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); //在HTTP请求中包含一个"User-Agent: "头的字符串。
+            curl_setopt($curl, CURLOPT_HEADER, 0); //启用时会将头文件的信息作为数据流输出。
+            curl_setopt($curl, CURLOPT_POST, true); //发送一个常规的Post请求
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $value);//Post提交的数据包
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); //启用时会将服务器服务器返回的"Location: "放在header中递归的返回给服务器，使用CURLOPT_MAXREDIRS可以限定递归返回的数量。
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); //文件流形式
+            curl_setopt($curl, CURLOPT_TIMEOUT, 20); //设置cURL允许执行的最长秒数。
+            $content =json_decode(curl_exec($curl),true);
+            curl_close($curl);
+            if ($content['code'] !==200){
+                $this->success('工单信息更新失败');
+            }
+        }
+
+        $this->assign('type',$type);
         $this->assign('row',$row);
         return $this->view->fetch();
     }
