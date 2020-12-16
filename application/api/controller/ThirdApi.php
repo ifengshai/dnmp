96de3c930f7aa487bf0ee6732a369bf5564e7044
743090740@qq.com <lover1118to0000>
2020-12-16 09:34:32 +0800

    no message

--- application/api/controller/ThirdApi.php
+++ application/api/controller/ThirdApi.php
@@ -63,9 +63,19 @@ class ThirdApi extends Api
     public function test(){
         $url = config('url.zeelool_url').'magic/order/updateOrderStatus';
         $value['increment_id']  = '100154077';
-        $a  = $this->post_json_data($url,$value);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); //在HTTP请求中包含一个"User-Agent: "头的字符串。
+        curl_setopt($curl, CURLOPT_HEADER, 0); //启用时会将头文件的信息作为数据流输出。
+        curl_setopt($curl, CURLOPT_POST, true); //发送一个常规的Post请求
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $value);//Post提交的数据包
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); //启用时会将服务器服务器返回的"Location: "放在header中递归的返回给服务器，使用CURLOPT_MAXREDIRS可以限定递归返回的数量。
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); //文件流形式
+        curl_setopt($curl, CURLOPT_TIMEOUT, 20); //设置cURL允许执行的最长秒数。
+        $content =json_decode(curl_exec($curl),true);
+        curl_close($curl);
         Log::write("输出接口返回信息");
-        Log::write($a);
+        Log::write($content);
     }
 
 
