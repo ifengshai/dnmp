e986853e602bee84eb6f16e6559b8eec01f7aa87
miaojingjing <mjj192015@gmail.com>
2020-12-21 10:42:59 +0800

    仪表盘修改

--- application/admin/controller/Crontab.php
+++ application/admin/controller/Crontab.php
@@ -3505,7 +3505,7 @@ class Crontab extends Backend
         $model->table('customer_entity')->query("set time_zone='+8:00'");
         //昨天销售额
         $order_where['order_type'] = 1;
-        $order_success_where['status'] = ['in', ['free_processing', 'processing', 'complete', 'paypal_reversed', 'payment_review', 'paypal_canceled_reversal']];
+        $order_success_where['status'] = ['in', ['free_processing', 'processing', 'complete', 'paypal_reversed', 'payment_review', 'paypal_canceled_reversal','delivered']];
         $yes_date = date("Y-m-d", strtotime("-1 day"));
         $yestime_where = [];
         $yestime_where1 = [];
