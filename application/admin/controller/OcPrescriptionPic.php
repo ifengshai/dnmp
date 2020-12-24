9f7a9e06f2d49406342b31a57a1baf65247b55b6
743090740@qq.com <lover1118to0000>
2020-12-24 18:53:30 +0800

    no message

--- application/admin/controller/OcPrescriptionPic.php
+++ application/admin/controller/OcPrescriptionPic.php
@@ -51,18 +51,30 @@ class OcPrescriptionPic extends Backend
             {
                 return $this->selectpage();
             }
-            $filter = json_decode($this->request->get('filter'), true);
 
+            $filter = json_decode($this->request->get('filter'), true);
+            $site = $filter['site'] ? $filter['site'] :1;
+            if ($site ==1){
+                $model = Db::connect('database.db_zeelool');
+            }else{
+                $model = Db::connect('database.db_voogueme');
+            }
+            unset($filter['site']);
             $this->request->get(['filter' => json_encode($filter)]);
+
             list($where, $sort, $order, $offset, $limit) = $this->buildparams();
-            $total = Db::table('oc_prescription_pics')->where($where)->count();
-            $list =  Db::table('oc_prescription_pics')->where($where)->order('created_at desc')->limit($offset, $limit)->select();
+
+            $total = $model->table('oc_prescription_pic')->where($where)->count();
+            $list = $model->table('oc_prescription_pic')->where($where)->order('id desc')->limit($offset, $limit)->select();
+
             foreach ($list as $key=>$item){
+
                 if ($item['status'] ==1){
                     $list[$key]['status']='未处理';
                 }else{
                     $list[$key]['status']= '已处理';
                 }
+                $list[$key]['site'] = $site;
                 $list[$key]['created_at'] =date("Y-m-d H:i:s",strtotime($item['created_at'])+28800);;
             }
 
