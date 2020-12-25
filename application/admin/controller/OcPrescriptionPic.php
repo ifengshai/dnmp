e5b08844b5bb94109970bf497b607aa58ace8619
743090740@qq.com <lover1118to0000>
2020-12-25 11:35:39 +0800

    no message

--- application/admin/controller/OcPrescriptionPic.php
+++ application/admin/controller/OcPrescriptionPic.php
@@ -69,7 +69,6 @@ class OcPrescriptionPic extends Backend
                 $WhereSql .= " and completion_time between '$completion_time[0]' and '$completion_time[1]' ";
             }
             $model  = Db::connect('database.db_zeelool');
-            dump($model);die();
             if ($filter['site']){
                 if ($filter['site'] ==1){
                     $count = "SELECT COUNT(1) FROM zeelool_test.oc_prescription_pic where".$WhereSql;
@@ -83,7 +82,7 @@ class OcPrescriptionPic extends Backend
             }else{
                 $count = "SELECT COUNT(1) FROM zeelool_test.oc_prescription_pic where".$WhereSql." union all  SELECT COUNT(1) FROM vuetest_voogueme.oc_prescription_pic where".$WhereSql;
                 $sql  = "SELECT * ,1 as site FROM zeelool_test.oc_prescription_pic where".$WhereSql." union all  SELECT * ,2 as site FROM vuetest_voogueme.oc_prescription_pic where".$WhereSql." limit  ". $offset.','.$limit;
-                $count = Db::connect('database.db_zeelool')->query($count);
+                $count = $model->query($count);
                 dump($count);
                 $total = $count[0]['COUNT(1)']  + $count[1]['COUNT(1)'];
             }
