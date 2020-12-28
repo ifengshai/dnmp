5c8dfeda5ce2ab81a8cccc4a99ef4c5b4144c9fe
miaojingjing <mjj192015@gmail.com>
2020-12-28 10:38:05 +0800

    1

--- application/admin/controller/supplydatacenter/DataMarket.php
+++ application/admin/controller/supplydatacenter/DataMarket.php
@@ -635,19 +635,13 @@ class DataMarket extends Backend
                     $map3['p.order_prescription_type'] = 3;
                     $where['o.status'] = ['in',['free_processing', 'processing', 'complete', 'paypal_reversed', 'payment_review', 'paypal_canceled_reversal','delivered']];
                     $sql1 = $this->process->alias('p')->join('fa_order o','p.increment_id = o.increment_id')->field('(p.complete_time-o.payment_time)/3600 AS total')->where($where)->where($map1)->group('p.order_id')->buildSql();
-                    $arr1 = $this->process->table([$sql1=>'t2'])->field('sum( IF ( total > 24, 1, 0) ) AS a,sum( IF ( total <= 24, 1, 0) ) AS b')->select(false);
-                    echo $arr1;exit;
+                    $arr1 = $this->process->table([$sql1=>'t2'])->field('sum( IF ( total > 24, 1, 0) ) AS a,sum( IF ( total <= 24, 1, 0) ) AS b')->select();
 
                     $sql2 = $this->process->alias('p')->join('fa_order o','p.increment_id = o.increment_id')->field('(p.complete_time-o.payment_time)/3600 AS total')->where($where)->where($map2)->group('p.order_id')->buildSql();
                     $arr2 = $this->process->table([$sql2=>'t2'])->field('sum( IF ( total > 72, 1, 0) ) AS a,sum( IF ( total <= 72, 1, 0) ) AS b')->select();
 
                     $sql3 = $this->process->alias('p')->join('fa_order o','p.increment_id = o.increment_id')->field('(p.complete_time-o.payment_time)/3600 AS total')->where($where)->where($map3)->group('p.order_id')->buildSql();
                     $arr3 = $this->process->table([$sql3=>'t2'])->field('sum( IF ( total > 168, 1, 0) ) AS a,sum( IF ( total <= 168, 1, 0) ) AS b')->select();
-                    if($value == '2020-12-23'){
-                        dump($arr1);
-                        dump($arr2);
-                        dump($arr3);exit;
-                    }
                     $timeout_count = $arr1[0]['a'] + $arr2[0]['a'] + $arr3[0]['a'];
                     $untimeout_count = $arr1[0]['b'] + $arr2[0]['b'] + $arr3[0]['b'];
                     $arr[$key]['timeout_count'] = $timeout_count;
