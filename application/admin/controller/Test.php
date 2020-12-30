d56a0a6bd57bf5be7e04afde436802cb64d56780
wpl <wangpenglei@nextmar.com>
2020-12-30 11:10:49 +0800

    1

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -1905,14 +1905,15 @@ class Test extends Backend
         }
     
         $percent = count($no_skus) / count(array_keys($skus));
-
+        echo count($no_skus) . "\n";
         echo $percent . "\n";
         $stock = array_sum(array_column($skus, 'stock'));
         $stock_percent = $no_stock / $stock;
 
+        echo $no_stock . "\n";
         echo $stock_percent . "\n";
         
-
+        echo $no_price. "\n";
         $price = $item->where(['is_del' => 1, 'is_open' => 1, 'stock' => ['>', 0], 'category_id' => ['<>', 43]])->sum('purchase_price*stock');
         $price_percent = $no_price / $price;
 
