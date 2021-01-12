a6e3d4b597f5df2454890ecb785fa4fbb07fd4d4
743090740@qq.com <lover1118to0000>
2021-01-12 18:54:14 +0800

    订单节点 js调整

--- public/assets/js/backend/order/index.js
+++ public/assets/js/backend/order/index.js
@@ -195,6 +195,17 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'bootstrap-table-jump
         detail: function () {
             Controller.api.bindevent();
         },
+        detail: function () {
+            $('#showmessage').onclick = function (){
+                var winuiconsole = $('#winuiconsole');
+                if (winuiconsole.isHidden()){
+                    winuiconsole.show();
+                }else{
+                    winuiconsole.hide();
+                }
+            }
+            Controller.api.bindevent();
+        },
         api: {
 
             formatter: {
