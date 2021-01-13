63e3c1c13ee7ab46457d9f9dee88bea9986bf41c
miaojingjing <mjj192015@gmail.com>
2021-01-13 13:59:44 +0800

    财务管理分支

---
+++ application/admin/controller/finance/WaitPay.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\admin\controller\finance;
+
+use app\common\controller\Backend;
+use think\Cache;
+use think\Controller;
+use think\Db;
+use think\Request;
+
+class WaitPay extends Backend
+{
+    /*
+     * 财务管理
+     * */
+    public function index()
+    {
+
+    }
+}
