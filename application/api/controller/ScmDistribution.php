affbc9595d4d91ad61b2df1694321818c582e28d
WT-20200816TYZG\Administrator <565467299@qq.com>
2021-01-14 18:26:15 +0800

    去掉忽略权限

--- application/api/controller/ScmDistribution.php
+++ application/api/controller/ScmDistribution.php
@@ -1386,7 +1386,8 @@ class ScmDistribution extends Scm
                 'b.work_status' => ['in', [1, 2, 3, 5]]
             ])
             ->select();
-            print_r($check_work_order);die;
+            $a = $this->_work_order_list->getlastSql();
+            print_r($a);die;
         if ($check_work_order) {
             $codeing_flag = 0;
             foreach ($check_work_order as $key => $value) {
