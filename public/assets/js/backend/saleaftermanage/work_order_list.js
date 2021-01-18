147ffc977b8843b775f562172b8cbc46d937d667
WT-20200816TYZG\Administrator <565467299@qq.com>
2021-01-18 11:18:41 +0800

    工单更改地址州选择联动

--- public/assets/js/backend/saleaftermanage/work_order_list.js
+++ public/assets/js/backend/saleaftermanage/work_order_list.js
@@ -2956,7 +2956,10 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'jqui', 'form'], function ($,
                         //判断更换处方的状态，如果显示的话把数据带出来，如果隐藏则不显示镜架数据 end
                     }
                 });
-                
+                //修改地址-State/Province联动补充Region内容
+                $(document).on('change', '#c-region2', function () {
+                    alert(this.html());
+                });
 
             },
         }
