baeabbeb4ef13a770f1abe1d6366939b47832da2
WT-20200816TYZG\Administrator <565467299@qq.com>
2021-01-21 17:43:38 +0800

    财务订单报表

--- public/assets/js/backend/finance/finance_cost.js
+++ public/assets/js/backend/finance/finance_cost.js
@@ -37,16 +37,16 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($
                             {field: 'id', title: 'ID',operate:false},
                             {field: 'bill_type', title: __('关联单据类型'),custom: { 1: 'success', 2: 'success', 3: 'success', 4: 'danger', 5: 'danger', 6: 'danger' , 7: 'danger'}, searchList: { 1: '订单', 2: 'VIP订单', 3: '工单补差价', 4: '退货退款', 5: '订单取消',6:'部分退款' ,7:'Vip退款' ,8:'出库单出库'},formatter: Table.api.formatter.status},
                             {field: 'order_number', title: '订单号'},
-                            {field: 'site', title: '平台'},
+                            {field: 'site', title: '平台',custom: { 1: 'danger', 2: 'success', 3: 'blue'}, searchList: { 1: 'zeelool', 2: 'voogueme', 3: 'nihao'},formatter: Table.api.formatter.status},
                             {field: 'order_type', title: __('订单类型'),custom: { 1: 'success', 2: 'orange', 3: 'danger'}, searchList: { 1: '普通订单', 2: '批发', 3: '网红单'},formatter: Table.api.formatter.status},
                             {field: 'order_money', title: __('订单金额'),operate:false},
                             {field: 'income_amount', title: __('收入金额'),operate:false},
                             {field: 'order_currency_code', title: __('币种')},
                             {field: 'is_carry_forward', title: __('是否结转'),custom: { 1: 'danger', 0: 'success'}, searchList: { 1: '已结转', 0: '未结转'},formatter: Table.api.formatter.status},
                             {field: 'action_type', title: __('增加/冲减'),custom: { 1: 'success', 2: 'danger'}, searchList: { 1: '增加', 2: '冲减'},formatter: Table.api.formatter.status},
-                            {field: 'payment_time', title: __('订单支付时间'),operate:false},
+                            {field: 'payment_time', title: __('订单支付时间'),operate:false,formatter: Table.api.formatter.datetime},
                             {field: 'payment_method', title: __('支付方式'),operate:false},
-                            {field: 'createtime', title: __('创建时间')},
+                            {field: 'createtime', title: __('创建时间'),formatter: Table.api.formatter.datetime},
                         ]
                     ]
                 });
