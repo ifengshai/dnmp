893bfa8e666f57d5ca9b56c572589a901a0f4ba4
wpl <wangpenglei@nextmar.com>
2021-01-21 17:43:16 +0800

    添加周期结转功能

--- public/assets/js/backend/finance/cycle_carry_order.js
+++ public/assets/js/backend/finance/cycle_carry_order.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($, undefined, Backend, Table, Form,Upload) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'upload'], function ($, undefined, Backend, Table, Form, Upload) {
 
     var Controller = {
         index: function () {
@@ -21,30 +21,30 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($
                 showExport: false,
                 columns: [
                     [
-                        {checkbox: true},
-                        {field: 'id', title: __('ID'),operate:false},
-                        {field: 'customer_name', title: __('结转单')},
-                        {field: 'createtime', title: __('结转时间'), operate: 'RANGE', addclass: 'datetimerange', formatter: Table.api.formatter.datetime},
+                        { checkbox: true },
+                        { field: 'id', title: __('ID'), operate: false },
+                        { field: 'cycle_number', title: __('结转单号') },
+                        { field: 'createtime', title: __('结转时间'), operate: 'RANGE', addclass: 'datetimerange', formatter: Table.api.formatter.datetime },
                         {
                             field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, buttons: [
-                                 {
-                                  name: 'detail',
-                                  text: '查看详情',
-                                  title: __('查看详情'),
-                                  extend: 'data-area = \'["80%","70%"]\'',
-                                  classname: 'btn btn-xs btn-primary btn-dialog',
-                                  icon: 'fa fa-list',
-                                  url: 'customer/wholesale_customer/detail',
-                                  callback: function (data) {
-                                      Layer.alert("接收到回传数据：" + JSON.stringify(data), {title: "回传数据"});
-                                  },
-                                  visible: function (row) {
-                                      //返回true时按钮显示,返回false隐藏
-                                      return true;
-                                  }
-                              },
+                                {
+                                    name: 'detail',
+                                    text: '查看详情',
+                                    title: __('查看详情'),
+                                    extend: 'data-area = \'["80%","70%"]\'',
+                                    classname: 'btn btn-xs btn-primary btn-dialog',
+                                    icon: 'fa fa-list',
+                                    url: 'finance/cycle_carry_order/detail',
+                                    callback: function (data) {
+                                        Layer.alert("接收到回传数据：" + JSON.stringify(data), { title: "回传数据" });
+                                    },
+                                    visible: function (row) {
+                                        //返回true时按钮显示,返回false隐藏
+                                        return true;
+                                    }
+                                },
 
-                                ],
+                            ],
                             formatter: Table.api.formatter.operate
                         }
                     ]
@@ -59,7 +59,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($
             Table.api.init({
                 searchFormVisible: true,
                 extend: {
-                    index_url: 'finance/cycle_carry_order/detail' + location.search,
+                    index_url: 'finance/cycle_carry_order/detail' + location.search + '&id=' + Config.id,
                 }
             });
 
@@ -70,23 +70,39 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($
                 url: $.fn.bootstrapTable.defaults.extend.index_url,
                 pk: 'id',
                 sortName: 'id',
-                showToggle: false,
+                commonSearch: true,
+                search: false,
                 showExport: false,
+                showColumns: false,
+                showToggle: false,
+                pagination: false,
                 columns: [
                     [
-                        {field: 'id', title: __('ID'),operate:false},
-                        {field: 'status', title: __('类型'),custom: { 1: 'danger'}, searchList: { 1: '订单'},formatter: Table.api.formatter.status,operate:false},
-                        {field: 'mobile', title: __('订单号'),operate:false},
-                        {field: 'status', title: __('平台'),custom: { 1: 'danger', 2: 'success', 3: 'blue'}, searchList: { 1: 'zeelool', 2: 'voogueme', 3: 'nihao'},formatter: Table.api.formatter.status,operate:false},
-                        {field: 'customer_name', title: __('币种'),operate:false},
-                        {field: 'status', title: __('订单类型'),custom: { 1: 'danger', 2: 'success'}, searchList: { 1: '普通订单', 2: '网红单'},formatter: Table.api.formatter.status,operate:false},
-                        {field: 'mobile', title: __('订单总金额'),operate:false},
-                        {field: 'mobile', title: __('支付金额'),operate:false},
-                        {field: 'mobile', title: __('镜架成本'),operate:false},
-                        {field: 'mobile', title: __('镜片成本'),operate:false},
-                        {field: 'mobile', title: __('支付时间'),operate:false},
-                        {field: 'mobile', title: __('结转单号'),visible:false},
-                        {field: 'createtime', title: __('结转时间'), operate: 'RANGE', addclass: 'datetimerange', formatter: Table.api.formatter.datetime},
+                        { field: 'id', title: __('ID'), operate: false },
+                        { field: 'bill_type', title: __('关联单据类型'), custom: { 1: 'danger', 2: 'success', 3: 'orange', 4: 'warning', 5: 'purple', 6: 'primary', 7: 'primary', 8: 'primary', 9: 'primary', 10: 'primary' }, searchList: { 1: '订单', 2: 'VIP订单', 3: '工单补差价', 4: '退货退款', 5: '订单取消', 6: '部分退款', 7: 'Vip退款', 8: '订单出库', 9: '出库单出库', 10: '冲减暂估' }, formatter: Table.api.formatter.status },
+                        { field: 'order_number', title: '订单号' },
+                        {
+                            field: 'site', title: __('站点'), addClass: 'selectpicker', data: 'multiple',
+                            searchList: {
+                                1: 'Zeelool',
+                                2: 'Voogueme',
+                                3: 'Nihao',
+                                4: 'Meeloog',
+                                5: 'Wesee',
+                                8: 'Amazon',
+                                9: 'Zeelool_es',
+                                10: 'Zeelool_de',
+                                11: 'Zeelool_jp'
+                            }, operate: 'IN',
+                            formatter: Table.api.formatter.status
+                        },
+                        { field: 'order_currency_code', title: __('币种'), operate: false },
+                        { field: 'order_type', title: __('订单类型'), custom: { 1: 'blue', 2: 'blue', 3: 'blue', 4: 'blue', 5: 'blue', 6: 'blue', 10: 'blue' }, searchList: { 1: '普通订单', 2: '批发单', 3: '网红单', 4: '补发单', 5: '补差价', 6: '一件代发', 10: '货到付款' }, formatter: Table.api.formatter.status },
+                        { field: 'order_money', title: __('订单金额'), operate: false },
+                        { field: 'frame_cost', title: __('镜架成本'), operate: false },
+                        { field: 'lens_cost', title: __('镜片成本'), operate: false },
+                        { field: 'payment_time', title: __('支付时间'), operate: 'RANGE', addclass: 'datetimerange', formatter: Table.api.formatter.datetime }
+
                     ]
                 ]
             });
--- public/assets/js/backend/finance/finance_cost.js
+++ public/assets/js/backend/finance/finance_cost.js
@@ -1,4 +1,4 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($, undefined, Backend, Table, Form,Upload) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'upload'], function ($, undefined, Backend, Table, Form, Upload) {
 
     var Controller = {
         index: function () {
@@ -6,7 +6,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($
             Table.api.init({
                 searchFormVisible: true,
             });
-            
+
             //绑定事件
             $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
                 var panel = $($(this).attr("href"));
@@ -19,7 +19,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($
                 //移除绑定的事件
                 $(this).unbind('shown.bs.tab');
             });
-            
+
             //必须默认触发shown.bs.tab事件
             $('ul.nav-tabs li.active a[data-toggle="tab"]').trigger("shown.bs.tab");
         },
@@ -33,20 +33,20 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($
                     sortName: 'createtime',
                     columns: [
                         [
-                            {checkbox: true, },
-                            {field: 'id', title: 'ID',operate:false},
-                            {field: 'bill_type', title: __('关联单据类型'),custom: { 1: 'success', 2: 'success', 3: 'success', 4: 'danger', 5: 'danger', 6: 'danger' , 7: 'danger'}, searchList: { 1: '订单', 2: 'VIP订单', 3: '工单补差价', 4: '退货退款', 5: '订单取消',6:'部分退款' ,7:'Vip退款' ,8:'出库单出库'},formatter: Table.api.formatter.status},
-                            {field: 'order_number', title: '订单号'},
-                            {field: 'site', title: '平台'},
-                            {field: 'order_type', title: __('订单类型'),custom: { 1: 'success', 2: 'orange', 3: 'danger'}, searchList: { 1: '普通订单', 2: '批发', 3: '网红单'},formatter: Table.api.formatter.status},
-                            {field: 'order_money', title: __('订单金额'),operate:false},
-                            {field: 'income_amount', title: __('收入金额'),operate:false},
-                            {field: 'order_currency_code', title: __('币种')},
-                            {field: 'is_carry_forward', title: __('是否结转'),custom: { 1: 'danger', 0: 'success'}, searchList: { 1: '已结转', 0: '未结转'},formatter: Table.api.formatter.status},
-                            {field: 'action_type', title: __('增加/冲减'),custom: { 1: 'success', 2: 'danger'}, searchList: { 1: '增加', 2: '冲减'},formatter: Table.api.formatter.status},
-                            {field: 'payment_time', title: __('订单支付时间'),operate:false},
-                            {field: 'payment_method', title: __('支付方式'),operate:false},
-                            {field: 'createtime', title: __('创建时间')},
+                            { checkbox: true, },
+                            { field: 'id', title: 'ID', operate: false },
+                            { field: 'bill_type', title: __('关联单据类型'), custom: { 1: 'success', 2: 'success', 3: 'success', 4: 'danger', 5: 'danger', 6: 'danger', 7: 'danger' }, searchList: { 1: '订单', 2: 'VIP订单', 3: '工单补差价', 4: '退货退款', 5: '订单取消', 6: '部分退款', 7: 'Vip退款', 8: '出库单出库' }, formatter: Table.api.formatter.status },
+                            { field: 'order_number', title: '订单号' },
+                            { field: 'site', title: '平台' },
+                            { field: 'order_type', title: __('订单类型'), custom: { 1: 'success', 2: 'orange', 3: 'danger' }, searchList: { 1: '普通订单', 2: '批发', 3: '网红单' }, formatter: Table.api.formatter.status },
+                            { field: 'order_money', title: __('订单金额'), operate: false },
+                            { field: 'income_amount', title: __('收入金额'), operate: false },
+                            { field: 'order_currency_code', title: __('币种') },
+                            { field: 'is_carry_forward', title: __('是否结转'), custom: { 1: 'danger', 0: 'success' }, searchList: { 1: '已结转', 0: '未结转' }, formatter: Table.api.formatter.status },
+                            { field: 'action_type', title: __('增加/冲减'), custom: { 1: 'success', 2: 'danger' }, searchList: { 1: '增加', 2: '冲减' }, formatter: Table.api.formatter.status },
+                            { field: 'payment_time', title: __('订单支付时间'), operate: false },
+                            { field: 'payment_method', title: __('支付方式'), operate: false },
+                            { field: 'createtime', title: __('创建时间') },
                         ]
                     ]
                 });
@@ -58,18 +58,18 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form','upload'], function ($
                 // 表格2
                 var table2 = $("#table2");
                 table2.bootstrapTable({
-                    url: 'finance/finance_cost/table2',
+                    url: 'finance/finance_cost/cost',
                     toolbar: '#toolbar2',
                     sortName: 'id',
                     columns: [
                         [
-                            {field: 'id', title: 'ID',operate:false},
-                            {field: 'bill_type', title: __('关联单据类型'),custom: { 1: 'danger', 2: 'success', 3: 'orange', 4: 'warning', 5: 'purple', 6: 'primary' , 7: 'primary'}, searchList: { 1: '订单', 2: 'VIP订单', 3: '工单补差价', 4: '退货退款', 5: '部分退款',6:'VIP退款' ,7:'冲减'},formatter: Table.api.formatter.status},
-                            {field: 'order_number', title: __('关联单号')},
-                            {field: 'frame_cost', title: __('镜架成本'),operate:false},
-                            {field: 'lens_cost', title: __('镜片成本'),operate:false},
-                            {field: 'createtime', title: __('创建时间')}, 
-                            {field: 'is_carry_forward', title: __('是否结转'),visible:false,searchList: { 1: '已结转', 2: '未结转'},formatter: Table.api.formatter.status},
+                            { field: 'id', title: 'ID', operate: false },
+                            { field: 'bill_type', title: __('关联单据类型'), custom: { 1: 'danger', 2: 'success', 3: 'orange', 4: 'warning', 5: 'purple', 6: 'primary', 7: 'primary', 8: 'primary', 9: 'primary', 10: 'primary' }, searchList: { 1: '订单', 2: 'VIP订单', 3: '工单补差价', 4: '退货退款', 5: '订单取消', 6: '部分退款', 7: 'Vip退款', 8: '订单出库', 9: '出库单出库', 10: '冲减暂估' }, formatter: Table.api.formatter.status },
+                            { field: 'order_number', title: __('关联单号') },
+                            { field: 'frame_cost', title: __('镜架成本'), operate: false },
+                            { field: 'lens_cost', title: __('镜片成本'), operate: false },
+                            { field: 'is_carry_forward', title: __('是否结转'),  searchList: { 1: '已结转', 0: '未结转' }, formatter: Table.api.formatter.status },
+                            { field: 'createtime', title: __('创建时间'), operate: 'RANGE', addclass: 'datetimerange',  formatter: Table.api.formatter.datetime },
                         ]
                     ]
                 });
