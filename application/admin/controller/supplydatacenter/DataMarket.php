8e2e6039b2e688abb357fc2084d7158014afbd5b
743090740@qq.com <lover1118to0000>
2021-01-22 17:20:43 +0800

    no message

--- application/admin/controller/supplydatacenter/DataMarket.php
+++ application/admin/controller/supplydatacenter/DataMarket.php
@@ -796,16 +796,29 @@ class DataMarket extends Backend
 //            ->field('(p.delivery_time-o.payment_time)/3600 AS total,o.increment_id,o.status')
             ->where($where)->where($map1)->group('p.order_id')->buildSql();
         $arr1 = $this->process->table([$sql1=>'t2'])->select();
-        dump($arr1);die();
-        $sql2 = $this->process->alias('p')->join('fa_order o','p.increment_id = o.increment_id')->field('(p.delivery_time-o.payment_time)/3600 AS total')->where($where)->where($map2)->group('p.order_id')->buildSql();
-        $arr2 = $this->process->table([$sql2=>'t2'])->field('sum( IF ( total > 72, 1, 0) ) AS a,sum( IF ( total <= 72, 1, 0) ) AS b')->select();
+        $arr1  = collection($arr1)->toArray();
 
-        $sql3 = $this->process->alias('p')->join('fa_order o','p.increment_id = o.increment_id')->field('(p.delivery_time-o.payment_time)/3600 AS total')->where($where)->where($map3)->group('p.order_id')->buildSql();
-        $arr3 = $this->process->table([$sql3=>'t2'])->field('sum( IF ( total > 168, 1, 0) ) AS a,sum( IF ( total <= 168, 1, 0) ) AS b')->select();
+        $sql2 = $this->process->alias('p')
+            ->join('fa_order o','p.increment_id = o.increment_id')
+            ->field('p.delivery_time,o.payment_time,o.increment_id,o.status')
+            ->where($where)->where($map2)->group('p.order_id')->buildSql();
+        $arr2 = $this->process->table([$sql2=>'t2'])
+//            ->field('sum( IF ( total > 72, 1, 0) ) AS a,sum( IF ( total <= 72, 1, 0) ) AS b')
+            ->select();
+        $arr2  = collection($arr2)->toArray();
+        $sql3 = $this->process->alias('p')
+            ->join('fa_order o','p.increment_id = o.increment_id')
+            ->field('p.delivery_time,o.payment_time,o.increment_id,o.status')
+            ->where($where)->where($map3)->group('p.order_id')->buildSql();
+        $arr3 = $this->process->table([$sql3=>'t2'])
+//            ->field('sum( IF ( total > 168, 1, 0) ) AS a,sum( IF ( total <= 168, 1, 0) ) AS b')
+            ->select();
+        $arr3  = collection($arr3)->toArray();
         $timeout_count = $arr1[0]['a'] + $arr2[0]['a'] + $arr3[0]['a'];
 
+        dump($arr1);
         dump($arr2);
-        dump($arr3);
+        dump($arr3);die();
 
         dump($timeout_count);die();
 
