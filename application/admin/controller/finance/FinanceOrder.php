4e4d6ba4696accbb32fee9983c06494677ebe4ef
WT-20200816TYZG\Administrator <565467299@qq.com>
2021-01-23 10:47:53 +0800

    财务报表增加支付金额

--- application/admin/controller/finance/FinanceOrder.php
+++ application/admin/controller/finance/FinanceOrder.php
@@ -50,7 +50,7 @@ class FinanceOrder extends Backend
                 ->where(['order_number' => $value['order_number'],'action_type' => 1,'type' => 2])
                 ->select();
                 $list_j = $this->finance_cost
-                ->where(['order_number' => $value['order_number'],'action_type' => 0,'type' => 2])
+                ->where(['order_number' => $value['order_number'],'action_type' => 2,'type' => 2])
                 ->select();
                 $list_z_frame = array_sum(array_column($list_z, 'frame_cost'));
                 $list_z_lens = array_sum(array_column($list_z, 'lens_cost'));
@@ -58,10 +58,20 @@ class FinanceOrder extends Backend
                 $list_j_lens = array_sum(array_column($list_j, 'lens_cost'));
                 $list[$key]['frame_cost'] = $list_z_frame-$list_j_frame;
                 $list[$key]['lens_cost'] = $list_z_lens-$list_j_lens;
+                //查询收入
+                $list_zs = $this->finance_cost
+                ->where(['order_number' => $value['order_number'],'action_type' => 1,'type' => 1])
+                ->select();
+                $list_js = $this->finance_cost
+                ->where(['order_number' => $value['order_number'],'action_type' => 2,'type' => 1])
+                ->select();
+                $list_zs_income_amount = array_sum(array_column($list_zs, 'income_amount'));
+                $list_js_income_amount = array_sum(array_column($list_js, 'income_amount'));
+                $list[$key]['income_amount'] = $list_zs_income_amount-$list_js_income_amount;
+                //物流成本
                 $list[$key]['fi_actual_payment_fee'] = $model->table('ld_delivery_order_finance')->where(['increment_id' => $value['order_number']])->value('fi_actual_payment_fee');
             }
             $result = array("total" => $total, "rows" => $list);
-
             return json($result);
         }
         $this->view->assign('magentoplatformarr',$magentoplatformarr);
