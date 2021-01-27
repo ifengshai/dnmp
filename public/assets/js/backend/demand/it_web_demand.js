50196c90b8b11b94959fef5e401afe6a03ef9c95
743090740@qq.com <lover1118to0000>
2021-01-27 16:36:22 +0800

    优先级状态判断

--- public/assets/js/backend/demand/it_web_demand.js
+++ public/assets/js/backend/demand/it_web_demand.js
@@ -144,7 +144,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'nkeditor', 'upload']
                             {
                                 field: 'priority',
                                 title: __('优先级'),
-                                searchList: { '': '-', 1: '低', 2: '低+', 3: '中', 4: '中+', 5: '高',6:'高+' },
+                                searchList: { '': '-',0: '-', 1: '低', 2: '低+', 3: '中', 4: '中+', 5: '高',6:'高+' },
                                 custom: { 1: 'black', 2: 'black', 3: 'black', 4: 'black', 5: 'black' },
                                 formatter: Table.api.formatter.status,
                                 operate: false
