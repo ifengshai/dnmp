1922efa89b33d52da53950cc8f23faf4afaf88ff
743090740@qq.com <lover1118to0000>
2021-02-01 13:37:08 +0800

    前端需求列表数据导出

--- application/admin/controller/demand/ItWebDemand.php
+++ application/admin/controller/demand/ItWebDemand.php
@@ -379,8 +379,9 @@ class ItWebDemand extends Backend
 
     public function batch_export_xls(){
         $where['is_del'] =['eq',1];
-        $where['demand_type'] =['eq',2];
-        $where['create_time'] = ['between',['2020-12-01 00:00:00','2020-12-31 23:59:59']];
+        $where['demand_type'] =['eq',1];
+        $where['web_designer_group'] =['eq',1];
+        $where['create_time'] = ['between',['2021-01-01 00:00:00','2021-01-31 23:59:59']];
 //        $field = 'id,site,entry_user_id,type,functional_module,title,create_time,pm_audit_status_time,web_designer_user_id,app_user_id,phper_user_id,node_time
 //        develop_finish_time,web_designer_complexity,web_designer_group,web_remarks,pm_audit_status,pm_confirm_time,copy_to_user_id';
         $list = $this->model
