2e275d57c5c69449cd75f95a72f58f80ad60dc34
miaojingjing <mjj192015@gmail.com>
2021-02-03 16:57:40 +0800

    调试大屏

--- application/admin/controller/supplydatacenter/DataMarket.php
+++ application/admin/controller/supplydatacenter/DataMarket.php
@@ -121,7 +121,7 @@ class DataMarket extends Backend
     //库存总览
     public function stock_overview(){
         $cache_data = Cache::get('Supplydatacenter_datamarket'  . md5(serialize('stock_overview')));
-        if (!$cache_data) {
+        if ($cache_data) {
            return $cache_data;
         }
         //呆滞库存数量/金额
@@ -450,7 +450,7 @@ class DataMarket extends Backend
     //库存分级概况
     public function stock_level_overview2(){
         $cache_data = Cache::get('Supplydatacenter_datamarket'  . md5(serialize('stock_level_overview2')));
-        if ($cache_data) {
+        if (!$cache_data) {
             return $cache_data;
         }
         //获取呆滞库存信息
