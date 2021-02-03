a36ee938877c40924afddceaa1eceb5a7f1f07fa
wpl <wangpenglei@nextmar.com>
2021-02-03 18:16:48 +0800

    测试回调事件

--- application/api/controller/Ding.php
+++ application/api/controller/Ding.php
@@ -433,10 +433,10 @@ class Ding extends Controller
 
                         //判断审核状态 审核拒绝
                         if ($payload['result'] == 'refuse') {
-                            FinancePurchase::where(['process_instance_id' => $payload['process_instance_id']])->update(['status' => 3]);
+                            FinancePurchase::where(['process_instance_id' => $payload['processInstanceId']])->update(['status' => 3]);
                             //最后一步判断如果为李亚方审核通过改为完成
                         } elseif ($payload['result'] == 'agree' && $payload['staffId'] == '171603353926064429') {
-                            FinancePurchase::where(['process_instance_id' => $payload['process_instance_id']])->update(['status' => 4]);
+                            FinancePurchase::where(['process_instance_id' => $payload['processInstanceId']])->update(['status' => 4]);
                         }
                     }
 
