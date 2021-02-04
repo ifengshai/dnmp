3624c9b20f42c769f6f50ddb898077021949d8f0
miaojingjing <mjj192015@gmail.com>
2021-02-04 12:58:17 +0800

    去除调试

--- application/admin/controller/supplydatacenter/DataMarket.php
+++ application/admin/controller/supplydatacenter/DataMarket.php
@@ -449,7 +449,7 @@ class DataMarket extends Backend
     }
     //库存分级概况
     public function stock_level_overview2(){
-        $cache_data = Cache::get('Supplydatacenter_datamarket5555'  . md5(serialize('stock_level_overview2')));
+        $cache_data = Cache::get('Supplydatacenter_datamarket'  . md5(serialize('stock_level_overview2')));
         if ($cache_data) {
             return $cache_data;
         }
