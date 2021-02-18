49880af6577a3a092339a690b7e8b950c65880d8
miaojingjing <mjj192015@gmail.com>
2021-02-18 16:19:23 +0800

    同步邮件

--- application/admin/controller/zendesk/Notice.php
+++ application/admin/controller/zendesk/Notice.php
@@ -1281,7 +1281,7 @@ class Notice extends Controller
      */
     public function asyncUpdate()
     {
-        $params = 'type:ticket updated_at>=2021-02-17T00:00:00Z updated_at<=2021-02-17T02:00:00Z order_by:updated_at sort:asc';
+        $params = 'type:ticket updated_at>=2021-02-16T00:00:00Z updated_at<=2021-02-16T02:00:00Z order_by:updated_at sort:asc';
         //Get all tickets
         $tickets = $this->client->search()->find($params);
 
