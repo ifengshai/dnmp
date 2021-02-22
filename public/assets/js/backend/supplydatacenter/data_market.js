63d48f276561115cc6492ae9e020d78782aba3d9
miaojingjing <mjj192015@gmail.com>
2021-02-22 16:10:54 +0800

    库存修改

--- public/assets/js/backend/supplydatacenter/data_market.js
+++ public/assets/js/backend/supplydatacenter/data_market.js
@@ -135,36 +135,16 @@ define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'form', 'echartsob
                                     type: 'shadow' //指示器类型。可选项'line' 直线指示器。'shadow' 阴影指示器。'cross' 十字准星指示器。其实是种简写，表示启用两个正交的轴的 axisPointer。
                                 },
                                 formatter: function (param) { //格式化提示信息
-                                    console.log(param);
-                                    return param[0].name + '<br/>' + param[0].seriesName + '：' + param[0].value;
+                                    return param[0].seriesName + '<br/>' + param[0].name + '：' + param[0].value;
                                 }
                             },
-                            grid: { //直角坐标系内绘图网格
-                                top: '10%', //grid 组件离容器上侧的距离。
-                                left: '5%', //grid 组件离容器左侧的距离。
-                                right: '10%', //grid 组件离容器右侧的距离。
-                                bottom: '10%', //grid 组件离容器下侧的距离。
-                                containLabel: true //grid 区域是否包含坐标轴的刻度标签。
+                            xAxis: {
+                                type: 'category',
+                                data: []
                             },
-                            legend: { //图例配置
-                                padding: 5,
-                                top: '2%',
-                                data: ['库存']
-                            },
-                            xAxis: [
-                                {
-                                    type: 'category'
-                                }
-                            ],
-                            yAxis: [
-                                {
-                                    type: 'value',
-                                    name: '库存',
-                                    axisLabel: {
-                                        formatter: '{value} 个'
-                                    }
-                                },
-                            ],
+                            yAxis: {
+                                type: 'value'
+                            }
                         }
                     };
                     var options = {
