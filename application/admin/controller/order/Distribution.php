3ab2dd749f1abee72cda5339f48d7de9c3f86693
wpl <wangpenglei@nextmar.com>
2021-02-26 10:40:09 +0800

    修改订单导出

--- application/admin/controller/order/Distribution.php
+++ application/admin/controller/order/Distribution.php
@@ -1459,7 +1459,7 @@ class Distribution extends Backend
 
 
                 //过滤饰品站
-                if ($value['site'] != 12) {
+                if ($value['site'] != 12 && $value['site'] != 5) {
                     //查询镜框尺寸
                     //$tmp_bridge = $this->get_frame_lens_width_height_bridge($value['product_id'], $value['site']);
                     $getGlassInfo = $this->httpRequest($value['site'], 'magic/order/getGlassInfo', ['skus' => $v['sku']], 'POST');
