db55a738c96f4a6d5fe72d015a98e1d7aabbf938
wpl <wangpenglei@nextmar.com>
2021-02-26 14:18:52 +0800

    修改脚本

--- application/admin/controller/Wangpenglei.php
+++ application/admin/controller/Wangpenglei.php
@@ -619,17 +619,17 @@ class Wangpenglei extends Backend
         foreach ($list as $k => $v) {
             $allcount = $orderItem->where(['order_id' => $v['order_id']])->count();
 
-            $count = $orderItem->where(['distribution_status' => ['in', [0, 8]], 'order_id' => $v['order_id']])->count();
-            
+            $count = $orderItem->where(['distribution_status' => ['in', [0, 8, 9]], 'order_id' => $v['order_id']])->count();
+
             //查询工单是否处理完成
-            $workcount = $worklist->where(['order_item_numbers' => ['like','%' .$v['item_order_number']. '%'],'work_status' => ['in',[1,2,3,5]]])->count();
+            $workcount = $worklist->where(['order_item_numbers' => ['like', '%' . $v['item_order_number'] . '%'], 'work_status' => ['in', [1, 2, 3, 5]]])->count();
             if ($allcount == $count && $workcount < 1) {
-                $orderItem->where(['order_id' => $v['order_id'],'distribution_status' => 8])->update(['distribution_status' => 9]);
+                $orderItem->where(['order_id' => $v['order_id'], 'distribution_status' => 8])->update(['distribution_status' => 9]);
                 $orderProcess->where(['order_id' => $v['order_id']])->update(['combine_status' => 1, 'combine_time' => time()]);
 
                 echo $v['id'] . "\n";
             }
-           
+
             usleep(100000);
         }
 
