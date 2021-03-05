72e8cbe36bd4282f0303a9557e00aa50d7120189
wpl <wangpenglei@nextmar.com>
2021-03-05 16:19:43 +0800

    添加采购审批功能

--- application/admin/controller/financepurchase/PurchasePay.php
+++ application/admin/controller/financepurchase/PurchasePay.php
@@ -376,6 +376,216 @@ class PurchasePay extends Backend
 
     }
 
+
+     /**
+     * 添加采购付款生清单
+     * 有两个入口一个从采购列表过来 一个从当前页面添加
+     * 当前页面添加需要手动输入采购单号
+     * Created by Phpstorm.
+     * User: jhh
+     * Date: 2021/1/13
+     * Time: 19:03:09
+     */
+    public function batch_add()
+    {
+        if ($this->request->isPost()) {
+            $params = $this->request->post("row/a");
+            $reason = $this->request->post("reason/a");
+            // dump($params);
+            // dump($reason);
+            // die;
+            if ($params) {
+                $params = $this->preExcludeFields($params);
+                Db::startTrans();
+                try {
+                    //校验是否存在未完成的付款申请单
+                    if ($params['pay_type'] == 1 || $params['pay_type'] == 2) {
+                        $finance_pirchase = $this->model->where('purchase_id', $params['purchase_id'])->where('status', 'in', [0, 1, 2])->find();
+                    } else {
+                        $finance_pirchase = $this->model->where('order_number', $params['order_number'])->where('status', 'in', [0, 1, 2])->find();
+                    }
+                    if (!empty($finance_pirchase)) {
+                        $this->error('当前单号存在未完成的付款申请单，请检查后重试');
+                    }
+
+                    $insert['order_number'] = $params['order_number'];
+                    $insert['pay_type'] = $params['pay_type'];
+                    switch ($insert['pay_type']) {
+                        case 1:
+                            $pay_type = '预付款';
+                            $insert['pay_rate'] = 0.3;
+                            break;
+                        case 2:
+                            $pay_type = '全款';
+                            $insert['pay_rate'] = 1;
+                            break;
+                        case 3:
+                            $pay_type = '尾款';
+                            $insert['pay_rate'] = 1;
+                            break;
+                        default:
+                            $pay_type = '其他';
+                    }
+                    $insert['status'] = $params['status'];
+                    $insert['remark'] = $params['remark'];
+                    $insert['purchase_id'] = $params['purchase_id'];
+                    $insert['1688_number'] = $params['1688_number'];
+                    $insert['supplier_id'] = $params['supplier_id'];
+                    $insert['order_number'] = $params['order_number'];
+                    $insert['pay_grand_total'] = $params['pay_grand_total'];
+                    $insert['base_currency_code'] = $params['base_currency_code'];
+                    $insert['create_time'] = time();
+                    $insert['create_person'] = session('admin.nickname');
+                    switch ($params['base_currency_code']) {
+                        case 'CNY':
+                            $currency = '人民币';
+                            break;
+                        case 'USD':
+                            $currency = '美元';
+                            break;
+                        default:
+                            $currency = '人民币';
+                    }
+                    //采购单信息
+                    $purchase_order = $this->purchase_order->where('id', $insert['purchase_id'])->find();
+                    //提交审核 需要创建钉钉审批单
+                    if ($insert['status'] == 1) {
+                        $initiate_approval = new Ding();
+                        //当前用户信息
+                        $admin = Db::name('admin')->where('id', session('admin.id'))->find();
+                        $arr['originator_user_id'] = $admin['userid'];
+                        $arr['dept_id'] = $admin['department_id'];
+                        if ($params['pay_grand_total'] >= 300000){
+                            // //任萍 王涛 陈见 王剑 李亚方
+                            $arr['approvers'] = '1007304767660594,0221135665945008,405544201227897,0647044715938022,01223742335734031749';
+                        }else{
+                            // //任萍 王涛 王剑 李亚方
+                            $arr['approvers'] = '1007304767660594,0221135665945008,0647044715938022,01223742335734031749';
+                        }
+                        // //抄送 屈金金
+                        $arr['cc_list'] = '204112301323897192';
+
+                        // $arr['originator_user_id'] = '071829462027950349';
+                        // $arr['dept_id'] = '143678442';
+                        // $arr['approvers'] = '285501046927507550,0550643549844645,056737345633028055';
+                        // //刘超 红亚 玉晓
+                        // // $arr['approvers'] = '0704513051687725,310818292339015332,111525355037914674';
+                        // $arr['cc_list'] = '071829462027950349';
+
+                        //结算单创建采购付款申请单
+                        if ($params['pay_type'] == 3) {
+                            foreach ($reason as $kk => $vv) {
+                                if (is_array($vv)){
+                                    $item = new Item();
+                                    $category_id = $item->where('sku',$vv['name'])->value('category_id');
+                                    $type = $this->category($category_id);
+                                    $reasons[$kk] = [
+                                        ['name' => '采购品名', 'value' => $vv['name']],
+                                        ['name' => '采购单号', 'value' => $vv['number']],
+                                        ['name' => '1688单号', 'value' => '0'],
+                                        ['name' => '采购批次', 'value' =>  $vv['batch'] ? $vv['batch']:0],
+                                        ['name' => '商品分类', 'value' => $type ? $type:'镜框'],
+                                        ['name' => '采购数量', 'value' => $vv['num']],
+                                        ['name' => '采购单价', 'value' => $vv['single']],
+                                        ['name' => '入库数量', 'value' => $vv['in_number']],
+                                        ['name' => '扣款', 'value' => $vv['kou_money']],
+                                        ['name' => '金额', 'value' => $vv['money']],
+                                        ['name' => '运费', 'value' => $vv['freight']]
+                                    ];
+                                }
+                            }
+                            $arr['form_component_values'] = [
+                                ['name' => '采购方式', 'value' => $purchase_order['purchase_type'] == 1 ? '线下采购' : '线上采购'],
+                                // ['name' => '采购产品类型', 'value' => $type],
+                                ['name' => '付款类型', 'value' => $pay_type],
+                                ['name' => '供应商名称', 'value' => $params['supplier_name']],
+                                ['name' => '币种', 'value' => $currency],
+                                ['name' => '付款比例', 'value' => '100%'],
+                                ['name' => '采购事由', 'value' =>
+                                    $reasons
+                                ],
+                                ['name' => '付款总金额', 'value' => $params['pay_grand_total']],
+                                ['name' => '收款方名称', 'value' => $params['linkname'] ? $params['linkname']:'无'],
+                                ['name' => '收款方账户', 'value' => $params['bank_account'] ? $params['bank_account']:0],
+                                ['name' => '收款方开户行', 'value' => $params['opening_bank_address'] ? $params['opening_bank_address'] : '无'],
+                            ];
+                        } else {
+                            //采购列表创建采购付款申请单 采购事由只会有一条 就是以整个采购单为维度 不考虑批次 无批次 无入库数量 无入库金额
+                            // 采购事由总金额是采购单总采购金额 付款总金额是采购单总金额加运费乘以付款比例
+                            $item = new Item();
+                            //分类拿sku的最上级分类
+                            $category_id = $item->where('sku',$reason['name'])->value('category_id');
+                            $type = $this->category($category_id);
+                            $arr['form_component_values'] = [
+                                ['name' => '采购方式', 'value' => $purchase_order['purchase_type'] == 1 ? '线下采购' : '线上采购'],
+                                // ['name' => '采购产品类型', 'value' => '镜框'],
+                                ['name' => '付款类型', 'value' => $pay_type],
+                                ['name' => '供应商名称', 'value' => $params['supplier_name']],
+                                ['name' => '币种', 'value' => $currency],
+                                ['name' => '付款比例', 'value' => $insert['pay_rate'] * 100 . '%'],
+                                ['name' => '采购事由', 'value' => [
+                                    [
+                                        ['name' => '采购品名', 'value' => $reason['name']],
+                                        ['name' => '采购单号', 'value' => $params['purchase_number']],
+                                        ['name' => '1688单号', 'value' => $params['1688_number']  ?: 0],
+                                        ['name' => '采购批次', 'value' => '0'],
+                                        ['name' => '商品分类', 'value' => $type ? $type:'镜框'],
+                                        ['name' => '采购数量', 'value' => $reason['num']],
+                                        ['name' => '采购单价', 'value' => $reason['single']],
+                                        ['name' => '入库数量', 'value' => '0'],
+                                        ['name' => '扣款', 'value' =>'0'],
+                                        ['name' => '金额', 'value' => $reason['money']],
+                                        ['name' => '运费', 'value' => $reason['freight']]
+                                    ]
+                                ]],
+                                ['name' => '付款总金额', 'value' => $params['pay_grand_total']],
+                                ['name' => '收款方名称', 'value' => $params['linkname'] ? $params['linkname']:'无'],
+                                ['name' => '收款方账户', 'value' => $params['bank_account'] ? $params['bank_account']:0],
+                                ['name' => '收款方开户行', 'value' => $params['opening_bank_address'] ? $params['opening_bank_address'] : '无'],
+                            ];
+                        }
+                       
+                        $res = $initiate_approval->initiate_approval($arr);
+                        if ($res['errcode'] != 0 || $res === false) {
+                            throw new Exception('发起审批失败'.$res['errcode'] . '----' . $res['errmsg']);
+                        }
+                    }
+                    $insert['process_instance_id'] = $res['process_instance_id'];
+                    // dump($insert);die;
+                    $finance_purchase_id = Db::name('finance_purchase')->insertGetId($insert);
+                    $label = input('label');
+                    //结算单页面过来的创建付款申请单 需要更新结算的付款申请单id字段
+                    if ($label == 'statement') {
+                        $ids = input('ids');
+                        Db::name('finance_statement')->where('id',$ids)->update(['finance_purcahse_id'=>$finance_purchase_id]);
+                    }
+                    Db::commit();
+                } catch (ValidateException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (PDOException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (Exception $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                }
+            } else {
+                $this->error(__('Parameter %s can not be empty', ''));
+            }
+            $this->success('添加成功！！');
+        }
+        $ids = input('ids');
+        return $this->view->fetch();
+    }
+
+    //是否满足
+    public function is_conditions()
+    {
+
+    }
+
+
     /**
      * 编辑采购付款申请单
      * Created by Phpstorm.
