77f1bd8a053ee5a2a84019882db4ec42a5d58cb3
wpl <wangpenglei@nextmar.com>
2021-03-12 14:54:55 +0800

    添加1688单号

--- application/admin/controller/finance/WaitPay.php
+++ application/admin/controller/finance/WaitPay.php
@@ -34,13 +34,13 @@ class WaitPay extends Backend
                 $map['p.create_time'] = ['between', [$start, $end]];
             }
             //商品分类筛选
+            $purchase = new \app\admin\model\purchase\PurchaseOrder();
             if ($filter['category_id']) {
                 //查询此商品分类下的SKU 采购单
                 $item = new \app\admin\model\itemmanage\Item();
                 $category = new \app\admin\model\itemmanage\ItemCategory();
                 $ids = $category->getList($filter['category_id']);
                 $skus = $item->where(['is_open' => 1, 'is_del' => 1, 'category_id' => ['in', $ids]])->column('sku');
-                $purchase = new \app\admin\model\purchase\PurchaseOrder();
                 $purchase_id = $purchase->alias('a')->where(['purchase_status' => 2, 'sku' => ['in', $skus]])->join(['fa_purchase_order_item' => 'b'], 'a.id=b.purchase_id')->column('purchase_id');
                 $map['purchase_id'] = ['in', $purchase_id];
                 unset($filter['category_id']);
@@ -64,13 +64,16 @@ class WaitPay extends Backend
             $list = $this->financepurchase
                 ->alias('p')
                 ->join('fa_supplier s', 's.id=p.supplier_id', 'left')
-                ->field('p.id,s.supplier_name,p.order_number,FROM_UNIXTIME(p.create_time) create_time,p.create_person')
+                ->field('p.id,s.supplier_name,p.order_number,FROM_UNIXTIME(p.create_time) create_time,p.create_person,p.purchase_id')
                 ->where($where)
                 ->where($map)
                 ->order($sort, $order)
                 ->limit($offset, $limit)
                 ->select();
             $list = collection($list)->toArray();
+            foreach ($list as $k => $v) {
+                $list[$k]['1688_number'] = $purchase->where(['id' => $v['purchase_id']])->value('1688_number');
+            }
             $result = array("total" => $total, "rows" => $list);
             return json($result);
         }
