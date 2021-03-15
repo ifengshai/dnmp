9454c3166caabd40477870e3722448eba5e79fe3
jianghaohui <jianghaohui@nextmar.com>
2021-03-15 17:50:31 +0800

    校验

--- application/api/controller/ScmWarehouse.php
+++ application/api/controller/ScmWarehouse.php
@@ -3480,7 +3480,7 @@ class ScmWarehouse extends Scm
             $this->error(__('条形码' . $code . 'sku：' . $detail['sku'] . '与当前调拨子单sku不一致请确认后重试'), '', 546);
         }
         if ($detail['purchase_id'] < 1) {
-            $this->error(__('条形码没有采购单绑定关系,条形码:' . $code, '', 402));
+            $this->error(__('条形码没有采购单绑定关系,条形码:' . $code, '', 546));
         }
         // //判断当前扫码的sku是否在当前调出的库位
         // if ($transfer_order_item['call_out_site'] != $detail['location_code']) {
