378479522765b5aa65daa92c26af69ca957de7a6
zjw <743090740@qq.com>
2021-03-16 18:21:45 +0800

    去除断点

--- application/admin/controller/supplydatacenter/DataMarket.php
+++ application/admin/controller/supplydatacenter/DataMarket.php
@@ -1303,12 +1303,9 @@ class DataMarket extends Backend
             } else {
                 $csv[$key]['work'] = '否';
             }
-
-
             $csv[$key]['delivery_time'] = $value['delivery_time']; //订单金额
             $csv[$key]['order_prescription_type'] = $work_type['problem_type_content']; //订单金额
         }
-        dump($csv);die();
         $headlist = [
             '订单号', '订单状态','处方类型',
             '支付时间', '站点','是否有工单',
