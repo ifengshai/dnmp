8c76d5a70df28bd1b26aa1c02bc5193cc0a59a05
wpl <wangpenglei@nextmar.com>
2021-03-18 14:59:05 +0800

    修改同步脚本

--- application/admin/controller/shell/OrderData.php
+++ application/admin/controller/shell/OrderData.php
@@ -1458,11 +1458,7 @@ class OrderData extends Backend
                 $arr['order_prescription_type'] = 3;
             }
         }
-        //定制处方镜
-        if ($arr['is_custom_lens'] == 1) {
-            $arr['order_prescription_type'] = 3;
-        }
-
+        
         //默认如果不是仅镜架 或定制片 则为现货处方镜
         if ($arr['order_prescription_type'] != 1 && $arr['order_prescription_type'] != 3) {
             $arr['order_prescription_type'] = 2;
