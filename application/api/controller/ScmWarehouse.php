4baee0061a2cfe17a5c1e4371ca123ca3c12118a
zjw <743090740@qq.com>
2021-03-23 14:02:04 +0800

    输出断点数据

--- application/api/controller/ScmWarehouse.php
+++ application/api/controller/ScmWarehouse.php
@@ -2215,7 +2215,11 @@ class ScmWarehouse extends Scm
             $cat['sku'] = ['like', '%' . $query . '%'];
             $cat['area_id'] = ['gt',0];
             //查询该sku是否绑定的有库区id
-            $inventory_item_id = $this->_inventory_item->where($cat)->column('sku,area_id');
+            $inventory_item_id = $this->_inventory_item->where($cat)
+                ->alias('a')
+                ->join(['fa_store_house' => 'b'], 'a.library_name=b.coding')
+                ->field('b.id,a.sku,a.warehouse_name,a.library_name,a.area_id')->select();
+            dump($inventory_item_id);die();
             //排除待盘点sku
             $sku_arr = $this->_inventory_item->alias('a')->join(['fa_inventory_list' => 'b'], 'a.inventory_id=b.id')
                 ->where(['b.status' => ['in', [0, 1]]])
