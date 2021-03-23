9a6aac0e535f0d6ec642afd7c079c7b3cbe5351e
zjw <743090740@qq.com>
2021-03-23 15:12:17 +0800

    .

--- application/api/controller/ScmWarehouse.php
+++ application/api/controller/ScmWarehouse.php
@@ -2215,7 +2215,7 @@ class ScmWarehouse extends Scm
             $cat['sku'] = ['like', '%' . $query . '%'];
             $cat['inventory_id'] = ['gt',0];
 //            //查询对应的库位id
-            $store_house_id =  Db::table('fa_product_barcode_item')->where($cat)->column('location_code_id');
+            $store_house_id =  Db::table('fa_product_barcode_item')->where($cat)->column('location_code_id,id');
             dump($store_house_id);die();
             if ($store_house_id){
                 $where['b.id'] = ['not in', $store_house_id];
