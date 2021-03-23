d82bf2d28f17f5e04ab098c1f17af0a68409ddcb
zjw <743090740@qq.com>
2021-03-23 16:21:23 +0800

    正式

--- application/api/controller/ScmWarehouse.php
+++ application/api/controller/ScmWarehouse.php
@@ -2229,8 +2229,6 @@ class ScmWarehouse extends Scm
                 }
 
             }
-            dump($where);die();
-
             //排除待盘点sku
             $sku_arr = $this->_inventory_item->alias('a')->join(['fa_inventory_list' => 'b'], 'a.inventory_id=b.id')
                 ->where(['b.status' => ['in', [0, 1]]])
