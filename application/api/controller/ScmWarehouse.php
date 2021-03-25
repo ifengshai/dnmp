2f9c71de3de3034599c55d4cc44ec05a2da00e34
zjw <743090740@qq.com>
2021-03-25 10:37:20 +0800

    去除断点

--- application/api/controller/ScmWarehouse.php
+++ application/api/controller/ScmWarehouse.php
@@ -2629,10 +2629,10 @@ class ScmWarehouse extends Scm
             Log::write($res);
             //审核通过 生成出、入库单 并同步库存
             if ($data['check_status'] == 2) {
+                //查询盘点单数据
                 $infos = $this->_inventory_item->where(['inventory_id' => $inventory_id])
                     ->field('sku,error_qty,inventory_id,area_id,library_name,sku_agg')
                     ->select();
-                dump($infos);die();
                 $infos = collection($infos)->toArray();
                 $sku_code = [];
                 Log::write("====输出审核盘点单======");
