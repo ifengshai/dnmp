42f047eb2a614fa18848294ddd7cd1576d0d432b
miaojingjing <mjj192015@gmail.com>
2021-03-29 13:57:14 +0800

    柱状图宽度调整

--- application/admin/controller/supplydatacenter/DataMarket.php
+++ application/admin/controller/supplydatacenter/DataMarket.php
@@ -349,7 +349,7 @@ class DataMarket extends Backend
             $json['columnData'] = [
                 [
                     'type' => 'bar',
-                    'barWidth' => '30%',
+                    'barWidth' => '20%',
                     'data' => array_column($data, 'virtual_stock'),
                     'name' => '虚拟仓库存'
                 ],
