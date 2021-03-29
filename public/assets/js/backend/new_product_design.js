48a60dc6d68e8b22c11fcba7daaf60050b624aee
zhangjingwei <743090740@qq.com>
2021-03-29 15:28:44 +0800

    选品设计管理

---
+++ public/assets/js/backend/new_product_design.js
@@ -0,0 +1,53 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'new_product_design/index' + location.search,
+                    add_url: 'new_product_design/add',
+                    edit_url: 'new_product_design/edit',
+                    del_url: 'new_product_design/del',
+                    multi_url: 'new_product_design/multi',
+                    table: 'new_product_design',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('序号')},
+                        {field: 'sku', title: __('Sku')},
+                        {field: 'status', title: __('状态')},
+                        {field: 'responsible_id', title: __('Responsible_id')},
+                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
