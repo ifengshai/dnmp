5e3f5b15db5063c5a1a4bcffc47e690298cddeea
zjw <743090740@qq.com>
2021-03-30 18:25:29 +0800

    feat(财务导出)：  时间字段调整
    时间字段调整

--- application/admin/controller/finance/FinanceCost.php
+++ application/admin/controller/finance/FinanceCost.php
@@ -261,7 +261,7 @@ class FinanceCost extends Backend
 
                 if ($value['payment_time']) {
 
-                    $value['payment_time'] = date('Y-d-m H:i:s', $value['payment_time']);
+                    $value['payment_time'] = date('Y-m-d H:i:s', $value['payment_time']);
 
                 } else {
 
@@ -271,7 +271,7 @@ class FinanceCost extends Backend
 
                 if ($value['createtime']) {
 
-                    $value['createtime'] = date('Y-d-m H:i:s', $value['createtime']);
+                    $value['createtime'] = date('Y-m-d H:i:s', $value['createtime']);
 
                 }
 
@@ -340,7 +340,7 @@ class FinanceCost extends Backend
 
                 if ($value['createtime']) {
 
-                    $value['createtime'] = date('Y-d-m H:i:s', $value['createtime']);
+                    $value['createtime'] = date('Y-m-d H:i:s', $value['createtime']);
 
                 }
 
