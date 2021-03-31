8c1085d82e0835068bfe65b550324b681da28b04
crasphb <646054215@qq.com>
2021-03-31 10:33:11 +0800

    feat(addibs): 上传s3插件开发
    上传s3插件开发

---
+++ addons/aws3/Aws3.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace addons\aws3;
+
+use think\Addons;
+
+/**
+ * 插件
+ */
+class Aws3 extends Addons
+{
+
+    /**
+     * 插件安装方法
+     * @return bool
+     */
+    public function install()
+    {
+
+        return true;
+    }
+
+    /**
+     * 插件卸载方法
+     * @return bool
+     */
+    public function uninstall()
+    {
+
+        return true;
+    }
+
+    /**
+     * 插件启用方法
+     * @return bool
+     */
+    public function enable()
+    {
+
+        return true;
+    }
+
+    /**
+     * 插件禁用方法
+     * @return bool
+     */
+    public function disable()
+    {
+
+        return true;
+    }
+
+}
---
+++ addons/aws3/config.php
@@ -0,0 +1,139 @@
+<?php
+
+return array (
+  0 => 
+  array (
+    'name' => 'uploadurl',
+    'title' => 'upload url',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'ajax/upload',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  1 => 
+  array (
+    'name' => 'savekey',
+    'title' => 'save key',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => '/uploads/{year}{mon}{day}/{filemd5}{.suffix}',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  2 => 
+  array (
+    'name' => 'maxsize',
+    'title' => 'max size',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => '10mb',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  3 => 
+  array (
+    'name' => 'mimetype',
+    'title' => 'mime type',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'jpg,png,bmp,jpeg,gif,zip,rar,xls,xlsx,docx,apk',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  4 => 
+  array (
+    'name' => 'access_key_id',
+    'title' => 'access key',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'AKIAT2RCARUTCLJLTCDL',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  5 => 
+  array (
+    'name' => 'secret_access_key',
+    'title' => 'secret access key',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'JDdEcIL5ViLh8PMm/fXRlWOiQyhk0J19AgJ2Xw2W',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  6 => 
+  array (
+    'name' => 'default_region',
+    'title' => 'default region',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'us-west-2',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  7 => 
+  array (
+    'name' => 'bucket',
+    'title' => 'bucket',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'mojing',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  8 => 
+  array (
+    'name' => 's3_url',
+    'title' => 's3 url',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'https://xmslaravel.s3-us-west-2.amazonaws.com/',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+);
