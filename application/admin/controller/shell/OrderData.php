3513257182882842e9a90581f87c93a82cb3cb70
wpl <wangpenglei@nextmar.com>
2021-04-02 14:18:26 +0800

    feat(跑支付币种支付金额): 跑支付币种支付金额
    跑支付币种支付金额

--- application/admin/controller/shell/OrderData.php
+++ application/admin/controller/shell/OrderData.php
@@ -2066,7 +2066,7 @@ class OrderData extends Backend
      */
     protected function order_data($site)
     {
-        $list = $this->order->where('grand_total < 1 and site = '.$site)->limit(4000)->select();
+        $list = $this->order->where('grand_total < 1 and site = '.$site)->where(['created_at' => ['>', '1612108800']])->limit(4000)->select();
         $list = collection($list)->toArray();
         $entity_id = array_column($list, 'entity_id');
         if ($site == 1) {
