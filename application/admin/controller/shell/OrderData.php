7219c96af9660e25f9545b011412591aed9599c6
wpl <wangpenglei@nextmar.com>
2021-04-02 14:09:14 +0800

    feat(跑支付币种支付金额): 跑支付币种支付金额
    跑支付币种支付金额

--- application/admin/controller/shell/OrderData.php
+++ application/admin/controller/shell/OrderData.php
@@ -1340,10 +1340,10 @@ class OrderData extends Backend
             $arr['order_prescription_type'] = 3;
             $arr['is_custom_lens'] = 1;
         }
-        $od_sph = (float) urldecode($params['od_sph']);
-        $os_sph = (float) urldecode($params['os_sph']);
-        $od_cyl = (float) urldecode($params['od_cyl']);
-        $os_cyl = (float) urldecode($params['os_cyl']);
+        $od_sph = (float)urldecode($params['od_sph']);
+        $os_sph = (float)urldecode($params['os_sph']);
+        $od_cyl = (float)urldecode($params['od_cyl']);
+        $os_cyl = (float)urldecode($params['os_cyl']);
 
         //仅镜框
         if ($params['lens_number'] == '10000000' || !$params['lens_number']) {
@@ -2048,7 +2048,7 @@ class OrderData extends Backend
         $this->order_data(1);
         $this->order_data(2);
         $this->order_data(3);
-        // $this->order_data(4);
+        $this->order_data(4);
         $this->order_data(5);
         $this->order_data(9);
         $this->order_data(10);
@@ -2065,31 +2065,33 @@ class OrderData extends Backend
      */
     protected function order_data($site)
     {
-        $list = $this->order->where('payment_time is null and site = '.$site)->limit(3000)->select();
+        $list = $this->order->where('grand_total < 1 and site = '.$site)->limit(4000)->select();
         $list = collection($list)->toArray();
         $entity_id = array_column($list, 'entity_id');
         if ($site == 1) {
-            $res = Db::connect('database.db_zeelool')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('payment_time', 'entity_id');
+            $res = Db::connect('database.db_zeelool')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
         } elseif ($site == 2) {
-            $res = Db::connect('database.db_voogueme')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('payment_time', 'entity_id');
+            $res = Db::connect('database.db_voogueme')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
         } elseif ($site == 3) {
-            $res = Db::connect('database.db_nihao')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('payment_time', 'entity_id');
+            $res = Db::connect('database.db_nihao')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
         } elseif ($site == 4) {
-            $res = Db::connect('database.db_meeloog')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('payment_time', 'entity_id');
+            $res = Db::connect('database.db_meeloog')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
         } elseif ($site == 5) {
-            $res = Db::connect('database.db_weseeoptical')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('payment_time', 'entity_id');
+            $res = Db::connect('database.db_weseeoptical')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
         } elseif ($site == 9) {
-            $res = Db::connect('database.db_zeelool_es')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('payment_time', 'entity_id');
+            $res = Db::connect('database.db_zeelool_es')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
         } elseif ($site == 10) {
-            $res = Db::connect('database.db_zeelool_de')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('payment_time', 'entity_id');
+            $res = Db::connect('database.db_zeelool_de')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
         } elseif ($site == 11) {
-            $res = Db::connect('database.db_zeelool_jp')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('payment_time', 'entity_id');
+            $res = Db::connect('database.db_zeelool_jp')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
+        } elseif ($site == 12) {
+            $res = Db::connect('database.db_voogueme_acc')->table('sales_flat_order')->where(['entity_id' => ['in', $entity_id]])->column('grand_total', 'entity_id');
         }
         $params = [];
         foreach ($list as $k => $v) {
             $params[$k]['id'] = $v['id'];
-            $params[$k]['payment_time'] = strtotime($res[$v['entity_id']]) + 28800;
-            // $params[$k]['region'] = $res[$v['entity_id']]['region'];
+            //$params[$k]['grand_total'] = $v['grand_total'];
+            $params[$k]['grand_total'] = $res[$v['entity_id']]['grand_total'];
             // $params[$k]['city'] = $res[$v['entity_id']]['city'];
             // $params[$k]['street'] = $res[$v['entity_id']]['street'];
             // $params[$k]['postcode'] = $res[$v['entity_id']]['postcode'];
