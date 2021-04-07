4304eaee7dd098286412ab06d795e9aa41952969
gyh <geyanghua@nextmar.com>
2021-04-07 11:44:34 +0800

    第三方对接工单

--- application/admin/controller/saleaftermanage/WorkOrderList.php
+++ application/admin/controller/saleaftermanage/WorkOrderList.php
@@ -2000,7 +2000,6 @@ class WorkOrderList extends Backend
             //判断此sku是否在第三方平台
             if ($siteType == 13 || $siteType == 14) {
                 $res = $this->model->httpRequest($siteType, 'api/mojing/check_sku', ['sku' =>$sku,'platform_order' =>$platform_order], 'POST');
-                print_r($res);die;
                 if (empty($res[$sku])) {
                     return ['result' => false, 'msg' => $sku . '不存在！！'];
                 }
