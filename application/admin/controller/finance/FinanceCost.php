300001af740c45981667f66fe2ab187043f5ac18
crasphb <646054215@qq.com>
2021-04-08 15:52:12 +0800

    fix(日语订单状态添加): 添加货到付款和便利店支付 #1721
    添加货到付款和便利店支付

--- application/admin/controller/finance/FinanceCost.php
+++ application/admin/controller/finance/FinanceCost.php
@@ -209,6 +209,11 @@ class FinanceCost extends Backend
             5 => '补差价',
 
             9 => 'vip订单',
+
+            10 => '货到付款',
+
+            11 => '便利店支付',
+
         ];
 
         $path = '/uploads/financeCost/';
