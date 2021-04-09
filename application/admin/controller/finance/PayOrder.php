ec64e2943dfd457b5fbe2f1c1b748cc97d007f27
miaojingjing <mjj192015@gmail.com>
2021-04-09 19:18:49 +0800

    feat(财务管理): 财务管理
    预估成本修改

--- application/admin/controller/finance/PayOrder.php
+++ application/admin/controller/finance/PayOrder.php
@@ -430,8 +430,9 @@ class PayOrder extends Backend
                         ->alias('i')
                         ->join('fa_purchase_order o', 'i.purchase_id=o.id')
                         ->where('i.purchase_id', $v)
-                        ->field('purchase_price,actual_purchase_price,i.sku')
+                        ->field('round(o.purchase_total/purchase_num,2) purchase_price,actual_purchase_price,i.sku')
                         ->find();
+
                     //实际采购成本和预估成本不一致，冲减差值
                     if ($purchaseOrder['purchase_price'] != $purchaseOrder['actual_purchase_price']) {
                         //计算订单出库数量
