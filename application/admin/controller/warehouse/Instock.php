262951a98f47debc1ce78d48e8669805a4a0bf28
zjw <743090740@qq.com>
2021-04-09 13:44:21 +0800

    入库单字段添加

--- application/admin/controller/warehouse/Instock.php
+++ application/admin/controller/warehouse/Instock.php
@@ -82,14 +82,14 @@ class Instock extends Backend
 
             list($where, $sort, $order, $offset, $limit) = $this->buildparams();
             $total = $this->model
-                ->with(['checkorder', 'instocktype'])
+                ->with(['checkorder', 'instocktype','productBarcodeItem'])
                 ->where($where)
                 ->where($map)
                 ->order($sort, $order)
                 ->count();
 
             $list = $this->model
-                ->with(['checkorder', 'instocktype'])
+                ->with(['checkorder', 'instocktype','productBarcodeItem'])
                 ->where($where)
                 ->where($map)
                 ->order($sort, $order)
@@ -471,7 +471,8 @@ class Instock extends Backend
         //查询条形码库区库位
         $barcodedata = $this->_product_bar_code_item->where(['in_stock_id' => ['in', $ids]])->column('location_code');
         $count = $this->_inventory->alias('a')
-            ->join(['fa_inventory_item' => 'b'], 'a.id=b.inventory_id')->where(['a.is_del' => 1, 'a.check_status' => ['in', [0, 1]], 'library_name' => ['in', $barcodedata]])
+            ->join(['fa_inventory_item' => 'b'], 'a.id=b.inventory_id')
+            ->where(['a.is_del' => 1, 'a.check_status' => ['in', [0, 1]], 'library_name' => ['in', $barcodedata]])
             ->count();
         if ($count > 0) {
             $this->error(__('此库位正在盘点,暂无法入库审核'), [], 403);
@@ -487,6 +488,7 @@ class Instock extends Backend
             }
         }
         $data['status'] = input('status');
+        //status  2 审核通过  3审核拒绝
         if ($data['status'] == 2) {
             $data['check_time'] = date('Y-m-d H:i:s', time());
         }
@@ -914,11 +916,11 @@ class Instock extends Backend
                     throw new Exception("入库失败！！请检查SKU");
                 }
             } else {
-                //审核拒绝解除条形码绑定关系
+                //审核拒绝解除条形码绑定关系 更新入库单id  条形码所在库位 条形码所在库位库区id 库位id
                 $_product_bar_code_item = new ProductBarCodeItem();
                 $_product_bar_code_item
                     ->where(['in_stock_id' => ['in', $ids]])
-                    ->update(['in_stock_id' => 0]);
+                    ->update(['in_stock_id' => 0,'location_code'=>'','location_id'=>'0','location_code_id'=>'0']);
             }
 
             $this->model->commit();
@@ -1500,5 +1502,13 @@ class Instock extends Backend
             $this->error($e->getMessage());
         }
         $this->success('导入成功！');
+    }
+
+    /**
+     * @author zjw
+     * @date   2021/4/9 10:05
+     */
+    public function Review_the_operation(){
+
     }
 }
