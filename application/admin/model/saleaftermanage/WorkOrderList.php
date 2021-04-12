fb0d4a9d77b27e72614dcc7fba349f9ce4cc09a9
gyh <geyanghua@nextmar.com>
2021-04-12 15:19:37 +0800

    第三方对接工单

--- application/admin/model/saleaftermanage/WorkOrderList.php
+++ application/admin/model/saleaftermanage/WorkOrderList.php
@@ -2241,7 +2241,7 @@ class WorkOrderList extends Model
 
                 }
                 $res = $this->httpRequest($workOrderList->work_platform, 'api/mojing/stock_change', $param, 'POST');//第三方平台库存
-                file_put_contents('/www/wwwroot/mojing/runtime/log/a.log',$param,FILE_APPEND);
+                //file_put_contents('/www/wwwroot/mojing/runtime/log/a.log',$param,FILE_APPEND);
                  if (1 == $measuerInfo) {
                     $ChangeFrameParam = [];
                     $ChangeFrameParam['increment_id'] = $workOrderList['platform_order'];
@@ -2249,7 +2249,7 @@ class WorkOrderList extends Model
                     $ChangeFrameParam['qty'] = $value['original_number'];
                     $ChangeFrameParam['action'] = 0;
                     $ress = $this->httpRequest($workOrderList->work_platform, 'api/mojing/stock_change', $ChangeFrameParam, 'POST');//第三方平台库存
-                    file_put_contents('/www/wwwroot/mojing/runtime/log/a.log',$ChangeFrameParam,FILE_APPEND);
+                    //file_put_contents('/www/wwwroot/mojing/runtime/log/a.log',$ChangeFrameParam,FILE_APPEND);
                 }
             }
             
