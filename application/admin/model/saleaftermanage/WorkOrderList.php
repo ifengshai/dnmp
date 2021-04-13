8db0f410ee70b68c69fb509e8ba84e9fba2ba869
gyh <geyanghua@nextmar.com>
2021-04-13 15:38:46 +0800

    第三方对接工单

--- application/admin/model/saleaftermanage/WorkOrderList.php
+++ application/admin/model/saleaftermanage/WorkOrderList.php
@@ -2218,8 +2218,9 @@ class WorkOrderList extends Model
             $info = (new Inventory())->workPresent($work_id, $workOrderList->work_platform, $workOrderList->platform_order, $result, 1);
             $param['action'] = 1;
         } elseif (5 == $measuerInfo) {//补发
-            if ($workOrderList['work_platform'] !== 13 || $workOrderList['work_platform'] !== 14) {//补发不扣第三方库存
-                $info = (new Inventory())->workPresent($work_id, $workOrderList->work_platform, $workOrderList->platform_order, $result, 2);
+            if ($workOrderList['work_platform'] == 13 || $workOrderList['work_platform'] == 14) {//补发不扣第三方库存
+                echo 123;die;
+                //$info = (new Inventory())->workPresent($work_id, $workOrderList->work_platform, $workOrderList->platform_order, $result, 2);
             }else{
                return false; 
             }
