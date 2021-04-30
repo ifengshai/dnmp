039c0450a6dd6b834331daeedc422b5aec0a22f0
jianghaohui <jianghaohui@nextmar.com>
2021-04-30 16:22:07 +0800

    feature(商品管理): #1837
    商品未经过产品设计管理审核通过不显示图片
    上传平台sku做校验 提示没有图片

--- application/admin/controller/itemmanage/ItemPlatformSku.php
+++ application/admin/controller/itemmanage/ItemPlatformSku.php
@@ -57,7 +57,7 @@ class ItemPlatformSku extends Backend
             if ($this->request->request('keyField')) {
                 return $this->selectpage();
             }
-            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            [$where, $sort, $order, $offset, $limit] = $this->buildparams();
             $total = $this->model
                 ->with(['item' => ['item_status']])
                 ->where($where)
@@ -109,7 +109,7 @@ class ItemPlatformSku extends Backend
         //自定义sku搜索
         $filter = json_decode($this->request->get('filter'), true);
 
-        list($where) = $this->buildparams();
+        [$where] = $this->buildparams();
         $list = $this->model
             ->with(['item' => ['item_status']])
             ->where($where)
@@ -263,7 +263,7 @@ class ItemPlatformSku extends Backend
             if ($this->request->request('keyField')) {
                 return $this->selectpage();
             }
-            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            [$where, $sort, $order, $offset, $limit] = $this->buildparams();
             $total = $this->model
                 ->where($where)
                 ->whereNotNull('presell_create_time')
@@ -654,6 +654,7 @@ class ItemPlatformSku extends Backend
     public function afterUploadItem($ids = null)
     {
         if ($this->request->isAjax()) {
+            $ids = input('id');
             $itemPlatformRow = $this->model->findItemPlatform($ids);
             $itemPlatformDetail = $this->model->where('id',$ids)->find();
             if ($itemPlatformRow['is_upload'] == 1) { //商品已经上传，无需再次上传
@@ -663,8 +664,6 @@ class ItemPlatformSku extends Backend
             $item = new \app\admin\model\itemmanage\Item();
             $itemAttribute = new ItemAttribute();
             $res = $item->where(['sku' => $itemPlatformRow['sku'], 'is_open' => 1, 'is_del' => 1])->find();
-            dump($itemPlatformDetail);
-            dump($res);
             if ($res['item_status'] <> 3){
                 $this->error(__('商品非审核通过状态，请先在商品管理模块进行审核'));
             }
