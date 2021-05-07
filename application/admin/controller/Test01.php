a6830da8f54d45542bc5fc225a8fad577ba35e9c
miaojingjing <mjj192015@gmail.com>
2021-05-07 18:19:28 +0800

    feat(财务管理): 台账
    导出数据

--- application/admin/controller/Test01.php
+++ application/admin/controller/Test01.php
@@ -921,5 +921,236 @@ class Test01 extends Backend
         return $hours_min;
     }
 
+    /**
+     * 财务数据 -- 库存台账导出
+     * @author mjj
+     * @date   2021/5/7 17:15:44
+     */
+    public function financeExport()
+    {
+        set_time_limit(0);
+        $this->instock = new \app\admin\model\warehouse\Instock;
+        $this->outstock = new \app\admin\model\warehouse\Outstock;
+        $this->stockparameter = new \app\admin\model\financepurchase\StockParameter;
+        $this->item = new \app\admin\model\warehouse\ProductBarCodeItem;
+        $this->model = new \app\admin\model\itemmanage\Item;
+        //时间
+        $start = '2020-05-06';
+        $end = '2020-05-06 23:59:59';
+        //采购入库数量
+        $instockWhere['s.status'] = 2;
+        $instockWhere['s.type_id'] = 1;
+        $instockWhere['s.check_time'] = ['between', [$start, $end]];
+        $inSkus = $this->instock
+            ->alias('s')
+            ->join('fa_in_stock_item i','i.in_stock_id=s.id')
+            ->where($instockWhere)
+            ->group('sku')
+            ->column('sku');
+        $arr1 = [];
+        $i = 0;
+        foreach ($inSkus as $inSku){
+            //获取入库sku类别
+            $category = $this->model
+                ->alias('i')
+                ->join('fa_item_category c','i.category_id=c.id')
+                ->where('sku',$inSku)
+                ->value('c.name');
+            $arr1[$i]['category'] = $category;
+            $arr1[$i]['sku'] = $inSku;
+            $arr1[$i]['inOutFlag'] = 1;//入库
+            $instocks = $this->instock
+                ->alias('s')
+                ->join('fa_check_order c','c.id=s.check_id')
+                ->join('fa_purchase_order_item oi', 'c.purchase_id=oi.purchase_id')
+                ->join('fa_purchase_order o', 'oi.purchase_id=o.id')
+                ->where($instockWhere)
+                ->where('oi.sku',$inSku)
+                ->field('sum(o.purchase_total) purchase_total,sum(oi.purchase_num) purchase_num')
+                ->find();
+            $arr1[$i]['total'] = $instocks['purchase_total'];//入库金额
+            $arr1[$i]['num'] = $instocks['purchase_num'];//入库数量
+            $i++;
+        }
 
+        /*************出库单出库start**************/
+        $barWhere['out_stock_time'] = ['between', [$start, $end]];
+        $barWhere['out_stock_id'] = ['<>', 0];
+        $barWhere['library_status'] = 2;
+        $bars = $this->item
+            ->where($barWhere)
+            ->group('sku')
+            ->column('sku');
+        $arr2 = [];
+        $j = 0;
+        foreach ($bars as $bar) {
+            //获取出库sku类别
+            $category = $this->model
+                ->alias('i')
+                ->join('fa_item_category c','i.category_id=c.id')
+                ->where('sku',$bar)
+                ->value('c.name');
+            $arr2[$j]['category'] = $category;
+            $arr2[$j]['sku'] = $bar;
+            $arr2[$j]['inOutFlag'] = 2;//出库单出库
+            $barItems = $this->item
+                ->alias('i')
+                ->join('fa_purchase_order_item p', 'i.purchase_id=p.purchase_id and i.sku=p.sku')
+                ->join('fa_purchase_order o', 'p.purchase_id=o.id')
+                ->field('sum(o.purchase_total) purchase_total,sum(p.purchase_num) purchase_num')
+                ->where($barWhere)
+                ->where('sku', $bar)
+                ->find();
+            $arr2[$j]['total'] = $barItems['purchase_total'];//出库金额
+            $arr2[$j]['num'] = $barItems['purchase_num'];//出库数量
+            $j++;
+        }
+        /*************出库单出库end**************/
+
+
+
+
+
+        $_item_platform_sku = new \app\admin\model\itemmanage\ItemPlatformSku();
+        $sku_data = $_item_platform_sku
+            ->field('sku,grade,platform_sku')
+            ->where(['platform_type' => 1])
+            ->select();
+        $sku_data = collection($sku_data)->toArray();
+        echo "sku_data:success\n";
+
+        $sku_arr = array_column($sku_data, 'sku');
+        $platform = [];
+        $grade = [];
+        foreach ($sku_data as $value) {
+            $grade[$value['sku']] = $value['grade'];
+            $platform[$value['sku']] = $value['platform_sku'];
+        }
+
+        $_new_product = new \app\admin\model\NewProduct();
+        $list = $_new_product
+            ->alias('a')
+            ->field('sku,frame_color,frame_texture,shape,frame_shape,price')
+            ->where(['item_status' => 2, 'is_del' => 1, 'sku' => ['in', $sku_arr]])
+            ->join(['fa_new_product_attribute' => 'b'], 'a.id=b.item_id', 'left')
+            ->select();
+        $list = collection($list)->toArray();
+        echo "list:success\n";
+
+        /*//从数据库查询需要的数据
+        $spreadsheet = new Spreadsheet();
+
+        //常规方式：利用setCellValue()填充数据
+        $spreadsheet->setActiveSheetIndex(0)
+            ->setCellValue("A1", "SKU")
+            ->setCellValue("B1", "产品评级")
+            ->setCellValue("C1", "材质")
+            ->setCellValue("D1", "框型")
+            ->setCellValue("E1", "形状")
+            ->setCellValue("F1", "颜色")
+            ->setCellValue("G1", "进价")
+            ->setCellValue("H1", "平均月销量")
+            ->setCellValue("I1", "平均售价")
+            ->setCellValue("J1", "最大月销量")
+            ->setCellValue("K1", "最大月销量月份")
+            ->setCellValue("L1", "201910~202009总销量")
+            ->setCellValue("M1", "配镜率")
+        ;   //利用setCellValues()填充数据*/
+
+        $frame_texture = [1 => '塑料', 2 => '板材', 3 => 'TR90', 4 => '金属', 5 => '钛', 6 => '尼龙', 7 => '木质', 8 => '混合材质', 9 => '合金', 10 => '其他材质'];
+        $frame_shape = [1 => '长方形', 2 => '正方形', 3 => '猫眼', 4 => '圆形', 5 => '飞行款', 6 => '多边形', 7 => '蝴蝶款'];
+        $shape = [1 => '全框', 2 => '半框', 3 => '无框'];
+        $purchase = new \app\admin\model\purchase\PurchaseOrder();
+        $file_content = '';
+        foreach ($list as $key => $value) {
+            //获取平均采购价
+            $res = $purchase->alias('a')->field('sum(b.purchase_num) as purchase_num,sum(b.purchase_total) as purchase_total')
+                ->where(['a.purchase_status' => ['in', [2, 5, 6, 7, 9, 10]]])
+                ->where(['b.sku' => $value['sku']])
+                ->join(['fa_purchase_order_item' => 'b'], 'a.id=b.purchase_id')
+                ->where(['a.createtime' => ['>=', '2019-10-01 00:00:00']])
+                ->where(['a.createtime' => ['<=', '2020-09-30 23:59:59']])
+                ->select();
+
+            $statistics = $this->zeelool
+                ->alias('a')
+                ->field("sum(b.qty_ordered) AS num,sum(base_price) as price,DATE_FORMAT(b.created_at, '%Y-%m') AS time")
+                ->where(['a.status' => ['in', ['processing', 'complete', 'creditcard_proccessing', 'free_processing']]])
+                ->where(['b.created_at' => ['>=', '2019-10-01 00:00:00']])
+                ->where(['b.created_at' => ['<=', '2020-09-30 23:59:59']])
+                ->where(['b.sku' => $platform[$value['sku']]])
+                ->join(['sales_flat_order_item' => 'b'], 'a.entity_id=b.order_id', 'LEFT')
+                ->group("time")
+                ->select();
+            $statistics = collection($statistics)->toArray();
+            $ages = array_column($statistics, 'num');
+            array_multisort($ages, SORT_DESC, $statistics);
+
+            $all_count = 0;
+            $all_money = 0;
+            foreach ($statistics as $item) {
+                $all_count += $item['num'];
+                $all_money += $item['price'];
+            }
+
+            $prescription = $this->zeelool
+                ->alias('a')
+                ->field("sum(b.qty_ordered) AS num")
+                ->where(['a.status' => ['in', ['processing', 'complete', 'creditcard_proccessing', 'free_processing']]])
+                ->where(['b.created_at' => ['>=', '2019-10-01 00:00:00']])
+                ->where(['b.created_at' => ['<=', '2020-09-30 23:59:59']])
+                ->where(['b.product_options' => ['not like', '%frameonly%']])
+                ->where(['b.product_options' => ['not like', '%nonprescription%']])
+                ->where(['b.sku' => $platform[$value['sku']]])
+                ->join(['sales_flat_order_item' => 'b'], 'a.entity_id=b.order_id', 'LEFT')
+                ->select();
+            $prescription = collection($prescription)->toArray();
+
+            $monthly_sales = $all_count > 0 ? $all_count / 12 : 0;
+            $average_price = $statistics[0]['price'] > 0 && $statistics[0]['num'] > 0 ? $statistics[0]['price'] / $statistics[0]['num'] : 0;
+            $proportion = $all_count > 0 && $statistics[0]['num'] > 0 ? $prescription[0]['num'] / $all_count : 0;
+
+            /*$num = $key + 2;
+            $spreadsheet->getActiveSheet()->setCellValueExplicit("A{$num}", $value['sku'], \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+            $spreadsheet->getActiveSheet()->setCellValue("B{$num}", $grade[$value['sku']]);
+            $spreadsheet->getActiveSheet()->setCellValue("C{$num}", $frame_texture[$value['frame_texture']]);
+            $spreadsheet->getActiveSheet()->setCellValue("D{$num}", $frame_shape[$value['frame_shape']]);
+            $spreadsheet->getActiveSheet()->setCellValue("E{$num}", $shape[$value['shape']]);
+            $spreadsheet->getActiveSheet()->setCellValue("F{$num}", $value['frame_color']);
+            $spreadsheet->getActiveSheet()->setCellValue("G{$num}", $value['price']);
+            $spreadsheet->getActiveSheet()->setCellValue("H{$num}", $monthly_sales);
+            $spreadsheet->getActiveSheet()->setCellValue("I{$num}", $average_price);
+            $spreadsheet->getActiveSheet()->setCellValue("J{$num}", $statistics[0]['num']);
+            $spreadsheet->getActiveSheet()->setCellValue("K{$num}", $statistics[0]['time']);
+            $spreadsheet->getActiveSheet()->setCellValue("L{$num}", $all_count);
+            $spreadsheet->getActiveSheet()->setCellValue("M{$num}", $proportion);*/
+
+            $ava_purchase_price = $res[0]['purchase_num'] > 0 && $res[0]['purchase_total'] > 0 ? $res[0]['purchase_total'] / $res[0]['purchase_num'] : 0;
+            $arr = [
+                $value['sku'],
+                $grade[$value['sku']],
+                $ava_purchase_price,
+                $frame_texture[$value['frame_texture']],
+                $frame_shape[$value['frame_shape']],
+                $shape[$value['shape']],
+                $value['frame_color'],
+                $value['price'],
+                $monthly_sales,
+                $average_price,
+                $statistics[0]['num'],
+                $statistics[0]['time'],
+                $all_count,
+                $all_money,
+                $proportion
+            ];
+            $file_content = $file_content . implode(',', $arr) . "\n";
+            echo "{$value['sku']}:success\n";
+        }
+
+        $export_str = ['SKU', '产品评级', '平均采购价CNY', '材质', '框型', '形状', '颜色', '进价', '平均月销量', '平均售价', '最大月销量', '最大月销量月份', '201910~202009总销量', '19年10月~20年9月总销售额', '配镜率'];
+        $file_title = implode(',', $export_str) . " \n";
+        $file = $file_title . $file_content;
+        file_put_contents('/www/wwwroot/mojing/runtime/log/analysis.csv', $file);
+        exit;
+    }
 }
