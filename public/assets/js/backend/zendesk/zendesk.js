e30aa37f98366af590251fd0dbca6405584c541c
miaojingjing <mjj192015@gmail.com>
2021-05-07 16:59:18 +0800

    feat(客服管理): 邮件工单
    添加组别

--- public/assets/js/backend/zendesk/zendesk.js
+++ public/assets/js/backend/zendesk/zendesk.js
@@ -59,6 +59,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'jq-tags', 'jqui','te
                         {field: 'type', title: __('type'), custom: { 1: 'yellow', 2: 'blue' ,3: 'danger'}, searchList: { 1: 'Zeelool', 2: 'Voogueme' ,3: 'Nihao'}, formatter: Table.api.formatter.status },
                         {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange',sortable: true},
                         {field: 'zendesk_update_time', title: __('Update_time'), operate:'RANGE', addclass:'datetimerange',sortable: true},
+                        {field: 'group_name', title: __('组别'),operate:false},
                         {
                             field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, buttons: [
                                 {
