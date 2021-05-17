ff4fa6af88a30f16dc108bf5fe996b2aa976812e
miaojingjing <mjj192015@gmail.com>
2021-05-17 11:47:28 +0800

    fix(供应链分析): 供应链大屏
    采购数据修复老数据

---
+++ application/admin/controller/shell/supply/PurchaseAsynData.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * 运营统计--用户复购率分析脚本
+ */
+namespace app\admin\controller\shell\supply;
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use think\Db;
+
+class PurchaseAsynData extends Command
+{
+    public function __construct()
+    {
+        $this->purchase = new \app\admin\model\purchase\PurchaseOrder();
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('purchase_asyn_data')
+            ->setDescription('purchase run');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        $data = Db::name('warehouse_data')
+            ->column('create_date','id');
+        $map['is_del'] = 1;
+        $map['purchase_status'] = ['in', [2, 5, 6, 7, 8, 9, 10]];
+        foreach ($data as $k=>$val){
+            $map['createtime'] = ['between', [$val . ' 00:00:00', $val . ' 23:59:59']];
+            $purchase_num = $this->purchase
+                ->alias('a')
+                ->where($map)
+                ->join('fa_purchase_order_item b', 'a.id=b.purchase_id')
+                ->sum('b.purchase_num');
+            Db::name('warehouse_data')
+                ->where('id',$k)
+                ->update(['all_purchase_num'=>$purchase_num]);
+            echo $val." is ok"."\n";
+            usleep(10000);
+        }
+        $output->writeln("All is ok");
+    }
+
+}
