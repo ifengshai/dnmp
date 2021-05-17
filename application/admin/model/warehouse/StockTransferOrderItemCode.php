d2e56e78de284179af8e611440ba1632fa6c1786
jianghaohui <jianghaohui@nextmar.com>
2021-05-17 18:39:02 +0800

    feature(仓库管理): #实体分仓
    实体分仓实体仓调拨单实体仓调拨单列表接口、配货出库实体仓
    调拨单详情页面、调出sku信息、扫描条码返回信息、配货出库
    实体仓调拨单扫码页面提交、sku信息页面编辑条码

---
+++ application/admin/model/warehouse/StockTransferOrderItemCode.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\admin\model\warehouse;
+
+use think\Model;
+
+
+class StockTransferOrderItemCode extends Model
+{
+
+
+    // 表名
+    protected $name = 'stock_transfer_order_item_code';
+
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = 'int';
+
+    // 定义时间戳字段名
+    protected $createTime = 'create_time';
+    protected $updateTime = false;
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [];
+
+}
