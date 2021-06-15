a1030d9efdf05fd5fa908f8b1aa8d9b86334392d
wangpenglei <wangpenglei@nextmar.com>
2021-06-15 11:07:27 +0800

    fix(测试快递100回调): 测试快递100回调
    测试快递100回调

--- application/api/controller/ThirdApi.php
+++ application/api/controller/ThirdApi.php
@@ -381,6 +381,7 @@ class ThirdApi extends Api
                     $update_order_node['order_node'] = 4;
                     $update_order_node['node_type'] = 40;
                     $update_order_node['update_time'] = $v['time'];
+                    $update_order_node['signing_time'] = $v['time'];
                     Db::name('order_node')->where('id', $order_node_date['id'])->update($update_order_node); //更新主表状态
 
                     $order_node_detail['order_node'] = 4;
