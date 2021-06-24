99d160c8d6a303cb062400af412f6a428e61af42
huangbinbin <huangbinbin@nextmar.com>
2021-06-24 10:46:51 +0800

    fix(订单): 订单导出优化
    订单导出优化

--- application/admin/controller/zendesk/Zendesk.php
+++ application/admin/controller/zendesk/Zendesk.php
@@ -1148,7 +1148,7 @@ Please close this window and try again.");
                 //被合并的状态closed，添加content，tag：closed_by_merge
                 $this->model->where('ticket_id', $ids)->update([
                     'status' => '5',
-                    'tags' =>  v,
+                    'tags' => $tagIds,
                     'assignee_id' => $agent_id,
                     'assign_id' => session('admin.id'),
                     'due_id' => session('admin.id'),
