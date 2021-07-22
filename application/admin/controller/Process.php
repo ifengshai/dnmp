8fe1ffa43f0441e18a5c9359a9789c227e8e5b6b
huangbinbin <huangbinbin@nextmar.com>
2021-07-22 14:51:25 +0800

    feat(物流数据): 物流数据修改
    修改加诺数据细分化

--- application/admin/controller/Process.php
+++ application/admin/controller/Process.php
@@ -1961,8 +1961,11 @@ class Process extends Backend
                     //波多黎各
                     $shipment_data_type ='加诺-波多黎各';
                 }
+                echo $shipment_data_type .PHP_EOL;
+                echo $order['id'] .PHP_EOL;
+                echo $orderNode['order_number'] .PHP_EOL;
                 //修改节点信息
-                OrderNode::where('id',$orderNode)->setField('shipment_data_type',$shipment_data_type);
+                //OrderNode::where('id',$orderNode)->setField('shipment_data_type',$shipment_data_type);
             }
         }
     }
