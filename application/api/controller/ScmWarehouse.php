0176778114bbc88872e5e2ef62c87e7a8f51b9c2
huangbinbin <huangbinbin@nextmar.com>
2021-07-23 18:44:33 +0800

    feat(工单): 工单
    工单

--- application/api/controller/ScmWarehouse.php
+++ application/api/controller/ScmWarehouse.php
@@ -3899,7 +3899,7 @@ class ScmWarehouse extends Scm
                                         Log::error('shitisql3'.$this->_item_platform_sku->where('sku', $sv['sku'])->where('platform_type', $val['platform_type'])->select(false));
                                         $itemPlatformSkuDetail = $this->_item_platform_sku->where('sku', $sv['sku'])->where('platform_type', $val['platform_type'])->find();
                                         Log::error('shitisql3'."{$stockNum}-{$sv['sku']}-{$val['platform_type']}");
-                                        $this->_item_platform_sku->where('sku', $sv['sku'])->where('platform_type', $val['platform_type'])->setDec('stock', $stockNum);
+                                        $this->_item_platform_sku->where(['sku' => $sv['sku'],'platform_type' => $val['platform_type']])->setDec('stock', $stockNum);
                                         Log::error('shitisql3'.' setDec');
                                         //插入日志表
                                         (new StockLog())->setData([
