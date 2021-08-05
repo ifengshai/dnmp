6b8bf96a5964f24b53b152e31e4d4cae83c57c51
wangpenglei <wangpenglei@nextmar.com>
2021-08-05 09:55:11 +0800

    feat(添加更新分仓脚本): 添加更新分仓脚本
    添加更新分仓脚本

--- application/admin/controller/shell/OrderPrescriptionType.php
+++ application/admin/controller/shell/OrderPrescriptionType.php
@@ -35,9 +35,10 @@ class OrderPrescriptionType extends Command
         $list = $order->where('order_prescription_type', 0)
             ->where('site', 1)
             ->where('created_at', '>', strtotime('2021-07-22 16:30:00'))
-            ->column('id');
+            ->field('id,entity_id')
+            ->select();
         foreach ($list as $key => $value) {
-            $order_type = $orderitemprocess->where('order_id', $value)->column('order_prescription_type');
+            $order_type = $orderitemprocess->where('magento_order_id', $value['entity_id'])->where('site', 1)->column('order_prescription_type');
             //查不到结果跳过 防止子单表延迟两分钟查不到数据
             if (!$order_type) {
                 continue;
@@ -52,13 +53,13 @@ class OrderPrescriptionType extends Command
                 $type = 1;
                 //如果Z站全为仅镜框 则分到丹阳仓
                 $data['stock_id'] = 2;
-                $orderitemprocess->where('order_id', $value)->update(['stock_id' => 2, 'wave_order_id' => 0]);
+                $orderitemprocess->where('magento_order_id', $value['entity_id'])->where('site', 1)->update(['stock_id' => 2, 'wave_order_id' => 0]);
             }
 
             $data['order_prescription_type'] = $type;
             $data['updated_at'] = time();
-            $order->where('id', $value)->update($data);
-            echo $value . ' is ok' . "\n";
+            $order->where('id', $value['id'])->update($data);
+            echo $value['id'] . ' is ok' . "\n";
             usleep(100000);
         }
     }
