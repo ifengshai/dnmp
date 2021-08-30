5e8434e0ba8479eb453bb896ddc424faa710327f
huangbinbin <huangbinbin@nextmar.com>
2021-08-30 16:07:45 +0800

    feat(product_design): product_design修改
    product_design添加ED，A

--- application/admin/controller/Process.php
+++ application/admin/controller/Process.php
@@ -2512,4 +2512,20 @@ class Process extends Backend
 
         }
     }
+
+    public function chageProductEdA()
+    {
+        $products = Db::name('fa_zzzz_product')->column('sku',['ED','A']);
+        $skus = array_keys($products);
+        $items = Db::connect('database.db_stock')->table('fa_item')->alias('a')->join('fa_item_attribute b','b.item_id=a.id')->where('a.origin_sku','in',$skus)->field('b.item_id,b.id,a.origin_sku')->select();
+        foreach($items as $item) {
+            $value = $skus[$item['origin_sku']];
+            Db::connect('database.db_stock')->table('fa_item_attribute')->where('id',$item['id'])->update([
+                'mirror_width' => $value['A'],
+                'mirror_width_ed' => $value['ED'],
+            ]);
+            echo $item['origin_sku'];
+        }
+
+    }
 }
