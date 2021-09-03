b30f34513fc3fb8d2d6b2300911e759c0f5ca324
huangbinbin <huangbinbin@nextmar.com>
2021-09-03 15:09:19 +0800

    feat(export): 数据导出
    数据导出

--- application/admin/controller/Test01.php
+++ application/admin/controller/Test01.php
@@ -60,7 +60,7 @@ class Test01 extends Backend
                 ->field('sku,frame_color,frame_texture,shape,frame_shape,price')
                 ->where(['item_status' => 2, 'is_del' => 1, 'sku' => ['in', $sku_arr]])
                 ->join(['fa_new_product_attribute' => 'b'], 'a.id=b.item_id', 'left')
-                ->limit($offset,1000)
+                ->limit($offset,100)
                 ->select();
             $list = collection($list)->toArray();
             echo "list:success{$i}\n";
