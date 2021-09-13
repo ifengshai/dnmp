3b13c3a0ec1e8b6ff1009fab16f83472c6fcb2e8
gyh <geyanghua@nextmar.com>
2021-09-13 11:47:35 +0800

    feat(售后工单): 抖音售后工单 #2505
    增加补发镜片，寄回换片抖音措施

--- public/assets/js/backend/saleaftermanage/work_order_list.js
+++ public/assets/js/backend/saleaftermanage/work_order_list.js
@@ -3886,6 +3886,7 @@ function changeOrderAddress(){
 function itemSelectpicker (type = 1,flag = null,item_order_info = '') {
         if($('#step3').prop('checked')) return false;
         $('#z-order_sku').html('');
+        $('.step24_lens_back').hide();
         $('#section_item_content').show();
         $('#section_item_content').html('');
         $("#item_input-hidden").html("");
@@ -3915,7 +3916,6 @@ function itemSelectpicker (type = 1,flag = null,item_order_info = '') {
             var step_item = Config.workOrderConfigValue.step;
             var item_checkbox = '';
             var order_item_numbers = item_order_sku.join(',');
-
             $('#div_item_content').show();
             var ihtml = '';
             for (var i in item_order_sku) {
@@ -3954,7 +3954,6 @@ function itemSelectpicker (type = 1,flag = null,item_order_info = '') {
             $('#order_item_numbers').val(order_item_numbers);
         } else{
             $('#section_item_content').hide();
-            $('.step24_lens_back').hide();
         }      
 }
 
