0c57b4a980103426dfbb650a14726b0f4b5b4dc0
jianghaohui <jianghaohui@nextmar.com>
2021-09-23 15:38:12 +0800

    feature(新增丹阳仓):销量排行榜页面增加展示商品图片
    销量排行榜页面增加展示商品图片
    展示魔晶数据库选品创建时上传的商品图片

--- application/admin/controller/operatedatacenter/newgoodsdata/GoodsSalesNum.php
+++ application/admin/controller/operatedatacenter/newgoodsdata/GoodsSalesNum.php
@@ -85,6 +85,10 @@ class GoodsSalesNum extends Backend
             foreach ($list as $k => $v) {
                 $result[$i]['platformsku'] = $k;
                 $result[$i]['sku'] = $skus[trim($k)]['sku'];
+                $result[$i]['image'] = Db::name('new_product')
+                    ->alias('a')
+                    ->join(['fa_new_product_attribute' => 'b'], 'a.id=b.item_id', 'left')
+                    ->value('frame_images');
                 //上架时间
                 $shelvesTime = Db::name('sku_shelves_time')
                     ->where(['site'=>$params['site'],'platform_sku'=>$k])
