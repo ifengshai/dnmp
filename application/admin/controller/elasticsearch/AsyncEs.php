1ceeb6a73cd66c3028a0973c6c8a055264b896e4
huangbinbin <huangbinbin@nextmar.com>
2021-11-13 10:14:40 +0800

    feat(shell): 批发站漏单添加
    批发站漏单添加

--- application/admin/controller/elasticsearch/AsyncEs.php
+++ application/admin/controller/elasticsearch/AsyncEs.php
@@ -189,7 +189,7 @@ class AsyncEs extends BaseElasticsearch
      */
     public function asyncCustomerMagento()
     {
-        $site = 10;
+        $site = 11;
 
         if($site == 1) {
             $db = Db::connect('database.db_zeelool_online');
